51a72ae2b239307e5aebb6b4558fa13f270d1099
Charles Pletcher <cpletcher2@gmail.com>
2016-07-29 12:50:07 -0400

    Update tests again

--- test/root.js
+++ test/root.js
@@ -1,26 +1,15 @@
 global.expect = require('expect');
 
 const babel = require('babel-core');
-const jsdom = require('jsdom');
+const jsdom = require('mocha-jsdom');
 const path = require('path');
 
-
-before(function(done) {
-  const babelResult = babel.transformFileSync(path.resolve(__dirname, '..', 'index.js'), {
+const babelResult = babel.transformFileSync(
+  path.resolve(__dirname, '..', 'index.js'), {
     presets: ['es2015']
-  });
+  }
+);
 
-  //const src = path.resolve(__dirname, '..', 'index.js');
-
-  jsdom.env('<div></div>', [], {src: babelResult.code}, (err, window) => {
-    if (err) {
-      return done(err);
-    }
-
-    Object.keys(window).forEach(key => {
-      global[key] = window[key];
-    });
-
-    return done();
-  });
-}); 
+jsdom({
+  src: babelResult.code
+});
