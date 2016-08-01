8cc04d62066ffb886d6cb5d3914541215c41f1e5
Steve Kuo <skuo314@gmail.com>
2016-08-01 01:00:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians,instruments) {
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  } return arr;
+}
+
+function johnLennonFacts(facts) {
+  var arr = [];
+  var counter = 0;
+  while (counter < facts.length) {
+    arr.push(facts[counter] + "!!!");
+    counter++;
+  } return arr;
+}
+
+function iLoveTheBeatles(number) {
+  var arr = [];
+  var i = 0;
+  if (number < 15) {
+    do {
+      arr.push("I love the Beatles!");
+      i++;
+    } while (i <= number);
+  } else {
+    arr.push("I love the Beatles!");
+  } return arr;
+}
