5a96ca7514a3c10a82cda8fb4ec74eb2a07ef72e
Joseph Sasson <ysasson88@gmail.com>
2016-08-07 16:58:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function the BeatlesPlay(musicians, instruments) {
+function theBeatlesPlay(musicians, instruments) {
   var a = []
   for(var i = 0; i < musicians.length; i++){
     a.push(musicians[i] + ' plays ' + instruments[i])
   }
+  return a
+}
+
+function johnLennonFacts(facts) {
+  var a = []
+  for(var i = 0; i < facts.length; i++){
+    a.push(facts[i] + '!!!')
+  }
+  return a
+}
+
+function iLoveTheBeatles(times) {
+  var a = []
+  do {
+    a.push("I love the Beatles!")
+    times--
+  }
+  while (times < 15 && times >= 0)
   return a
 }
