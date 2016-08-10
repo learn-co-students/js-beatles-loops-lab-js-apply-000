552d98c31d1d79a206205eb3cf9a51d26e661088
Krystle Mason <krystlejustine@gmail.com>
2016-08-10 13:36:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments){            //function called theBeatlesPlay passing musicians and instruments in it
+    var array = [];                                         // set up empty array stored in variable called array
+    for (var i=0; i<musicians.length; i++){                 // for loop to loop over array of musicians
+  //    for (var j=0; j<instruments.length; j++)           for loop to loop over array of instruments
+        array.push(musicians[i] + " plays " + instruments[i])                                        // create string using index at i for musicians and index at j for instruments
+
+
+  }
+  return array
+}
+
+
+function johnLennonFacts(facts){
+    var i = 0;
+    var arrayB = [];
+    while(i<facts.length){
+      arrayB.push(facts[i] + "!!!")
+      i++
+    }
+    return arrayB
+  }
+var i = 0;
+function iLoveTheBeatles(){
+    do{
+      var arrayC=[];
+      console.log(i);
+      i++
+      }
+    while (i < 15);
+  }
