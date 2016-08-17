d1250db6f1110ab2b24cea0258718b5902f8948a
Hillary Kane <hillaryrkane47@gmail.com>
2016-08-17 16:30:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  const array = [];
+
+  let i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`)
+    i++
+  }
+  return array;
+}
+
+function iLoveTheBeatles(n) {
+  const array = [];
+
+  do {
+    array.push(`I love the Beatles!`)
+    n++
+  }
+  while (n < 15)
+  return array;
+}
