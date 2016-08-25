01dba6f2457eebca23a3b0ba9680dfab45f9dc5a
nycsteven7 <nyviasteven@gmail.com>
2016-08-25 10:44:32 -0400

    first update to file
    has the structure but need to fill in the details

---
+++ index.js
@@ -0,0 +1,20 @@
+var theBeatlesPlay = function(arrayMusic,arrayInstrument){
+	var myString = [];
+	for(i=0,myString=null;;i++){
+		
+	}
+}
+
+var johnLennonFacts = function(anArray){
+	while(condition){
+		
+	}
+}
+
+var iLoveTheBeatles = function(){
+	do{
+		
+	}
+	while(condition);
+}
+
