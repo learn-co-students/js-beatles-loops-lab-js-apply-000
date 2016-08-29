496778ac7e4630dfc75c5f368b1c0bde6bc86b25
steven pan <nyviasteven@gmail.com>
2016-08-29 15:58:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 var theBeatlesPlay = function(arrayMusic,arrayInstrument){
 	var myString = [];
-	for(i=0;i<arrayMusic.length;i++){
+	for(var i=0;i<arrayMusic.length;i++){
 		myString.push(`${arrayMusic[i]} plays ${arrayInstrument[i]}`);
 	}
 	return myString;
 }
 
 var johnLennonFacts = function(anArray){
-	i=1;
-	while(i<(anArray.length)*2){
-		anArray.splice(i,0,"!!!");
-		i+=2;
+  var myString = [];
+	var i=0;
+	while(i<anArray.length){
+		myString.push(`${anArray[i]}!!!`);
+		i+=1;
 	}
-	return anArray;
+	return myString;
 }
 
 var iLoveTheBeatles = function(n){
