923f66a936cdf25e2b9eed8a7fed2822d18f8e0a
Richard Shyong <rshyong@gmail.com>
2016-09-01 04:51:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var ary = [];
+  for (var i = 0; i < musicians.length; i++) {
+    ary.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return ary;
+}
+
+function johnLennonFacts(argument) {
+  var i = 0;
+  var ary = [];
+  while (i < argument.length) {
+    ary.push(argument[i] + "!!!");
+    i += 1;
+  }
+  return ary;
+}
+
+function iLoveTheBeatles(num) {
+  var ary = [];
+  do {
+    ary.push("I love the Beetles!");
+    num += 1;
+  } while (num < 15);
+  return ary;
+}
