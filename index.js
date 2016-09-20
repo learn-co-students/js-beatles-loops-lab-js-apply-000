98fb8a2d33b39c1c7147416c886edde302510cc2
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-09-20 12:49:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
+}
+
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+
+  for (let i=0, l = facts.length; i < l; i++) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+  }
+  return shoutedFacts
+}
+
+
+function iLoveTheBeatles(n) {
+  const arr = []
+
+  do {
+    arr.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+
+  return arr
+}
