8b987d33eba174afde45e6a53ed5d4ed9c101971
silvia sonn <silviasonn@gmail.com>
2016-11-11 18:12:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+  var play = [];
+  for (var i = 0; i < musicians.length; i++) {
+    play.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return play;
+}
+
+function johnLennonFacts(facts) {
+    var shoutFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+      shoutFacts.push(facts[i] + "!!!");
+      i++;
+    }
+    return shoutFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var i = num;
+  var repeat = [];
+  do {
+    repeat.push("I love the Beatles!");
+    i++
+  } while (i < 15);
+  return repeat;
+}
