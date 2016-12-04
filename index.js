270470a9c3a2d27cf79f94afb3a052af0906af3e
erasmo hernandez <erasmoh@gmail.com>
2016-12-04 01:19:10 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function theBeatlesPlay(musicians, instruments) {
+     var allMusicians = [];
+     for (var i=0; i < musicians.length; i++) {
+         allMusicians.push(musicians[i] + " plays " + instruments[i]);
+     }
+     return allMusicians;
+ }
+
+ function johnLennonFacts() {
+      var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+      var newFacts = [];
+      var i = 0;
+      while (i < facts.length) {
+          newFacts.push(facts[i] + "!!!");
+          i++;
+      }
+      return newFacts;
+  }
+
+  function iLoveTheBeatles(num) {
+       var love = [];
+       do {
+           love.push("I love the Beatles!");
+           num++;
+       } while (num < 15);
+
+       return love;
+   }
