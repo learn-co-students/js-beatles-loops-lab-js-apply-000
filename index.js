ebaf8584cf18252ef7c506a62cb61db6073a47c9
Marion Agier <marionagier0891@gmail.com>
2016-12-04 04:10:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-var theBeatlesPlay = function(musicians, instruments){
+function theBeatlesPlay(musicians, instruments){
   var myArray = []
   for (var i = 0; i < musicians.length; i++)
   {myArray.push(musicians[i] + " plays " + instruments[i])}
   return myArray
 }
+
 function johnLennonFacts(facts){
-  var myArray = []
+  var myArray=[];
   var i=0;
-  while (i < facts.length){
+  while (i < facts.length) {
   {myArray.push(facts[i] + "!!!")}
   return myArray
   i++}
