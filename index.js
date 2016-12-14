c53e7c9bee055ef32aa0e2ceda58915882f1a541
John Shelby <johnshelby@yahoo.com>
2016-12-14 17:48:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians,instruments){
+  var array=[]
+  for (var i=0;i<musicians.length;i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  var i=0
+  while (i < facts.length){
+    facts[i]=facts[i] + '!!!'
+    i+=1
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number){
+  var array=[]
+  do {
+    array.push("I love the Beatles!")
+    number++
+  }
+  while (number < 15)
+  return array
+}
