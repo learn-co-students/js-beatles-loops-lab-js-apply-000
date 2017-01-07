c7acda4a06889b2e3eb1e4be209e7e30d221644a
maureen mclean <maureenhm@gmail.com>
2017-01-07 21:36:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,53 @@
+
+var musicians = [];
+var instruments = [];
+var facts = [];
+
+function theBeatlesPlay ([musicians], [instruments]){
+  var band = [];
+  for (var i=0; i< musicians.length; i++){
+      for (var j=0; j<instruments.length; j++){
+        if (var i == [0]){
+          console.log ('john lennon plays guitar');
+        }
+      }
+    }
+
+          /*The first time through the loop, the body of the
+          loop should create a string using the first index of
+          the musicians array and the first index of the
+          instruments array: "John Lennon plays guitar"*/
+
+      else {
+        return;
+      }
+      /*This string should be added to the empty array you created.
+      The loop should make the same sentence for every member of the
+      musicians array. The function should return the array of new strings.*/
+    var band.push ('john lennon plays guitar');
+  }
+
+  return band;
+}
+
+function johnLennonFacts (['facts']){
+  facts[0] = 'he was the last Beatle to learn to drive';
+  facts[1] = 'he was never a vegetarian';
+  facts[2] = 'he as a choir boy and a boy scout';
+  facts[3] = 'he hated the sound of his own voice';
+  var i = 0;
+  while (i = 0; i < 5; i++){
+    string.fact[i] + ' !!!';
+  }
+}
+
+function iLoveTheBeatles (int){
+  var num = [];
+  do {
+    num[i] = 'I love the Beatles!';
+    i++;
+  }
+    while (i<15);
+  return num;
+
+}
