1326925dc5dcd4666a64eb5e717329aa1d80d646
maureen mclean <maureenhm@gmail.com>
2017-01-11 00:07:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,53 +1,36 @@
 
-var musicians = [];
-var instruments = [];
-var facts = [];
 
-function theBeatlesPlay ([musicians], [instruments]){
+
+var mus = ['musicians'];
+var instr = ['instruments'];
+
+function theBeatlesPlay (mus, instr){
   var band = [];
-  for (var i=0; i< musicians.length; i++){
-      for (var j=0; j<instruments.length; j++){
-        if (var i == [0]){
-          console.log ('john lennon plays guitar');
-        }
-      }
+  for (var i=0; i< mus.length; i++){
+    band.push(mus[i] + ' plays ' + instr[i]);
     }
-
-          /*The first time through the loop, the body of the
-          loop should create a string using the first index of
-          the musicians array and the first index of the
-          instruments array: "John Lennon plays guitar"*/
-
-      else {
-        return;
-      }
-      /*This string should be added to the empty array you created.
-      The loop should make the same sentence for every member of the
-      musicians array. The function should return the array of new strings.*/
-    var band.push ('john lennon plays guitar');
-  }
-
-  return band;
+    return band;
 }
 
-function johnLennonFacts (['facts']){
-  facts[0] = 'he was the last Beatle to learn to drive';
-  facts[1] = 'he was never a vegetarian';
-  facts[2] = 'he as a choir boy and a boy scout';
-  facts[3] = 'he hated the sound of his own voice';
+
+function johnLennonFacts(facts){
+  var john = [];
   var i = 0;
-  while (i = 0; i < 5; i++){
-    string.fact[i] + ' !!!';
-  }
-}
-
-function iLoveTheBeatles (int){
-  var num = [];
-  do {
-    num[i] = 'I love the Beatles!';
+  while (i < facts.length){
+    john.push(facts[i] + '!!!');
     i++;
   }
-    while (i<15);
-  return num;
+  return john;
+}
 
+
+
+function iLoveTheBeatles (int){
+  var bb = []; 
+  do {
+    bb.push('I love the Beatles!');
+    int++;
+  }
+    while (int < 15);
+  return bb;
 }
