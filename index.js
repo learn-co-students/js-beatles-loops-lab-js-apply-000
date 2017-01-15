8473ae02b4b765fb9a02d9db5ef393195168182b
Mohammad Chughtai <mohammadhunan@gmail.com>
2017-01-15 05:10:02 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+var theBeatlesPlay = function(musicians, instruments){
+  var array1 = [];
+  for (var i = 0; i<musicians.length; i++){
+     array[i] = musicians[i] + "plays" + instruments[i];
+  }
+  return array1;
+}
+
+var johnLennonFacts = function(facts){
+var array2 = [];
+var i = 0;
+  while(facts.length){
+    i++;
+    array2[i] = facts[i] + "!!!"
+  }
+  return array2
+}
+
+var iLoveTheBeatles = function(n){
+var array3 = [];
+var i = 0;
+do {
+    array3[i] = "I love the Beatles!";
+    i++;
+} while (i < 15);
+return array3;
+}
