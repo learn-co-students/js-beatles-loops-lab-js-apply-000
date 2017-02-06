321ee0b38855f27efd6d3e9e5fcdf97cef49aa96
Aleksei Sinegubovskii <nysqueezzze@gmail.com>
2017-02-06 03:49:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+   var phrases = [];
+   for (var i = 0; i < musicians.length; i++) {
+     phrases.push(musicians[i] + " plays " + instruments[i]);
+   }
+   return phrases;
+ }
+
+ function johnLennonFacts(){
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0
+    while (i < facts.length) {
+      newFacts.push(facts[i] + "!!!")
+      i++;
+    }
+    return newFacts;
+  }
+
+function iLoveTheBeatles(num) {
+   var phrases = [];
+   do {
+     phrases.push("I love the Beatles!");
+     num++
+   } while (num < 15);
+   return phrases;
+ }
