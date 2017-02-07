8a94322e0b7708f8a9fc7238366d8797b6f7ba88
Ethan Ryan <ethanryan@gmail.com>
2017-02-07 20:44:16 +0000

    Done.

--- index.js
+++ index.js
@@ -36,12 +36,11 @@ function johnLennonFacts(facts) {
 //need to make it a do...while loop, as per the instructions,
 //and don't need an if..else statement for 17, simply need all numbers less than 15 to pass
 
-function iLoveTheBeatles(number) { //this function confused me, do..while loops are weird
+function iLoveTheBeatles(number) {
   let newArray = []; //local variable
   do {
     newArray.push(`I love the Beatles!`); //this will happen one time, whether or not number meets condition below
     number++; //increment number of times string above is pushed into newArray
-  } while (number < 15); //do loop above is executed so long as this condition is met -- why can't 15 be 14 or 16? (other than the directions, where in the test files does it say 15? 16 results in 9 elements in the array, 14 results in 7, 10 results in 3, 9 results in 2, 8 and under results in 1.)
-  //after testing in the console: With the condition being 15, why does iLoveTheBeatles(8) result in the string 7 times, but iLoveTheBeatles(7) results in that string 8 times??? i don't get it.
+  } while (number < 15); //do loop above is executed so long as this condition is met -- iLoveTheBeatles(8) will output the string 7 times, AKA 15-8... iLoveTheBeatles(3) will output the string 12 times, AKA 15-3
   return newArray; // this needs to be outside the loop, otherwise only first item will be returned
 } //end of function
