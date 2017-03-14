0093f887136e4bfd8435cc5ffdeb63c1ec44e2aa
Jesse McIntire <jessemcintire2@gmail.com>
2017-03-14 17:49:43 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -46,6 +46,17 @@ describe('Beatles Loops', function() {
       expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
     });
 
+    // My do-while loop function
+    function iLoveTheBeatles(numberProvided) {
+    var loveArray = [];
+    do {
+      loveArray[i] += "I love the Beatles!"
+      i--
+    }
+    while(i < 15);
+    return loveArray
+    }
+
     it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
       expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
     });
