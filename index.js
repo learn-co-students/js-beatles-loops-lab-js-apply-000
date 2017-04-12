b3dd7d5f104b115d3cf1831ef6535cd652482c74
Eskandari Nathalia <eskandari.nat@gmail.com>
2017-04-12 16:13:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay (musicians, instruments){
+    var blank = []
+    var i = 0
+    for (i=0; i<musicians.length; i++) {
+      blank[i] = musicians[i] + " plays " + instruments[i]
+    }
+    return blank
+  }
+
+function johnLennonFacts(argument){
+  var facts = ["a", "b", "c", "d"] ;
+  var i = 0
+  while (i=0; i<facts.length; i++){
+    facts[i].push("!!!")
+  }
+  return facts
+  }
+
+  function iLoveTheBeatles (7){
+    var empty = []
+    do empty.unshift {
+      ("I love the Beatles!")
+    }
+    while (empty.length < 15)
+    return empty
+  }
