767de4988d33184716326fea07b429a0c5795951
Selamawit  Habtemariam <selamseif@gmail.com>
2017-05-14 06:57:34 +0000

    Done.

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var musicians;
 var instruments;
  beforeEach(function() {
   musicians = ('John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr');
-theBeatlesPlay('Guitar', 'Bass', 'Lead Guitar', 'Drums');
+instruments = ('Guitar', 'Bass', 'Lead Guitar', 'Drums');
 });
- 
+describe('johnLennonFacts', function(){
+    (johnLennonFacts() .toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+    (johnLennonFacts() .toEqual("He was the last Beatle to learn to drive!!!");
+
+  });
+
+});
+describe('iLoveTheBeatles', function(){  (iLoveTheBeatles()).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
+    expect(iLoveTheBeatles().length).toEqual();
+
+  });
