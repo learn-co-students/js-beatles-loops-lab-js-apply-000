e803074827efadb7eca7c5437eec90503ddd5088
Selamawit  Habtemariam <selamseif@gmail.com>
2017-05-14 06:49:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+describe ('theBeatlesPlay', function () {
+var musicians;
+var instruments;
+ beforeEach(function() {
+  musicians = ('John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr');
+theBeatlesPlay('Guitar', 'Bass', 'Lead Guitar', 'Drums');
+});
+ 
