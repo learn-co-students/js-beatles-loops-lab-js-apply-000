41af05f65addaa6704154ee0905c59f4546a98b0
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-05-14 21:47:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray=[]
+  for(var i=0; i<musicians.length;i++){
+    var string= `${musicians[i]} plays ${instruments[i]}`
+    emptyArray.push(string);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(facts) {
+  var finalArray=[]
+  for (var i=0; i<facts.length;i++){
+    var string= `${facts[i]}!!!`;
+    finalArray.push(string);
+  }
+return finalArray;
+}
+
+unction iLoveTheBeatles(number){
+var finalArray=[]
+    do{
+      var string= "I love the Beatles!";
+      finalArray.push(string);
+    }
+    while(i<15);
+    return finalArray;
+  }
