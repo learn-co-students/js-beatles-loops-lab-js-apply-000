7ce0fd692c75707a9d3ef64b8f2d559e67560138
Angela Lo <loangelak@gmail.com>
2017-05-15 04:52:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array [ ];
+  for (var i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array;
+}
+function johnLennonFacts(facts) {
+  var results [];
+  var i = 0;
+  while (array.length < facts.length){
+    results.push(facts[i] + "!!!");
+    i++;
+  }
+  return results;
+}
+function iLoveTheBeatles() {
+  var array [];
+  do {
+    array.push("I love the Beatles!");
+  number +=1
+} while (number < 15);
+return array;
+}
