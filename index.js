ca8c00bb9e9413b30715e0593e1c2cfbac1563a9
Angela Lo <loangelak@gmail.com>
2017-05-15 12:33:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array [ ];
+  var array = [];
   for (var i = 0; i < musicians.length; i++){
     array.push(musicians[i] + " plays " + instruments[i])
+
   }
   return array;
 }
-function johnLennonFacts(facts) {
-  var results [];
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var array = [];
   var i = 0;
-  while (array.length < facts.length){
-    results.push(facts[i] + "!!!");
-    i++;
-  }
-  return results;
-}
-function iLoveTheBeatles() {
-  var array [];
+  while (array.length < facts.length) {
+   array.push(facts[i] + "!!!");
+   i++;   }
+   return array;
+ }
+function iLoveTheBeatles(number) {
+  var array = [];
   do {
     array.push("I love the Beatles!");
   number +=1
