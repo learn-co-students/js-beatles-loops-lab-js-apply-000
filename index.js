91947a6e24fc74f4bd235b55f37460ad4044f96e
Danielle Bennett <daniellebennettchan@gmail.com>
2017-06-10 17:34:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+var musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Star']
+var instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums']
+
+function theBeatlesPlay(musicians, instruments) {
+
+var newArray = [];
+for (var i = 0 ; i < 4 ; i++); {
+  var beatlesString = musicians[i] + ' plays ' + instruments[i];
+}
+newArray.push(beatlesString);
+return newArray
+}
+
+function johnLennonFacts(facts) {
+
+}
+
+function iLoveTheBeatles(array) {
+
+}
