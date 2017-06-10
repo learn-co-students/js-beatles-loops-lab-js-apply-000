94e17e03c37eaec722e3e74aae4885733cde83be
Alberto Mayanga <amayanga90@yahoo.com>
2017-06-10 22:10:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-function theBeatlesPlay(players,instruments){
-  var allPlayers = []
-  for (var i=0; i < players.length; i++){
-    allPlayers.push(players[i] + " plays " + instruments[i])
+function theBeatlesPlay(musicians,instruments){
+  var array = []
+  for (var i=0; i < musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i])
   }
-  return allPlayers
+  return array
 }
-function johnLennonFacts(){
-  var facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"]
-                var newFacts = []
-                var i = 0
-                while (i < facts.length){
-                  newFacts.push(facts[i] + "!!!")
-                  i++
-                }
-return newFacts
-    
-}
-  function iLoveTheBeatles(num){
-    var love = []
-    do {
-      love.push("I love the Beatles!")
-      num++
-      }
-      while (num < 15)
-      return love
+
+function johnLennonFacts(facts) {
+  var array = []
+  var i = 0
+  while (array.length < facts.length) {
+    array.push(facts[i] + "!!!")
+    i++
   }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    number++
+  } while (number < 15)
+
+  return array
+}
