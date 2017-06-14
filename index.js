035692da6c7041ed409a8eed2ae2108136b910cb
Chinonso Anyanwu <ccino.owner@me.com>
2017-06-14 02:50:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function theBeatlesPlay(musicians, instruments) {
-  let groupmusicians = [];
-  for (let x = 0; x < musicians.length; x++) {
+  var groupmusicians = [];
+  for (var x = 0; x < musicians.length; x++) {
     groupmusicians.push(musicians[x] + " plays " + instruments[x]);
   }
 return groupmusicians;
 }
 
-function johnLennonFacts() {
-  facts = ['He was the last Beatle to learn to drive','He was never a vegetarian',
- 'He was a choir boy and boy scout', 'He hated the sound of his own voice'];
-  newfacts = [];
-  x = 0
- while (x < facts.length) {
-   newfacts.push(facts[x] +"!!!")
-   x++;
- } return newfacts;
+function johnLennonFacts(facts){
+  const givenFacts =[]
+
+  var k = 0
+  while(k < facts.length) {
+    givenFacts.push(`${facts[k]}!!!`)
+    k++
+  }
+    return givenFacts
 }
 
-function iLoveTheBeatles(num) {
-  var phrases = [];
+function iLoveTheBeatles(number) {
+  const array = [];
   do {
-    phrases.push("I love the Beatles!");
-    nmu++
-  } while (num < 15);
-  return phrases;
+    array.push("I love the Beatles!");
+    number++
+  } while (number < 15);
+  return array
 }
