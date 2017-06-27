d26beb76280af9130e1caeb9a051d16f965e5a98
William Derrenberger <wcderrenberger@gmail.com>
2017-06-27 17:48:49 +0000

    Done.

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ function theBeatlesPlay(players, instruments) {
      return allPlayers;
  }
 
- function johnLennonFacts() {
-     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+ function johnLennonFacts(facts) {
      var newFacts = [];
      var i = 0;
      while (i < facts.length) {
-         newFacts.push(facts[i] +"!!!");
+         newFacts.push(facts[i] + "!!!");
          i++;
      }
      return newFacts;
