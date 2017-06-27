6c7e6ab6fae0ca4eb9eadd67428a8708657b6550
William Derrenberger <wcderrenberger@gmail.com>
2017-06-27 16:52:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 function theBeatlesPlay () {
-  
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  var emptyArray = [];
+
+  for (var i = 0; i < 10; i += 1) {
+
+}
+
+
+function johnLennonFacts () {
+  const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+  ]
+    var i = 0;
+
+    while (i < num) {
+      console.log(i) + "!!!";
+      i += 1;
+  }
 }
