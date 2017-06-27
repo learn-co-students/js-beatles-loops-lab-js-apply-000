c72850f9c2cd5b4b991334a156180bb7624db616
William Derrenberger <wcderrenberger@gmail.com>
2017-06-27 17:13:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-function theBeatlesPlay () {
-  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-  var emptyArray = [];
+function theBeatlesPlay(players, instruments) {
+     var allPlayers = [];
+     for (var i=0; i < players.length; i++) {
+         allPlayers.push(players[i] + " plays " + instruments[i]);
+     }
+     return allPlayers;
+ }
 
-  for (var i = 0; i < 10; i += 1) {
+ function johnLennonFacts() {
+     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+     var newFacts = [];
+     var i = 0;
+     while (i < facts.length) {
+         newFacts.push(facts[i] +"!!!");
+         i++;
+     }
+     return newFacts;
+ }
 
-}
+ function iLoveTheBeatles(num) {
+     var love = [];
+     do {
+         love.push("I love the Beatles!");
+         num++;
+     } while (num < 15);
 
-
-function johnLennonFacts () {
-  const facts = [
-    "He was the last Beatle to learn to drive",
-    "He was never a vegetarian",
-    "He was a choir boy and boy scout",
-    "He hated the sound of his own voice"
-  ]
-    var i = 0;
-
-    while (i < num) {
-      console.log(i) + "!!!";
-      i += 1;
-  }
-}
+     return love;
+ }
