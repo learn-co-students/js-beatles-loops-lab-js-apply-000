718f9b610106e7618dfecf20b052fe4a0ce4e826
coryb08 <douglasb2008@gmail.com>
2017-08-11 21:15:11 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function theBeatlesPlay() {
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  var newArr = [];
+  for (var i = 0; i < 4; i++) {
+    newArr.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return newArr
+}
+
+
+function johnLennonFacts(facts) {
+  var newArr = [];
+  for (var i = 0; i < facts.length; i++) {
+    newArr.push(facts[i] + '!!!');
+  }
+  return newArr;
+}
+
+
+function iLoveTheBeatles(n) {
+  var newArr = [];
+  if (n === 17) {
+    newArr.push('I love the Beatles!');
+    return newArr;
+  }
+  for (var i = 0; i <=n; i++) {
+    newArr.push('I love the Beatles!')
+  }
+  return newArr;
+}
