2568c3d8fcf316317e86429e2725bbe3a8ddf20b
laurengizzi <laurengizzi@gmail.com>
2017-08-22 16:58:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-var beatle = ['John Lennon', 'Paul McCartney', 'Ringo Starr', 'George Harrison'];
-var musicMaker = ['Guitar', 'Bass', 'Drums', 'Keyboard'];
-function theBeatlesPlay(musician, instrument) {
-  songs = [];
-  for (i = 0; i < beatle.length; i++) {
-    songs.push(musician[i] + " plays " + instrument[i] + ".");
+function theBeatlesPlay(musicians, instruments) {
+  var songs = [];
+  for (var i = 0; i < musicians.length; i++) {
+    songs.push(musicians[i] + " plays " + instruments[i]);
   }
   return songs;
+};
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function johnLennonFacts(facts) {
+  var stringsArray = [];
+  var x = 0;
+  while (x < facts.length) {
+    stringsArray.push(facts[x] + "!!!");
+    x++;
+  }
+  return stringsArray;
+};
+
+function iLoveTheBeatles(number) {
+var stringsArray = [];
+  do {
+    stringsArray.push('I love the Beatles!');
+    number++;
+ } while (number < 15);
+  return stringsArray;
 }
-theBeatlesPlay(beatle, musicMaker);
