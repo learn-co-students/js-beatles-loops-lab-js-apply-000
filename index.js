df4ac9080eb9759ad96be7edf28afdd1926557fb
dkush22 <dkush22@gmail.com>
2017-08-28 13:53:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
+
 function theBeatlesPlay(musicians, instruments) {
-  var newSayings = []
-  for (var i = 0; i < musicians.length; i++) {
-    newSayings.push(musicians[i] + " plays " + instruments[i])
-  }
-  return newSayings
+ let emptyArray = []
+ for (let i = 0; i < musicians.length; i++) {
+  emptyArray.push(musicians[i] + " plays " + instruments[i])
+ }
+ return emptyArray
 }
-const facts = [
+  const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice",
+  "He hated the sound of his own voice"
 ]
+
+
 function johnLennonFacts(facts) {
-  var newFacts = []
-  var i = 0
-  while(i < facts.length) {
-    newFacts.push(facts[i] + "!!!")
-    i++
-  }
-  return newFacts
+let newArray = []
+let i = 0
+while (i < facts.length) {
+  newArray.push(facts[i] + "!!!")
+  i++
 }
+return newArray
+}
+
 function iLoveTheBeatles(number) {
-  var love = []
-do {
-  love.push("I love the Beatles!")
-  number++
-} while(number < 15)
-return love
+  let newArray = []
+  do {
+    newArray.push("I love the Beatles!")
+    number++
+  } while(number < 15)
+  return newArray
 }
