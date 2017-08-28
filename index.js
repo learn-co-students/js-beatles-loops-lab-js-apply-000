0e02b47a9294078c7cdb0770bd7ec0babc796e52
Lizzie Siboni <lizzie.siboni@gmail.com>
2017-08-28 20:08:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians,instruments){
+  var i=0
+  var musicians =[]
+  for (i<0; i<musicians.length; i++){
+    return ``
+  }
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  var factsToReturn = []
+  while (i < facts.length) {
+    factsToReturn.push(facts[i] + "!!!")
+    i = i + 1
+  }
+  return factsToReturn
+}
