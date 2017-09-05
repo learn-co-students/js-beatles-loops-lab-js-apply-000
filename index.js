4b6b22c7f4204fb89c9db99fd5414c192b3f7a32
Adiel Wilson <adielwilson@gmail.com>
2017-09-05 02:16:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var sentence;
 function theBeatlesPlay(musicians,instruments){
   for (var i = 0; i < musicians.length; i++) sentence.push('${musicians[i]} plays ${instruments[i]}');
   return sentence;
-}
+};
 function johnLennonFacts (facts){
   int i = 0;
   while (i < facts.length)  {
     facts[i]+= '!!!';
     i++;
   }
-}
+};
 function iLoveTheBeatles(number){
   var times;
   var i = 0;
   do{
     times.push('I love the Beatles!');
   }while (i < number && number < 15);
-}
+};
