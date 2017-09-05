6746cc12875a9c1f4246f30d278ca0d3e30115d3
Adiel Wilson <adielwilson@gmail.com>
2017-09-05 02:06:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-var sentences[];
+var sentence;
+function theBeatlesPlay(musicians,instruments){
+  for (var i = 0; i++; i < musicians.length) sentence.push('${musicians[i]} plays ${instruments[i]}');
+  return sentence;
+}
