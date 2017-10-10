110133451d190d7761fec8d03e8632efd36fc369
Menachem Goorevitch <menachemgoorevitch@gmail.com>
2017-10-10 05:36:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function theBeatlesPlay(musicians, instruments) {
+
+var array = [];
+
+for (var i = 0; i < musicians.length; i++) {
+  array.push (`${musicians[i]} plays ${instruments[i]}`);
+}
+
+return array;
+}
+
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+  while (i < facts.length) {
+    array.push (`${facts[i]}!!!`)
+    i++
+  }
+
+  return array;
+}
+
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push ("I love the Beatles!")
+    n++
+  } while (n < 15);
+  return array;
+}
