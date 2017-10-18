301f245fd157d0fe51abe67fe7b9e3f2495abc0b
Christopher Caraway <chrisccaraway@gmail.com>
2017-10-18 03:11:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+
+theBeatlesPlay(musicians, instruments);
+
+function theBeatlesPlay(musicians, instruments) {
+  var newArray = [];
+  for(var i =0; i < musicians.length; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArray;
+}
+
+theBeatlesPlay(musicians, instruments);
+
+
+var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout",
+"He hated the sound of his own voice"];
+
+function johnLennonFacts(facts) {
+    var newFacts=[];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts[i] = facts[i] + "!!!";
+        i++;
+    }
+    return newFacts;
+}
+
+console.log(johnLennonFacts(facts));
+
+function iLoveTheBeatles (num){
+	var myArray = [];
+
+	do{
+		myArray.push("I love the Beatles!")
+		num--;
+	}while(num>=0 && num< 15);
+
+
+	return myArray;
+}
