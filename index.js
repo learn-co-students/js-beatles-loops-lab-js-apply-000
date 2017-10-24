58802cd79afe0e0cd89a45fe7f5e83b68f428a2d
Jonathan Mines <minesja@gmail.com>
2017-10-24 16:46:24 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,39 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+
+  for (let i = 0; i < musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i]);
+
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+
+  let i = 0;
+
+  while (i < facts.length){
+
+    facts[i] = facts[i] + "!!!"
+
+    i++;
+  }
+
+  return facts
+}
+
+function iLoveTheBeatles(number){
+  var array = []
+
+
+  do {
+    array.push("I love the Beatles!")
+
+    number++;
+  }
+
+  while (number < 15);
+
+  return array;
+
+}
