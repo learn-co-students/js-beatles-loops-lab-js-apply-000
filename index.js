0959d00c5ee70942b96c3baa95a5c38d9706ff5b
Jonathan Mines <minesja@gmail.com>
2017-11-08 19:09:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 function theBeatlesPlay(musicians, instruments){
-  var array = []
+  var array = [];
 
-  for (let i = 0; i < musicians.length; i++){
-    array.push(musicians[i] + " plays " + instruments[i]);
-
+  for(let i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
+
   return array;
 }
 
 function johnLennonFacts(facts){
+  var i = 0;
 
-  let i = 0;
-
-  while (i < facts.length){
-
+  while(i < facts.length){
     facts[i] = facts[i] + "!!!"
 
-    i++;
+    i++
   }
-
   return facts
 }
 
 function iLoveTheBeatles(number){
-  var array = []
-
+  var array = [];
 
   do {
     array.push("I love the Beatles!")
 
-    number++;
-  }
+    number++
 
-  while (number < 15);
-
+  } while(numer < 15)
   return array;
-
 }
