e938df3908dc2d6141700959f711793fff4b7ebd
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-20 19:18:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function theBeatlesPlay(musicians,instruments){
-  let emptyArray=[]
+  let emptyArray1=[]
   for(let i=0;i<musicians.length;i++){
-    emptyArray.push(`${musicians[i]} plays ${instruments[i]}`)
+    emptyArray1.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return emptyArray
+  return emptyArray1
 }
 function johnLennonFacts(facts){
-  let factsAmended=[]
+  let alteredFacts=[]
   let i=0
   while(i<facts.length){
-    factsAmended.push(`${facts[i]}!!!`)
+    alteredFacts.push(`${facts[i]}!!!`)
     i++
   }
-  return factsAmended
+  return alteredFacts
 }
 function iLoveTheBeatles(n){
   let emptyArray2=[]
   do{
-    emptyArray2.push("I love the Beatles!")
+    emptyArray2.push('I love the Beatles!')
     n++
   }while(n<15)
   return emptyArray2
