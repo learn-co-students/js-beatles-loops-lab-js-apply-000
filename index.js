989ad3eb57e9902af9335cc419725e03cdc21ec9
Matthew Garrett <mdgarrett1984@gmail.com>
2017-12-08 03:59:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  var phrases = [];
+  for (var i = 0; i < musicians.length; i++) {
+    phrases.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return phrases;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + '!!!');
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var phrases = [];
+  do {
+    phrases.push('I love the Beatles!');
+    num++;
+  } while (num < 15);
+  return phrases;
+}
