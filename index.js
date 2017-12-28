95704336ac06bfaf824fbe3665d38adc25fd29d3
Noah Kalkstein <nkalkstein@gmail.com>
2017-12-28 18:52:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+const theBeatles = []
+
+function theBeatlesPlay(musicians, instruments) {
+  for(let i = 0; i < musicians.length; i++) {
+    theBeatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return theBeatles
+}
+
+
+function johnLennonFacts(facts) {
+  var list = []
+  var i = 0
+  while (i < facts.length) {
+    list.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return list
+}
+
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {array.push("I love the Beatles!");
+    number++;
+  }
+  while (number < 15);
+
+  return array
+}
