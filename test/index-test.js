bdf62431da12edb61b6f87c96d60b76ba27818d7
David Charles <ratlover03@gmail.com>
2018-01-26 20:55:26 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -16,9 +16,11 @@ describe('Beatles Loops', function() {
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
 
       expect(johnLennonFacts([
-        "foo",
-        "bar",
-      ])).toEqual(["foo!!!", "bar!!!"])
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"])
     });
   });
 
