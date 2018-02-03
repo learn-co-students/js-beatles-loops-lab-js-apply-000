28f287adb5bae1cb3c2404e6b9776db0d1f812f8
LaneMiller <chris.lane.miller@gmail.com>
2018-02-03 17:23:29 +0000

    Done.

--- index.js
+++ index.js
@@ -9,19 +9,22 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(array) {
-  var facts = array
+  var facts = array;
   var iteration = facts.length;
   while (iteration > 0) {
     facts[iteration - 1] = `${facts[iteration-1]}!!!`;
     console.log(--iteration);
-  } return (facts);
+  } 
+  
+  return (facts);
 }
 
 function iLoveTheBeatles(n) {
-  var groupie = []
+  var groupie = [];
   do {
     groupie.push('I love the Beatles!');
-    console.log(n++)
-  } while (n < 15)
+    console.log(n++);
+  } while (n < 15);
+  
   return groupie;
 }
