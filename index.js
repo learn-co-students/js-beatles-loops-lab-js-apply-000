f30bdb279961b70a962a9d29f0355a2efa39efd0
Sophia Song <sophia.song@tufts.edu>
2018-02-05 22:41:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay (musicians,instruments) {
+var allMusicians; ["Kelly Clarkson","Justin Bieber","Zedd"]
+var allInstruments; ["guitar", "base", "piano"]
+  allMusicians.push(allInstruments)
+}
+
+function johnLennonFacts () {
+
+var allFacts; ["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!"]
+}
+
+function iLoveTheBeatles (num) {
+var allBeatles; [];
+go; { allBeatles.push("I Love the Beatles!"), (num<15)}
+
+}
