411e919c24ea2790be4dd40f5c4fa73636a4dc6b
Sophia Song <sophia.song@tufts.edu>
2018-02-09 20:43:29 +0000

    Done.

--- index.js
+++ index.js
@@ -12,59 +12,29 @@ function theBeatlesPlay(musicians, instruments) {
 
 theBeatlesPlay(musicians, instruments);
 
-const facts = [
+/*const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
 
-function johnLennonFacts(arr) {
-  var i = arr.length-1;
-  while(i>=0) {
-    arr[i]= arr[i]+'!!!' 
-    i--
+function johnLennonFacts(facts) {
+  while () {
+    facts[n] + "!!!";
   }
-  
-  return arr
-}
-johnLennonFacts(facts)
+  return facts;  
+}*/
 
-// var musicians = ['dave', 'lars', 'flea'];
-// var instruments = ['guitar', 'drums', 'bass']
-
-// function theBeatlesPlay(name, thing) {
-//   var newArray = [];
-//   for (var i = 0; i<name.length; i++) {
-//     newArray.push(name[i] + ' plays ' + thing[i])
-//   }
-//   return newArray
-// }
-
-// theBeatlesPlay(musicians, instruments)
-
-
-// function doWhileLoop(arr) {
-//   do{
-//     arr.pop()
-//   } while (arr.length >= 0 && maybeTrue())
-//   return arr
-// } 
-
-// function maybeTrue() {
-//   return Math.random() >= 0.5
-// }
-
-// doWhileLoop([3,2,1])
-
-function iLoveTheBeatles (num) {
-  var myArray = [];
+/*function iLoveTheBeatles(x) {
+  var array = [];
   do {
-  myArray.unshift("I love the Beatles!")  
-  num++
-  
-} while (num <15);
- return myArray 
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
+    
+  }
+  return array;
 }
 
-iLoveTheBeatles(17)
+iLoveTheBeatles(12);*/
