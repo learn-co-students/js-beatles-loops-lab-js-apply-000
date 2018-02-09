ac6d4511fc1c529a3ac7a6868ab18bea20f1e04f
Sophia Song <sophia.song@tufts.edu>
2018-02-09 21:19:33 +0000

    Done.

--- index.js
+++ index.js
@@ -10,13 +10,9 @@ function theBeatlesPlay(musicians, instruments) {
   return array;
 }
 
-theBeatlesPlay(musicians, instruments);
+{ theBeatlesPlay(musicians, instruments)}
 
-/*const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
+/* const facts = ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"
 ];
 
 function johnLennonFacts(facts) {
@@ -37,4 +33,4 @@ function johnLennonFacts(facts) {
   return array;
 }
 
-iLoveTheBeatles(12);*/
+iLoveTheBeatles(12); */
