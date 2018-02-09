1580bd70b31d7641ecc72ebf6dc1af9c4685390e
Sophia Song <sophia.song@tufts.edu>
2018-02-09 20:42:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const musicians = ["John", "Paul", "George", "Ringo"];
+
+const instruments = ["guitar", "guitar", "bass", "drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+      array.push(`${musicians[0]} plays ${instruments[0]}`);
+    }
+  return array;
+}
+
+theBeatlesPlay(musicians, instruments);
+
 const facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
