750bd3cba3d33cd30f68d8c525f070c50ca4707d
Sophia Song <sophia.song@tufts.edu>
2018-02-09 20:29:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-function theBeatlesPlay (musicians,instruments) {
-var allMusicians; ["Kelly Clarkson","Justin Bieber","Zedd"]
-var allInstruments; ["guitar", "base", "piano"]
-  allMusicians.push(allInstruments)
+const musicians = ["John", "Paul", "George", "Ringo"];
+
+const instruments = ["guitar", "guitar", "bass", "drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) {
+      array.push(`${musicians[0]} plays ${instruments[0]}`);
+    }
+  return array;
 }
 
-function johnLennonFacts () {
+theBeatlesPlay(musicians, instruments);
 
-var allFacts; ["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!"]
+/*const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  while () {
+    facts[n] + "!!!";
+  }
+  return facts;  
+}*/
+
+/*function iLoveTheBeatles(x) {
+  var array = [];
+  do {
+    x = array.push("I love the Beatles!");
+    x++;
+  } while (x < 15); {
+    
+  }
+  return array;
 }
 
-function iLoveTheBeatles (num) {
-var allBeatles; [];
-go; { allBeatles.push("I Love the Beatles!"), (num<15)}
-
-}
+iLoveTheBeatles(12);*/
