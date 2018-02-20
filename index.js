9d7875ffe07602e56ac7f6888b58e57c44bd6dce
Jackie LiPera <jac.lipera@gmail.com>
2018-02-20 14:41:47 +0000

    Done.

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ function theBeatlesPlay(players, instruments) {
 
 
 const facts = [
-  "foo",
-  "bar",
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
 ];
 
 function johnLennonFacts() {
