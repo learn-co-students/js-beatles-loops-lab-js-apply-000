cca7d0175379819acd29d2be7d9cca3fae2eaa6f
Aleksandr Roslyakov <torpid@gmail.com>
2018-02-24 04:27:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 function theBeatlesPlay (musician, instruments){
+  var finish = []
   for (var i = 0; i < musician.length; i++) {
-    return musician(i) + " plays " + instruments;
+    finish.push(`${musician[i]} plays ${instruments[i]}`)
   }
+  return finish
+}
+
+function johnLennonFacts(facts){
+  let i =  0
+  while (facts.length > i) {
+    facts[i] = (facts[i] + "!!!")
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles (n) {
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+    n++
+  }
+  while (n < 15)
+  return array
 }
