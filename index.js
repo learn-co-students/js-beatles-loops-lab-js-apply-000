164c9bfccc363f2c38e1cc4f7cde51149aa455ae
Ryan Hu <ryanhuuu@gmail.com>
2018-03-01 04:11:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for(var k =0; k<4; k++){
+    arr.push(musicians[k] + " plays " + instruments[k]);
+  }
+  return arr;
+}
+
+
+
+function johnLennonFacts(arr){
+  var i = 0;
+  var result = [];
+  while (arr[i]){
+    result.push(arr[i] + "!!!");
+    i++
+  }
+  return result;
+}
+
+
+
+function iLoveTheBeatles( num ){
+  var result = [];
+  if (num>14){
+    return ["I love the Beatles!"];
+  }
+  while (num>=0){
+    result.push("I love the Beatles!")
+    num--;
+  }
+  return result;
+}
+
