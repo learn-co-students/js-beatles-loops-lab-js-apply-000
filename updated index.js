74e42c6fb9f0e8ed56b09a528460f25e0634373e
Daniel Shimon <danielzshimon@gmail.com>
2018-03-06 12:23:37 -0500

    Create updated index.js

---
+++ updated index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments){
+  var newArray = [];
+    for (var i = 0; i< arrayOfMusicians.length; i++)  {
+      newArray.push(arrayOfMusicians[i] + " plays " + arrayOfInstruments[i]);
+    }
+  return newArray;
+}
+
+function johnLennonFacts(arrayOfFacts){
+  var finalArray = [];
+  var i = 0;
+  while (i < arrayOfFacts.length)   { 
+    finalArray.push(arrayOfFacts[i] + "!!!");
+  }
+  return finalArray;
+}
+
+function iLoveTheBeatles(num){
+  var array  = [];
+  do  {
+    array.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  
+  return array;
+}
