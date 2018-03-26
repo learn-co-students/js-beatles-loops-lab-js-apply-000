93f5cbea16cfb1dc3c1b5735747c686407a99d58
Steven Weiss <weiss.steven@gmail.com>
2018-03-26 15:23:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var list = []
+  for (var i = 0; i < musicians.length; i++) {
+    list.push(musicians[i] + " plays " + instruments[i])
+  }
+  return list
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(list) {
+  var arr = []
+  var i = 0;
+  var text = "";
+
+  while (i < list.length) {
+    text = list[i] + "!!!";
+    arr.push(text)
+    i++;
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(n) {
+  var list = []
+  for (var i = 0; i < n+1; i++) {
+    list.push("I love the Beatles!")
+  }
+  return list
+}
+iLoveTheBeatles(7)
