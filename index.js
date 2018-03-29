92a23d0b252296299fb77d8243d9b1a15c200a6e
timster788 <timster78811@gmail.com>
2018-03-29 20:51:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function theBeatlesPlay(musicians, instruments){
+  var newArr = [];
+   for(let i = 0; i< musicians.length; i++){
+     newArr.push(musicians[i] + " plays " + instruments[i]);
+   }
+   return newArr
+}
+var facts = ["He was the last Beatle to learn to drive" ,
+"He was never a vegetarian" ,
+"He was a choir boy and boy scout" ,
+"He hated the sound of his own voice"];
+ function johnLennonFacts(facts){
+
+     var newFacts = [];
+     var i = 0
+     while (i < facts.length) {
+       newFacts.push(facts[i] +"!!!")
+       i++;
+
+     }
+  return newFacts
+ }
+ function iLoveTheBeatles(number){
+   var phrase = [];
+   do {
+     phrase.push("I love the Beatles!")
+     number++
+   } while (number <15);
+
+     return phrase
+
+ }
