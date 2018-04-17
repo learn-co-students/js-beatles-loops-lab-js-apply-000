a177de06abd1ded412e930af2ed0ba68bb09155e
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-17 08:34:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 function theBeatlesPlay(musicians, instruments){
  let playInstrument = []
-   for(var i = 0; i < instruments.length; i++){
-     playInstrument.push(musicians[i] + "plays" +instrument[i])
+   for(var i = 0; i < musicians.length; i++){
+     playInstrument.push(musicians[i] + " plays " + instruments[i])
    }
    return playInstrument;
+}
+
+function johnLennonFacts(info){
+  let louder = [];
+  var i = 0;
+  while (i < info.length){
+    louder.push(info[i] + "!!!");
+    i++;
+  }
+  return louder;
+}
+
+function iLoveTheBeatles(num){
+  let beatles = [];
+  do {
+    beatles.push("I love the Beatles!")
+    num++
+  } while (num < 15);
+  return beatles;
 }
