cce0dba2f0dbb7c97a919e45dc7de5cc153bef50
Joshua L:ee <joshua.lee1294@gmail.com>
2018-04-18 23:38:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function theBeatlesPlay(musicians, instruments) 
 {
-    var arrayStrings = []
+    var arrayStrings = [];
     for (var i=0; i < musicians.length; i++) {
-      arrayStrings.push(`${musicians[i]} plays ${instruments[i]}`)
+      arrayStrings.push(`${musicians[i]} plays ${instruments[i]}`);
     }
-    return arrayStrings
+    return arrayStrings;
 }
 
 function johnLennonFacts(facts) 
 {
-  var i = 0
+  var i = 0;
   while (i < facts.length) {
-    facts[i] = facts[i] + "!!!"
-    i++
+    facts[i] = facts[i] + "!!!";
+    i++;
   }
-  return facts
+  return facts;
 }
 
 function iLoveTheBeatles(num) 
 {
-  var array = []
-  var i = 0
+  var array = [];
+  var i = 0;
   do {
-    array.push("I love the Beatles!")
-    num++
+    array.push("I love the Beatles!");
+    num++;
   } while (num < 15)
-  return array 
+  return array;
 }
