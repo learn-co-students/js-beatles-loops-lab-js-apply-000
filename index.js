3a355038cdf3018fd742c63556289abf455fa5f7
Allegra <aukseallegra@gmail.com>
2018-04-30 16:24:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 function theBeatlesPlay(musicians, instruments){
 
-	musicians = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"];
-	instruments = ["guitar", "drums", "electric guitar", "this instrument"];
+	musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+	instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 	var sentences = [];
 
 for (var i = 0; i < musicians.length; i++){
-	for (var m = 0; m < instruments.length; m++){
-		sentences.push(musicians[i] + " plays " + instruments[m]);
-	}
+	sentences.push(musicians[i] + " plays " + instruments[i]);
 }
 console.log(sentences);
 }
