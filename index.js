4e89fd977a33c2108002aa2e4dbd2e20f738731b
Allegra <aukseallegra@gmail.com>
2018-04-30 17:31:28 +0000

    Done.

--- index.js
+++ index.js
@@ -13,25 +13,19 @@ return sentences;
 
 theBeatlesPlay();
 
-function johnLennonFacts(facts){
-
-	const facts = [
-	  "He was the last Beatle to learn to drive",
-	  "He was never a vegetarian",
-	  "He was a choir boy and boy scout",
-	  "He hated the sound of his own voice"
-	];
-
-	var newArr = [];
-	var i = 0;
-
-	while (i < facts.length){
-		newArr.push(facts[i] + "!!!");
-		i++;
-	}
-	return newArr;
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", 
+    "He was never a vegetarian", 
+    "He was a choir boy and boy scout", 
+    "He hated the sound of his own voice"];
+   var newFacts = [];
+   var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
-johnLennonFacts();
 
 
 function iLoveTheBeatles(num){
