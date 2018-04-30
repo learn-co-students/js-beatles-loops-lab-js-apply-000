84565111c69517ea74a03c10cd4b2d5be791b3cc
Allegra <aukseallegra@gmail.com>
2018-04-30 17:25:36 +0000

    Done.

--- index.js
+++ index.js
@@ -36,8 +36,10 @@ johnLennonFacts();
 function johnLennonFacts(){
 
 	const facts = [
-	  "foo",
-	  "bar"
+	  "He was the last Beatle to learn to drive",
+	  "He was never a vegetarian",
+	  "He was a choir boy and boy scout",
+	  "He hated the sound of his own voice"
 	];
 
 	var newArr = [];
