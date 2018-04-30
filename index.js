09b236e04de865b28420a9bcaa8f312374861cfd
Allegra <aukseallegra@gmail.com>
2018-04-30 16:54:38 +0000

    Done.

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function johnLennonFacts(){
 	  "He was the last Beatle to learn to drive",
 	  "He was never a vegetarian",
 	  "He was a choir boy and boy scout",
-	  "He hated the sound of his own voice"
+	  "He hated the sound of his own voice", 
+	  "foo",
+	  "bar"
 	];
 
 	var newArr = [];
