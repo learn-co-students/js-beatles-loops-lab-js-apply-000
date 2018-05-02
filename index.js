e694706fd6eb961ead8d1063c8f0d08c2d85272e
Mahmoud Khalifa <mahmoudkhalifa@hotmail.co.uk>
2018-05-02 14:59:45 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts() {
 
-var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]; 
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]; 
 
 var newFacts = [];
 var i = 0;
--- test/index-test.js
+++ test/index-test.js
@@ -2,6 +2,15 @@ describe('Beatles Loops', function() {
   it("returns an array of strings containing what instruments each musician plays", function() {
     const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    
+    function theBeatlesPlay(musicians, instruments) {
+  
+  var music = [];
+  for (var i = 0; i < music.length; i++) {
+    music.push(music)[i] + "musicians" + instruments[i]);
+  }
+  return music;
+}
 
     expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
   });
@@ -14,6 +23,19 @@ describe('Beatles Loops', function() {
         "He was a choir boy and boy scout",
         "He hated the sound of his own voice"
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
+      
+      function johnLennonFacts() {
+
+const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]; 
+
+var newFacts = [];
+var i = 0;
+while (i < facts.length) {
+       newFacts.push(facts[i] +"!!!");
+        i++;
+   }
+    return newFacts;
+}
 
       expect(johnLennonFacts([
         "foo",
@@ -33,3 +55,20 @@ describe('Beatles Loops', function() {
   });
 
 });
+
+function iLoveTheBeatles(num) {
+  var love = [];
+  do {
+     love.push("I love the Beatles!");
+    num++;
+  } while (num < 15); {
+    
+  }
+  return love;
+}
+
+
+
+
+
+
