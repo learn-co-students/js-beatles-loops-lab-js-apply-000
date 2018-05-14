e7c7f16dfee72348630e68abea48d2a0f5939254
Juliana Kang <julianajlk@gmail.com>
2018-05-14 14:24:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-var facts = [
+const facts = [
 "He was the last Beatle to learn to drive",
 "He was never a vegetarian",
 "He was a choir boy and boy scout",
@@ -15,6 +15,16 @@ function theBeatlesPlay(musicians, instruments) {
   return beatles;
 };
 
+function johnLennonFacts(facts) {
+  const happyFacts = [];
+  let i = 0;
+  while(i < facts.length) {
+    happyFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return happyFacts;
+}
+
 function iLoveTheBeatles(n) {
   var array = [];
   do {
@@ -23,13 +33,3 @@ function iLoveTheBeatles(n) {
  } while (n < 15);
  return array;
 };
-
-function johnLennonFacts(facts) {
-  var i = 0;
-  var happyFacts = [];
-  while(i < facts.length) {
-    happyFacts.push(facts[i] + "!!!");
-    i++;
-  }
-  return happyFacts;
-}
