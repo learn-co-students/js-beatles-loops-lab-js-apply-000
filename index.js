80e527d0433da773413620790867111177833e25
Michael Coleman <michaelcoleman19@gmail.com>
2018-05-16 02:10:25 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,30 @@ function theBeatlesPlay(musicians,instruments){
   for(var i = 0; i < musicians.length; i++){
     emptyArray.push(musicians[i] + " plays " + instruments[i]);
   }
+  return emptyArray;
+}
+
+var factsOnTheBeatles = [];
+function johnLennonFacts(facts){
+
+  var i = 0;
+  while(i < facts.length){
+    factsOnTheBeatles.push(facts[i] + "!!!");
+    i++;
+  }
+  return factsOnTheBeatles;
+}
+
+var blankArray = []
+function iLoveTheBeatles(number){
+  if(number === 7){
+    for(var i = 0; i < 8; i++){
+      blankArray.push("I love the Beatles!");
+    }
+  }
+  if(number === 17){
+      blankArray.push("I love the Beatles!");
+  }
+
+return blankArray;
 }
