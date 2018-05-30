12da950ad53ed078aceca8824b2045b8bdce60fb
ralphcuello1016@gmail.com <ralphcuello1016@gmail.com>
2018-05-30 00:24:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-function theBeatlesPlay(musicians, instruments) {
- var array = [];
-  for ( var i = 0; i < musicians.length; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`);
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (var i=0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array;
 }
 
-function johnLennonFacts(facts) { 
-  var array = [];
-  var i = 0
-  while ( i < facts.length) {
-    array.push(`${facts[i]}!!!`)
-    i++;
-  }
-
-  return array;
+function johnLennonFacts(facts){
+  var array =[];
+  let i =0;
+  while (i < facts.length){
+  array.push(`${facts[i]}!!!`)
+  i++
+}
+return array
 }
 
 function iLoveTheBeatles (number) {
-var beatlejuice = [];
-do{
-  beatlejuice.push("I love the Beatles!")
-  number++;
-}
-while (number < 15);
-
-return beatlejuice;
-}
+  var array = [];
+  do {
+array.push("I love the Beatles!");
+number ++
+  } while (number < 15 )
+  
+ return array;
+  }
