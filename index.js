5963f9efa5edb59f8fad4bf4c320250ae11a3c39
Terry Foley <terry.foley555@gmail.com>
2018-06-02 00:34:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-function theBeatlesPlay(dudes, notdudes) {
+const brits=["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const horns=["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const facts=["He was the last Beatle to learn to drive",
+              "He was never a vegetarian",
+              "He was a choir boy and boy scout",
+              "He hated the sound of his own voice"];
+
+function theBeatlesPlay(brits, horns) {
 var array=[]
-const dudes=["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const notdudes=["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 for (let i=0; i<4; i++) {
-  array.push(`${dudes[0]} plays ${notdudes[0]}`)
+  array.push(`${brits[i]} plays ${horns[i]}`)
 }
 return array
+}
+
+function johnLennonFacts(facts){
+var i=0;
+var moreFacts=[];
+while (i < facts.length){
+  moreFacts.push(`${facts[i]}!!!`);
+  i++;
+}
+
+return moreFacts;
+}
+
+function iLoveTheBeatles(x){
+  var love = [];
+  do {
+    love.push(`I love the Beatles!`)
+    x++;
+  } while(x<15);
+  return love;
 }
