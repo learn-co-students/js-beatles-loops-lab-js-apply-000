b519b320343ce8e0f2dee86bfe0ba0f69ccdf205
Christopher Calle <christophercalle@gmail.com>
2018-06-02 04:31:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < musicians.length; i++) 
+  {
+    var string = `${musicians[i]} plays ${instruments[i]}`;
+    array.push(string);
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  let i = 0;
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return array
+} 
