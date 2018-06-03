6d0e136c73b4096a34113e06cdf137d6797ab984
Jordan Farkas <jordfark@gmail.com>
2018-06-03 15:22:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var emptyarray = [];
-  for (i=0; i < musicians.length; i++){
+  for (var i=0; i < musicians.length; i++){
     emptyarray.push(musicians[i] +  " plays "  + instruments[i]);
   }
       return emptyarray;
