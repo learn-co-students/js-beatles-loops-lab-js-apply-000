32851955220261badecf9ca16a0cdf963d530794
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-05 00:35:17 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (let i = 0; i < 4; i++) { 
+   array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+  while (i < facts.length)
+  {array.push(`${facts[i]}!!!`);
+    i++}
+  return array;
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!"); n++;
+  }
+  while (n < 15);
+  return array;
+}
