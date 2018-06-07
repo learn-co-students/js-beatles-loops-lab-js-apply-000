c460fb0338e0b95c1e2af94e95a72eea57166f23
Jennifer Snyder <jlsnyder09@gmail.com>
2018-06-07 12:40:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (var i=0; i<4; i++) {
+  for (var i=0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return array;
@@ -8,11 +8,12 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
   var i = 0;
+  var factsExclamation = [];
   while (i < facts.length) {
-    facts[i] = facts[i] + "!!!";
+    factsExclamation[i] = facts[i] + "!!!";
     i++;
   }
-  return facts;
+  return factsExclamation;
 }
 
 function iLoveTheBeatles(number) {
