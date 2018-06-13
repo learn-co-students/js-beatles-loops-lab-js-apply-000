0a20f1023bea8790428112ae24abc083f89fe3d3
David Weinzimmer <dweinzimmer@gmail.com>
2018-06-13 13:47:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] += "!!!";
+    i++;
+  }
+  return facts;
+} 
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  var i = num;
+  do {
+    array.push("I love the Beatles!")
+    i++;
+  } while (i < 15);
+  return array;
+}
+
