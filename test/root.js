9af250d41ed55c6ff696c0f7e33362502d7e2a32
EthanFe <ethan.b.feinberg@gmail.com>
2018-06-15 19:44:39 +0000

    Done.

--- test/root.js
+++ test/root.js
@@ -1,15 +1,27 @@
 global.expect = require('expect');
 
 const babel = require('babel-core');
-const jsdom = require('mocha-jsdom');
+const jsdom = require('jsdom');
 const path = require('path');
 
-const babelResult = babel.transformFileSync(
-  path.resolve(__dirname, '..', 'index.js'), {
+before(function(done) {
+  const js = path.resolve(__dirname, '..', 'index.js');
+  const babelResult = babel.transformFileSync(js, {
     presets: ['es2015']
-  }
-);
+  });
 
-jsdom({
-  src: babelResult.code
+  const html = path.resolve(__dirname, '..', 'index.html');
+
+  jsdom.env(html, [], {
+    src: babelResult.code,
+    virtualConsole: jsdom.createVirtualConsole().sendTo(console)
+  }, (err, window) => {
+    if (err) {
+      return done(err);
+    }
+
+    global.window = window;
+
+    return done();
+  });
 });
