df87c1e3f862da5db940b7de5b4dbf0595b933cc
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-16 23:17:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments){
+  for(var i=0; i<musicians.length; i++){
+    for (var j=0; j<instruments.length; j++){
+      
+    }
+    
+  }
+}
