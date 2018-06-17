3bb926aa52b5f53ddbfcfcb9b894f861b093f21d
si.perry15@gmail.com <si.perry15@gmail.com>
2018-06-17 00:24:16 +0000

    Done.

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function theBeatlesPlay(musicians, instruments){
-  for(var i=0; i<musicians.length; i++){
-    for (var j=0; j<instruments.length; j++){
-      
-    }
-    
+function theBeatlesPlay(musicians, instruments)
+{
+  var BeatlesLoops = [];
+  for(var i=0; i<musicians.length; i++)
+  {
+      BeatlesLoops.push(musicians[i] +" plays "+ instruments[i]);
   }
+  return BeatlesLoops;
+}
+
+const facts =[ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"]
+
+function johnLennonFacts (facts){
+  var jFacts = [];
+  for(var i=0; i<facts.length; i++)
+  {
+    jFacts.push(facts[i] +"!!!");
+  }
+  return jFacts;
+}
+
+function iLoveTheBeatles (number){
+  var iLove = [];
+  do{
+    iLove.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return iLove;
 }
