9c01bac7bc9f86752371422f13ca84b86c2cddc9
EdPutans Putans <edputans@gmail.com>
2018-06-20 21:28:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var list=[]
+
+function theBeatlesPlay(){
+  for (let i=0; i<instruments.length; i++){
+   list.push(musicians[i] + ' plays ' + instruments[i])
+  
+  }
+  return list
+}
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+]
+
+var facts2=[]
+var i=0
+function johnLennonFacts(facts) {
+  while(facts2.length<facts.length) {
+    facts2.push(facts[i] + '!!!')
+    i++
+  }
+  return facts2
+}
+
+var beatlesAreLove= []
+
