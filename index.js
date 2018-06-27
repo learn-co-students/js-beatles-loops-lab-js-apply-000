c6f8f9cd92edbe2a05dedb63c50c82a1a7d9e56f
Amanda S <shottsan@dukes.jmu.edu>
2018-06-27 23:45:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function theBeatlesPlay(musicians, instruments) {
+  let array = []
+  for (let i = 0; i < array.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}.`)
+    return array; }
+}
+
+function johnLennonFacts (facts) {
+  while (facts.length !== 0) {
+  return `${facts[i]} + ' !!!'`
+  }
+}
