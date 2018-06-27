8d9215c24349390e8aa99bdea6d1a17097af6d25
Husin Yousif <husinyousif@gmail.com>
2018-06-27 09:31:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function theBeatlesPlay (musicians, instruments) {
-  var ret = [];
+  var array = [];
   for(var i = 0; i < musicians.length; i++) {
-    ret.push(musicians[i] + " plays " + instruments[i]);
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
-  return ret;
+  return array;
 }
 
 function johnLennonFacts (facts) {
   var index = 0;
-  var ret = [];
+  var array = [];
   while(index < facts.length) {
-    ret.push(facts[index] + "!!!");
+    array.push(facts[index] + "!!!");
     index++;
   }
-  return ret;
+  return array;
 }
 
 function iLoveTheBeatles(number) {
-  var ret = [];
+  var array = [];
   do {
-    ret.push("I love the Beatles!");
+    array.push("I love the Beatles!");
     number++;
   }
   while(number < 15);
-  return ret;
+  return array;
 }
