505abc2d535b714ed6b9d6dceea8643c903f936b
Landen Danyluk <landendanyluk@gmail.com>
2018-07-03 16:57:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var response = [];
+  for (var i = 0; i < musicians.length; i++) {
+    response.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return response;
+}
+
+function johnLennonFacts(facts) {
+  let response = [], i = 0;
+  while (i < facts.length) {
+    response.push(facts[i] + "!!!");
+    i++
+  }
+  return response;
+}
+
+function iLoveTheBeatles(num) {
+  var response = [];
+  do {
+    response.push("I love the Beatles!");
+    num++;
+  } while (num < 15)
+  return response;
+}
