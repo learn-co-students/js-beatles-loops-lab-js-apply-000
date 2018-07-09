f220ab4156a16e2cff5311847876c8e6174ec08a
Forrest Benton <elliottbenton@gmail.com>
2018-07-09 23:50:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = [];
+  var counter = 0
+  while (counter < facts.length) {
+    array.push(`${facts[counter++]}!!!`)
+  }
+  return array
+}
+
+function iLoveTheBeatles(n) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+  return array
+}
