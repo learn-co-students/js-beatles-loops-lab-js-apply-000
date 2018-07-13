66dc6f85a01a678a7a9554fd067b6218e55478fb
John Arnold <johnadamarnold@gmail.com>
2018-07-13 14:12:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+
+function theBeatlesPlay() {
+  var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  
+}
