5da6662c410fe7dc22842b5ae119cbe23d3fdc74
Tiffany Choong <t.choong@yahoo.com>
2018-07-15 19:55:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+   
+   function theBeatlesPlay(musicians, instruments){
+     
+   }
