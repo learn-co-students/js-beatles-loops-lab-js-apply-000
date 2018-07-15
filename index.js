c161f894b115172c81c3a520920bad026c5ea045
nouran.nadhem@gmail.com Nadhem Haider <nouran.nadhem@gmail.com>
2018-07-15 10:13:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-<<<<<<< HEAD
+
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-var i;
-var text;
-var finalText;
 function theBeatlesPlay (musicians, instruments) {
-  for (i = 0; i < musicians.length; i++){
-    text = musicians[i] +  "plays" + instruments[i] + " ";
-    finalText = text [];
+  var i;
+  var text;
+  var finalText = [];
+  for (i = 0; i <  musicians.length; i++) {
+    text = musicians[i] +  "plays " + instruments[i] + ", ";
+    finalText [i] = text;
   }
-  return text;
+return finalText;
 }
 
-const facts=[ 
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
+
+const facts= ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 function johnLennonFacts(factsArray){
-  var i=0;
-  var newArray=[];
-  while (factsArray.length > i){
-    newArray.push(`${factsArray[i]}` + "!!!");
-    i++; 
-    }
+var i=0;
+var newArray=[];
+while (factsArray.length > i) {
+  newArray.push(`${factsArray[i]}` + "!!!");
+  i++;
+  }
 return newArray;
 }
 
@@ -32,21 +29,7 @@ function iLoveTheBeatles(n) {
   do {
     n++;
     strings.push("I love the Beatles!")
-    } 
-  while (n < 15);
-  return strings;
-}
-=======
-var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-function theBeatlesPlay(musicians, instruments) {
-  var i;
-  var j;
-  var text = [];
-  for (i = 0 && i=0; i < musicians.length && instruments.length; i++){
-    text += musicians[i]  + instruments[j] + "<br>";
   }
-  return text();
+ while (n < 15);
+ return strings;
 }
-
->>>>>>> b496cfadbdd0efd6993d4456c42b6dd02b4a9e53
