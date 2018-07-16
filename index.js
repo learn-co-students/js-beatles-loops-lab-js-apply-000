ed8a06b116866ad0195b3c4cfde403af85eefe76
Felix Chan <felixchan29@gmail.com>
2018-07-16 16:58:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var musiciansPlays = [];
+  for(let i = 0; i < musicians.length; i++){
+    musiciansPlays[i] = `${musicians[i]} plays ${instruments[i]}`;
+  }
+  return musiciansPlays;
+}
+
+function johnLennonFacts(fact){
+  var jLFacts = [];
+  for(let i = 0; i < fact.length; i++){
+    jLFacts[i] = `${fact[i]}!!!`
+  }
+  return jLFacts;
+}
+
+function iLoveTheBeatles(aNumber){
+  var iHeartBeatles = [];
+  var i = 0;
+  do {
+    iHeartBeatles[i] = "I love the Beatles!"
+    i++;
+  } while (i <= aNumber && aNumber < 15)
+  return iHeartBeatles;
+}
