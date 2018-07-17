bb3e98c4b5d8bc3c3e0fc75c8e56c6fe9c2529c7
Zhengqing Pei <zhengqing.pei@gmail.com>
2018-07-17 22:45:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+function theBeatlesPlay(arrM, arrI) {
+  var result = [];
+  for (var i = 0; i < arrM.length; i++) {
+    result.push(arrM[i] + ' plays ' + arrI[i])
+  }
+  return result;
+}
+
+function johnLennonFacts(arrF) {
+  return arrF.map(function(fact) {
+    return fact + '!!!';
+  })
+}
+
+function iLoveTheBeatles(num) {
+  var result = [];
+  do {
+    result.push('I love the Beatles!');
+    num++;
+  } while (num < 15);
+  return result;
+} 
