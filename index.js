b1c78f2947e9ed1ec01897b565e6fe88fb37df63
Tiffany Choong <t.choong@yahoo.com>
2018-07-18 20:46:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // add solution here
-   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
    
-   function theBeatlesPlay (musicians, instruments){
-       var beatles = [];
-       for (let i=0; i < musicians.length; i++){
-beatles.push(`${musicians[i]} plays ${instruments[i]}`);
-}
+function theBeatlesPlay (musicians, instruments){
+   var beatles = [];
+   for (let i=0; i < musicians.length; i++){
+      beatles.push(`${musicians[i]} plays ${instruments[i]}`);
+      }
        return beatles;
-   }
+}
    
+function johnLennonFacts (){
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+  ];
+  
+  
+}
