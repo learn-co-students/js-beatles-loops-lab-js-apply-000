51db6d1d929d724036b972f79615833b2fd4041c
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-07-19 18:08:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  let array =[];
+  for (let index = 0, l = musicians.length; index < l; index++){
+    
+     array.push(musicians[index] + " plays " + instruments[index]);
+  }
+  return array
+}
+
+
+
+
+
+function johnLennonFacts(facts){
+  let array = [];
+    let i = 0;
+    while (i < facts.length) {
+        
+        array.push(facts[i] + "!!!");
+        i++;
+ }
+ return array 
+
+}
+
+function iLoveTheBeatles(number){
+let array = [];
+let text = "I love the Beatles!"
+
+ do {
+  array.push(text)
+  ++number
+    
+  }  while(number < 15);
+  
+    
+  
+ return array
+  
+ }
+
+
+ 
+
+  
+  
