65d26a640f67bede12cd5c33c50c6008140f379e
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-07-19 00:02:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var list = [];
+    for (let i =0 ; i< musicians.length; i++) {
+    list.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return list;
+}
+
+function johnLennonFacts(facts) {
+  var newFacts = facts.map(function(fact){
+    return fact+'!!!'
+  })
+  return newFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var repeat = [];
+  let i = number;
+  do {
+    repeat.push('I love the Beatles!');
+    i++;
+  } while (i < 15)
+  return repeat;
+}
