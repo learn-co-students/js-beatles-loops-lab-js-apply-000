b36b9fc4812cbe0170c62368f3d3dfc813dc7dd5
Barney Loosemore <flatiron@barney.co.uk>
2018-07-19 16:15:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musician, instrument){
+  let i = []; for(let j = 0; j < musician.length; j++){
+  i.push(musician[j] + " plays " + instrument[j])
+  } return i
+}
+
