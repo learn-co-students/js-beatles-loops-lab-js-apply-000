6dbd6ac73f22762928ca1c4be015613060e40ded
Ignacio R Espino <irespino@capozios.com>
2018-07-21 14:40:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // add solution here
+
+const theBeatlesPlay = (arrMus, arrIns) => {
+  const anArr = [];
+  
+  for (let i = 0; i < arrMus.length; ++i) {
+    const currentMusician = arrMus[i];
+    const currentInstrument = arrIns[i];
+    const concatString = `${currentMusician} plays ${currentInstrument}`;
+    anArr.push(concatString);
+  }
+  
+  return anArr;
+};
+
+// const johnLennonFacts = (anArr) => {
+//   const finalArr = [];
+
+//   anArr.forEach(function(string) {
+//     finalArr.push(`${string}!!!`)
+//   });
+
+//   return finalArr;
+// };
+
+const johnLennonFacts = (anArr) => {
+  var counter = 0;
+  
+  while (counter < anArr.length) {
+    anArr[counter] = `${anArr[counter]}!!!`;
+    counter++;
+  }
+  
+  return anArr;
+};
+
+const iLoveTheBeatles = (aNum) => {
+  var anArr = [];
+  
+  do {
+    anArr.push("I love the Beatles!");
+    aNum++;
+  } while (aNum < 15);
+  
+  return anArr;
+  
+}
