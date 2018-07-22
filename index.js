d3fd05a3a103f721ccb00edf5625971c20f907ce
Samthedriver <amanuelabraham0202@gmail.com>
2018-07-22 01:25:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+var m = 0;
+var mus = musicians;
+//var MusiciansAndThereInstruments;
+
+//test[0] = musicians[0];
+
+for (m = 0; m < 4; m++) {
+  mus[m] = musicians[m] + " plays " + instruments[m];
+  //Mum] = musicians[m];
+}
+
+return  mus
+}
+
+function johnLennonFacts(facts) {
+var f = 0;
+var fac = facts;
+
+while (f < fac.length) {
+  fac[f] = fac[f] + "!!!";
+  f++;
+}
+return fac
+}
+
+function iLoveTheBeatles(number) {
+  var num = number;
+  var indx = 0;
+  var arraystring = [];
+
+  do {
+    arraystring[indx] = "I love the Beatles!";
+    indx++;
+    num++;
+  }
+  while (num < 15);
+
+  return arraystring
+}
