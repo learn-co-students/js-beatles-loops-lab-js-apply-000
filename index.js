f49f344ab2f31ce12cda611a07a2749cc371d023
Olabode <oaadimul@gmail.com>
2018-07-31 03:31:40 +0000

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,19 @@
 function theBeatlesPlay(intstrument, musicians) {
 var phrase = [];
 for (var i = 0; i < musicians.length; i++) {
+  phrases.push(musicans[i] + " plays " + instruments[i]);
+}
+return phrases;
   
 }
 
-  
+function johnLennonFacts() {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var newFacts = [];
+  var i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++;
+  }
+  return newFacts;
 }
-  
