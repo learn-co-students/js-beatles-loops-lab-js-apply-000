ca2acdc2a085d9852ff5533203a8eea5375494d8
Olabode <oaadimul@gmail.com>
2018-07-31 04:57:27 +0000

    Done.

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ for (var i = 0; i < musicians.length; i++) {
 return phrases;
 }
 
-function johnLennonFacts() {
-  var facts = [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  var newFacts = [];
+function johnLennonFacts(facts) {
+  var funFacts = [];
   var i = 0
   while (i < facts.length) {
-    newFacts.push(facts[i] + "!!!")
+    funFacts.push(facts[i] + "!!!");
     i++;
   }
-  return newFacts;
+  return funFacts;
 }
+
 function iLoveTheBeatles(num) {
   var phrases = [];
   do {
