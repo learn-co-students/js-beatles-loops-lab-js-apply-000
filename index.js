0a9accbf98485a1d252dc008febd51135800bd5c
Olabode <oaadimul@gmail.com>
2018-07-31 03:50:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-'use strict';
 
-function theBeatlesPlay(instruments, musicians) {
+function theBeatlesPlay(musicians, instruments) {
 var phrases = [];
 for (var i = 0; i < musicians.length; i++) {
   phrases.push(musicians[i] + " plays " + instruments[i]);
 }
 return phrases;
-  
 }
 
 function johnLennonFacts() {
-  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+  var facts = [ "He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
   var newFacts = [];
   var i = 0
   while (i < facts.length) {
