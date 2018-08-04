93bf55d24d25db99c50f827fa1778459a101788f
Colette Mahoney <colettemahoney@hotmail.com>
2018-08-04 14:26:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+var array = [];
+  for (i = 0; i < 4; i++ ){
+    console.log(musicians[i] + "plays" + instruments[i])}
+    }
