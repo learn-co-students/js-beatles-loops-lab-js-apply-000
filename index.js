32853689a9182ff53c51e8c48c502880356aaaa0
aaronmillersliveaccount@live.com <aaronmillersliveaccount@live.com>
2018-08-04 17:44:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(anArrOfMusicians, anArrOfInstruments){
+  let final = [];
+  for(let i = 0; i < anArrOfMusicians.length; i++){
+    final.push(`${anArrOfMusicians[i]} plays ${anArrOfInstruments[i]}`)
+  }
+  return final;
+}
+
+function johnLennonFacts(anArrOfFacts){
+  let counter = 0;
+  while(counter < anArrOfFacts.length){
+  anArrOfFacts[counter] = `${anArrOfFacts[counter]}!!!`;
+    counter++
+  }
+  return anArrOfFacts;
+}
+
+function iLoveTheBeatles(aNum){
+  let final = [];
+  do{
+    final.push(`I love the Beatles!`);
+    aNum++;
+  } while (aNum < 15);
+  return final;
+}
