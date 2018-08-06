5136754fca9b44d323ac4728cc6a6940cf859fe1
Colette Mahoney <colettemahoney@hotmail.com>
2018-08-06 18:04:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 function theBeatlesPlay(musicians, instruments) {
 var array = [];
-  for (i = 0; i < 4; i++ ){
-    console.log(musicians[i] + "plays" + instruments[i])}
-    }
+  for (let i = 0; i < musicians.length; i++ ){
+    array.push(musicians[i] + " plays " + instruments[i])
+    
+  }
+     return array  
+}
+  
+  function johnLennonFacts(facts) {
+  var array = [];
+  var i = 0;
+while (i < facts.length){
+array.push(facts[i] + "!!!")
+i++;
+}
+return array
+}
+
+
+function iLoveTheBeatles(n) {
+var array = [];
+do {
+  array.push("I love the Beatles!");
+  n++;
+} while (n < 15);
+return array
+}
