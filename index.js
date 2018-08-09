2a20a5a6e5c6002e43f5515ee22e740cbf52dd51
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-09 14:36:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var myArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    myArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return myArray
+}
+
+const lennonFacts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts) {
+  var lennonResult = [];
+  var i = 0;
+  while (i < facts.length) {
+    lennonResult.push(facts[i] + '!!!')
+    i++;
+  }
+  return lennonResult;
+}
+
+function iLoveTheBeatles(n) {
+  var beatlesArray = []
+  do {
+    beatlesArray.push("I love the Beatles!");
+    n++
+  } while (n < 15);
+  return beatlesArray;
+}
