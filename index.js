3c93cad2e23b7a204a46f8aaa73720d722f2fdcd
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-10 19:07:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay (musicians,instruments){
+  var playList = [];
+  for (let i = 0; i < musicians.length; i++){
+    playList.push(`${musicians} plays ${instruments}.`)
+  }
+  return playList;
+}
