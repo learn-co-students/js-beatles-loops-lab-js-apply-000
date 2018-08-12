30994b1d6f1197f0dae2ba1a98bf1796b151fccd
Ashley  Zaki <ahnahn.un@gmail.com>
2018-08-12 23:17:36 +0000

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,28 @@
 function theBeatlesPlay (musicians,instruments){
   var playList = [];
   for (let i = 0; i < musicians.length; i++){
-    playList.push(`${musicians} plays ${instruments}.`)
+    playList.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return playList;
+}
+
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while (i < facts.length){
+    facts[i] = `${facts[i]}!!!`
+    i++;
+  }
+  return facts;
+}
+
+
+function iLoveTheBeatles(num){
+  var beatles = [];
+  do {
+    beatles.push(`I love the Beatles!`);
+  }
+  while (num < 15)
+  num++;
+  return Beatles;
 }
