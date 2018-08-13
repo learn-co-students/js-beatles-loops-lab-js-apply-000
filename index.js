573f374e386d29f150b39182b13a94d4b85e4417
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-08-13 12:28:00 -0400

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+array1=["John", "Ringo", "Paul", "George"];
+array2=["vocals", "drums", "guitar", "guitar"];
+
+
+const facts = [
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated the sound of his own voice"
+];
+function theBeatlesPlay(array1, array2) {
+    var beatlesArray=[];
+    for ( var i=0; i<array1.length; i++){
+    beatlesArray.push(array1 + "plays the"+ array2);
+    return beatlesArray;
+    }
+}
+    
+function johnLennonFacts(facts)
+{
+    while( i=0) 1<facts.length; i++);
+    return( i + "!!!")
+}
+
+function iLoveTheBeatles(){
+var loveArray=[];
+loveArray.push("I Love the Beatles!")
+    while( i<15);
+
+}
