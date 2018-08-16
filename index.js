45d9bb31911e95f90a6f7dfd972cb0f158876676
larryvsdata <ermanbekaroglu@gmail.com>
2018-08-16 06:15:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+
+ 
+function theBeatlesPlay(musicians,instruments){
+  
+  var resultArr=[]; 
+  
+  for(var i=0;i<musicians.length;i++){
+    resultArr[i]=musicians[i]+" plays "+instruments[i];
+  }
+  
+  return resultArr;
+}
+
+function johnLennonFacts(facts){
+  
+  var i=0;
+  
+  while (i < facts.length) {
+    facts[i] +=  "!!!";
+   
+}
+
+return facts;
+  
+}
