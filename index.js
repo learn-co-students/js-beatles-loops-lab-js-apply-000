8ae6133633de7a588c655aa2c1a7351e6421129b
Luke Matthews <onelukeshow@googlemail.com>
2018-08-20 20:26:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+
+function theBeatlesPlay(musicians,instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return array;
+}
+
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+    facts[i] += '!!!';
+    i++;
+  }
+  return facts;
+}
+
+
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+    
+    return love;
+}
