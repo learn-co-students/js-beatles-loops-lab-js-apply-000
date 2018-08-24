3608d46e6a8b65e753d39f5ca9f9de713dc55914
Jongik Thom <ljik95@hotmail.com>
2018-08-24 14:53:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+function theBeatlesPlay(musiciansArr, instrumentsArr) {
+  let listOfInstruments = [];
+  
+  for (var i = 0; i < musiciansArr.length; i++) {
+    listOfInstruments.push(musiciansArr[i] + ' plays ' + instrumentsArr[i]);
+  }
+  
+  return listOfInstruments;
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  
+  while (i < facts.length) {
+    facts[i] += '!!!';
+    i++;
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  let anArr = [];
+  
+  do {
+    anArr.push('I love the Beatles!');
+    num++;
+  }
+  while (num < 15);
+  
+  return anArr;
+}
