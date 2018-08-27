653465d468f0eca08cdfb0aacb0355f693792790
Jason Crescenzo <crescenzojay@gmail.com>
2018-08-27 19:41:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay(arr1, arr2){
+  var empty = [];
+  for(var i = 0; i<arr1.length; i++){
+    empty.push(arr1[i]+" "+"plays"+" "+arr2[i]);
+  }
+  return empty;
+}
+
+
+function johnLennonFacts(arr1) {
+  var empty =[];
+  var i = 0;
+  while (i <arr1.length){
+    empty.push(arr1[i]+"!!!");
+    i++
+  }
+  return empty;
+}
+
+function iLoveTheBeatles(number){
+  var empty = [];
+  var i = 0;
+  do {
+    empty.push("I love the Beatles!");
+    i++;
+  } while(i< number +1 && number < 15)
+  return empty;
+}
