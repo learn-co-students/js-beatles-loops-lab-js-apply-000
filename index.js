94cddc429c500169fe350e7688236ec80a0478d5
DONOVAN G DWYER <dwyer.donovan@gmail.com>
2018-08-27 15:20:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// add solution here
+var musician = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instrument = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musician, instrument) {
+  var combination = [];
+  for (var i = 0; i <= 3; i++) {
+    combination.push(musician[i] + " plays " +
+    instrument[i]);
+  }
+  return combination;
+}
+
+const facts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ];
+
+function johnLennonFacts(facts) {
+  var lpedia = [];
+  var j = 0;
+  while (j < facts.length) {
+    lpedia.push(`${facts[j]}!!!`)
+    j++
+  };
+  return lpedia;
+}
+
+
+function iLoveTheBeatles(num) {
+  var love = [];
+  var k = num;
+  do {
+    love.push("I love the Beatles!");
+    k++;
+  }
+  while (k < 15);
+  return love;
+}
