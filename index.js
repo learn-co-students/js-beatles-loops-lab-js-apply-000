b58069b8deb64ccb9a22174e284175f8f663df0b
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-08-29 00:58:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+
+
+var beatles = ['John Lennon','Ringo Starr','Paul McCartney','George Harrison']
+var instruments = ['Guitar','Drums','Bass','Guitar']
+var beatlesPlay = []
+var number = 6
+var i = null
+
+theBeatlesPlay(){
+  for (i = 0; i < beatles.length; i++){
+  beatlesPlay[i] = beatles[i] + " plays " + instruments[i]
+  }
+  return beatlesPlay[i]
+}
