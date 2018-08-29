a695e0c7bcf63da4c54e14ec457d495f95907ebf
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-08-29 01:43:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 
-var beatles = ['John Lennon','Paul McCartney','George Harrison','Ringo Starr']
-var instruments = ['Guitar','Bass','Lead Guitar','Drums']
+const beatles = [
+  'John Lennon',
+  'Paul McCartney',
+  'George Harrison',
+  'Ringo Starr'
+];
+const instruments = [
+  'Guitar',
+  'Bass',
+  'Lead Guitar',
+  'Drums'
+];
 var beatlesPlay = []
 var i = null
 
-function theBeatlesPlay(){
+function theBeatlesPlay() {
   for (i = 0; i < beatles.length; i++){
   beatlesPlay[i] = beatles[i] + ' plays '  + instruments[i]
   }
   return beatlesPlay[i]
+}
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts() {
+  while (i < facts.length){
+    facts[i] = facts[i] + "!!!"
+  }
 }
