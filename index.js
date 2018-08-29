ddc6dfd292658b7acf9830d84f9731c0b920a813
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-08-29 02:05:34 +0000

    Done.

--- index.js
+++ index.js
@@ -7,30 +7,39 @@ const beatles = [
 ];
 const instruments = [
   'Guitar',
-  'Bass',
+  'Bass Guitar',
   'Lead Guitar',
   'Drums'
 ];
+const facts = [
+  'He was the last Beatle to learn to drive',
+  'He was never a vegetarian',
+  'He was a choir boy and boy scout',
+  'He hated the sound of his own voice'
+];
 var beatlesPlay = []
-var i = null
+var i, num
+var num = 15
 
 function theBeatlesPlay() {
   for (i = 0; i < beatles.length; i++){
   beatlesPlay[i] = beatles[i] + ' plays '  + instruments[i]
   }
-  return beatlesPlay[i]
+  return beatlesPlay
 }
-
-
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
 function johnLennonFacts() {
   while (i < facts.length){
-    facts[i] = facts[i] + "!!!"
+    facts[i] = facts[i] + '!!!'
+    i++
   }
+  return facts
+}
+
+function iLoveTheBeatles() {
+  var beatlesLuv = []
+  i = 0
+  do {i = i + 1
+    beatlesLuv[i] = 'I love the Beatles!'
+  } while (i < num)
 }
