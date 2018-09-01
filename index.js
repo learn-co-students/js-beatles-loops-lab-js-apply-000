c2d26bd864315bd28a368ff2b9b11e236e3bf523
David Thai <david.thai@dafuloth.me>
2018-09-01 04:03:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = []
+
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + ' plays ' + instruments[i])
+  }
+
+  return arr
+}
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  // condition works because anything with a value is TRUE
+  while (facts[i]) {
+    facts[i] += '!!!'
+    i++
+  }
+
+  return facts
+}
+
+
+function iLoveTheBeatles(n) {
+  var arr =[]
+
+  do {
+    arr.push("I love the Beatles!")
+    n++
+  } while (n<15);
+
+  return arr
+}
