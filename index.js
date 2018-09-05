3c011b4ff3fdba4086537078bc08716b63559e04
Sang Yoon Song <sangyoon.song@gmail.com>
2018-09-05 12:07:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var res = []
+  
+  for (var i=0; i < musicians.length; i++){
+    var str = musicians[i] + " plays " + instruments[i]
+    res.push(str)
+  }
+  
+  return res
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  while (i < facts.length){
+    facts[i] = facts[i] + "!!!"
+    i++
+  }
+  
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  var res = []
+  
+  do{
+    res.push("I love the Beatles!")
+    n++
+  }  while(n < 15)
+  return res
+}
