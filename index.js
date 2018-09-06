3d202c9c3c8ee937351835f64448d9c3c4743084
Edwin Moses Isang <edmosesisang@gmail.com>
2018-09-06 08:24:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var music = [];
+for (var i=0; i < musicians.length; i++)
+music.push(musicians[i] + ' plays ' + instruments[i]);
+  return music;
+}
+theBeatlesPlay("John Lennon plays guitar")
+
+
+function johnLennonFacts(facts) {
+  var info = [];
+  var num = 0
+  while (num < facts.length) {
+info.push(facts[num] + "!!!");
+ num++
+ }
+return info;
+}
+
+function iLoveTheBeatles(number) {
+var items =[];
+do {
+  items.push("I love the Beatles!");
+  number++;
+} while (number < 15);
+return items;
+}
