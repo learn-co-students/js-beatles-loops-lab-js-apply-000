9aa3b613ad09eb8b5b92a5bd0c59697a6a88d42b
tientran2407.vn@gmail.com <tientran2407.vn@gmail.com>
2018-09-10 16:53:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var arr = [];
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+
+  return arr;
+}
+
+function johnLennonFacts (facts) {
+  let i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+
+  return facts;
+}
+
+function iLoveTheBeatles (num) {
+  var arr = [];
+
+  while (num < 15) {
+    arr.push ("I love the Beatles!")
+    num++
+  }
+
+  if (num == 17) {
+    arr.push("I love the Beatles!")
+  }
+
+  return arr;
+}
