eb588ef7e9a49ec0cd5b5aa383db86e8a12fc6a6
Breonna Moore <breonna.a.moore@outlook.com>
2018-09-10 21:58:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments){
+  
+  let array = [];
+  
+  for (let i = 0; i< musicians.length; i++){
+    array[i] = `${musicians[i]} plays ${instruments[i]}`;
+  }
+  
+  return array;
+}
+
+function johnLennonFacts (facts){
+  
+  let index = 0;
+  let array = [];
+  
+  while (index < facts.length){
+    
+    array[index] = facts[index] + '!!!';
+    index++;
+  }
+  
+  return array;
+}
+
+function iLoveTheBeatles(num){
+
+  let array = [];
+  
+  do {
+    
+    array.push("I love the Beatles!")
+    num++;
+    
+  } 
+  
+  while (num < 15);
+  
+  return array;
+}
+
--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,4 @@
+
 describe('Beatles Loops', function() {
   it("returns an array of strings containing what instruments each musician plays", function() {
     const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
