f73c4cf78097dec0455344fa269fd22f0be33b1a
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-11 02:24:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musician, instruments){
+  var facts = [];
+  for(var i = 0; i < musician.length; i++){
+    facts.push(`${musician[i]} plays ${instruments[i]}`);
+  }
+  return facts;
+}
+
+function johnLennonFacts(facts){
+  var newFacts = [];
+  
+  var i = 0;
+  while(i < facts.length) {
+    newFacts.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(number) {
+  
+  var n = [];
+  
+  do{
+    n.push('I love the Beatles!');
+    number++;
+  }
+  while(number < 15);
+    return n;
+}
