4a3ffc4f4c640e8b33f05ff8ca6390bbf8cc2c7c
Marcos Da Silva <dasilva.marcos@gmail.com>
2018-09-15 04:37:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+function theBeatlesPlay(musician_array, instrument_array){
+
+var squad = []
+
+for (var i=0; i<musician_array.length; i++){
+  squad.push(musician_array[i] + " plays " + instrument_array[i])
+}
+return squad
+}
+
+function johnLennonFacts(factarray){
+  var result =[]
+  
+  var i = 0
+  while (i < factarray.length){
+   result.push(factarray[i] + "!!!")
+   i++
+  }
+  
+  return result
+}
+
+function iLoveTheBeatles(number){
+  
+  var empty =[];
+  var i = number;
+  do {
+    empty.push("I love the Beatles!");
+    i++;
+    } while(i < 15);
+    
+  return empty
+}
