a939f12f7322be2de151e9a33ab1d71c246dc0ac
Maharshee roy <m.roy@optonline.net>
2018-09-19 18:12:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+theBeatlesPlay(musicians, instruments);
+function theBeatlesPlay(musicians,instruments){
+  var newArray = [];
+  for (var i=0; i < musicians.length ; i++) {
+    newArray.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return newArray;
+}
+function johnLennonFacts(array) {
+ var results = [];
+ var count = 0;
+ while (count < array.length) {
+   results.push(array[count] + "!!!");
+   count++
+  }
+ return results;
+ }
+ function iLoveTheBeatles(num) {
+   var array = []
+   do {
+     array.push("I love the Beatles!")
+     num++
+   } while (num <15);
+   return array
+ }
