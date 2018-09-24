73d2acde0a04c68e7d1e5021054ff7c86ede4fa8
Michael Lewell <michaellewell@hotmail.com>
2018-09-24 17:55:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(arr1,arr2){
+  const sentanceArr = [];
+  for(let i = 0 ; i < arr1.length ; i++){
+    sentanceArr.push(`${arr1[i]} plays ${arr2[i]}`);
+  }
+  return sentanceArr;
+}
+
+function johnLennonFacts(arr){
+  const exclamationArr = [];
+  let i = 0;
+  while(i < arr.length){
+    exclamationArr.push(`${arr[i]}!!!`);
+    i++;
+  }
+  return exclamationArr;
+}
+
+function iLoveTheBeatles(num){
+  const arr = [];
+ let i = num;
+  do{
+    arr.push("I love the Beatles!");
+    i++;
+  }
+  while(i < 15);
+  return arr;
+}
+
+
+
