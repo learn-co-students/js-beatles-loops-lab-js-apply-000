75048b619f852434829df0543f8a777446ae53a4
super-state-6474 <tchiu2@gmail.com>
2018-09-24 15:21:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var i = 0;
+  var results = [];
+  
+  while (i < 4) {
+    results[i] = musicians[i].toString() + " plays " + instruments[i].toString();
+    i++;
+  }
+ 
+  return results;
+  
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var results = [];
+  
+  while (i < facts.length) {
+    results[i] = facts[i].toString() + "!!!";
+    i++;
+  }
+  
+  return results;
+  
+}
+
+function iLoveTheBeatles(num) {
+  var i = 0;
+  var results = [];
+  
+  do {
+    results[i] = "I love the Beatles!";
+    i++;
+  } while (i < num + 1 && num < 15);
+  
+  return results;
+  
+}
