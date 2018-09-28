09dec8cd1bd14cc4fc161d6016e92a07281b82b2
Cris Hanks <crishanks12@gmail.com>
2018-09-28 00:23:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let result = [];
+  for (let i = 0; i < musicians.length; i++) {
+    const currentMusician = musicians[i];
+    const currentInstrument = instruments[i];
+    result.push(`${currentMusician} plays ${currentInstrument}`);
+  }
+  return result;
+}
+
+function johnLennonFacts(facts) {
+  let result = [];
+  let i = 0;
+  while (i < facts.length) {
+    result.push(facts[i] + '!!!');
+    i++;
+  }
+  return result;
+}
+
+function iLoveTheBeatles(n) {
+  let result = [];
+  if (n < 15) {
+    do {
+     result.push('I love the Beatles!');
+     n--;
+   }
+    while (n >= 0);
+  } else {
+    result.push('I love the Beatles!')
+  }
+  return result;
+}
+
+
