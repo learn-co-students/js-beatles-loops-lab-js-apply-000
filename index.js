88b8ea78931a887a345e067a94e4c2f5f4d656e2
Cris Hanks <crishanks12@gmail.com>
2018-09-29 20:16:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-function theBeatlesPlay(musicians, instruments) {
+var theBeatlesPlay = function(musicians, instruments) {
   let result = [];
   for (let i = 0; i < musicians.length; i++) {
     const currentMusician = musicians[i];
-    const currentIstrument = instruments[i];
-    result.push(`${currentMusician} plays ${currentIstrument}`);
+    const currentInstrument = instruments[i];
+    result.push(`${currentMusician} plays ${currentInstrument}`);
   }
   return result;
 }
 
-function johnLennonFacts(arrOfFacts) {
+const johnLennonFacts = function(arrOfFacts) {
   let result = [];
   let i = 0;
   while (i < arrOfFacts.length) {
-    const currentFact = arrOfFacts[i];
-    result.push(currentFact + '!!!');
+    result.push(arrOfFacts[i] + '!!!');
     i++;
   }
   return result;
 }
 
-function iLoveTheBeatles(n) {
+const iLoveTheBeatles = function(n) {
   let result = [];
   if (n < 15) {
     do {
       result.push('I love the Beatles!');
-    }
-    while (result.length <= n);
+      n--;
+    } while (n >= 0);
   } else {
     result.push('I love the Beatles!');
   }
   return result;
 }
-
 
 
