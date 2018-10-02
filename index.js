d4b6043c470448a58a805a12a490c25a381e1fb8
mecjagger <mj@makejagger.it>
2018-10-02 05:53:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay(array_musicians, array_instruments){
-  var empty_array = new Array()
+  var empty_array = [];
 
   for (var i=0; array_musicians.length > i; i++){
       empty_array[i] = array_musicians[i]+" plays "+array_instruments[i]
@@ -18,7 +18,7 @@ function johnLennonFacts(array){
 }
 
 function iLoveTheBeatles(number){
-var  empty_array = new Array()
+var  empty_array = [];
 
   do{
     empty_array.push("I love the Beatles!")
