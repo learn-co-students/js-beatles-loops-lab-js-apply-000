8ccef67c0cc14114b867b660b37755d058c02d4c
Ariel Hicks <hicks.ariel@gmail.com>
2018-10-08 19:07:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-function theBeatlesPlay(p1, p2) {
+function theBeatlesPlay(musicians, instruments) {
+   const arr =  [];
+   for(let i = 0; i< musicians.length; i++) {
+     arr.push(`${musicians[i]} plays ${instruments[i]}`)
+     
+   }
+   return arr;
 }
 
-// for ([]) {}
-var ourArray = [];
-  for (var = )
-  
+function johnLennonFacts(facts) {
+  let i = 0 
+  const arr = [];
+  while (i < facts.length) {
+  arr.push(`${facts[i]}!!!`)
+  i++
+  }
+  return arr;
 }
+
 
 // add solution here
+//theBeatlesPlay(["Bono", "Cher"], ["Guitar", "Piano"])
+//["Bono plays Guitar", "Cher plays Piano"]
