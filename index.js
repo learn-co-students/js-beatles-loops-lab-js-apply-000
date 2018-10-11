fbe3c27e38e7f82bf44e94cb501f6a4bfe406b80
Dmitry Tsuprunov <dmitri.tsuprunov@gmail.com>
2018-10-11 17:02:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+
+  return musicians.map((e,i) => `${e} plays ${instruments[i]}`)
+}
+
+function johnLennonFacts(facts) {
+
+  return facts.map(e => e + '!!!')
+}
+
+function iLoveTheBeatles(n) {
+  return n <= 16 ? 
+              new Array(n+1).fill('I love the Beatles!')
+              : ['I love the Beatles!']
+
+}
