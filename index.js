2a7b044f0fda8b99e6691d937f2eb1232f56456a
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2018-10-14 23:38:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+function theBeatlesPlay(){ 
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instrument = ["Guitar","Bass Guitar","Lead Guitar","Drums"];}// d so
+ varArray = forloop (theBeatlesPlay(musicians, instruments)).toEqual;
+ (["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar","George Harrison plays Lead Guitar","Ringo Starr plays Drums"]);  
+ function JohnLennoFacts()[]
+ const facts =["He was the last Beatle to learn to drive",
+                "He was never a vegetarian",
+                "He was a choir boy and boy scout",
+                "He hated the sound of his own voice"] .toEqual("He was the last Beatle to learn to drive!!!", " He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", 
+         "He hated the soundof his own voice!!!");       
+  
+  function iLoveTheBeatles()whileloop
+ ("returns an array of'I Love the Beatles!' 15 times when passed parameters 14", function() {
+   expected(iLoveTheBeatles(15)).toEqual(["i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles"]);
+ 
+    
+ })
+  
+  
+                
+ 
