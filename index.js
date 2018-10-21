1a33d15549fe798a195b6a11131e3fa3ffb6eb02
Albina Zakirova <zak1804@gmail.com>
2018-10-21 18:13:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+var musicians = ['John Lennon', 'paul McCartney']
+var instruments = ['guitar', 'piano']
+var musicianLenght = musician.lenght
+var createdArray = []
+
+for (var i, j = 0; i < musicianLenght; i, j++) {
+    createdArray[i] = musicians[i], + 'plays', + instruments[j], + '!'
+    //Do something
+}
