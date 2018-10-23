85daec753006aaea7a01fbe59424d03c73342cec
Clara Munro <clarajmunro@gmail.com>
2018-10-23 02:34:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = ['John Lennon plays Guitar', 'Paul McCartney plays Bass Guitar',
+  'George Harrison plays Lead Guitar', 'Ringo Starr plays Drums'];
+  for (var i = 0; i < musicians.legth; i++) {
+        array.push(musicians[i]+ 'plays' + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var results =[];
+  var count = 0;
+  while (count<facts.length){
+    results.push(facts[count]+"!!!");
+    count++;
+  }
+  return results;
+}
+
+function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  }
+  while (num<15);
+  return array;
+  }
