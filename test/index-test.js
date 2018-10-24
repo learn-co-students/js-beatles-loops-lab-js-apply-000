482da207474efed2f86ef041dafac0b77c8f9bdd
Rafael Cruz <ralph1786@gmail.com>
2018-10-24 20:12:27 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -27,7 +27,7 @@ describe('Beatles Loops', function() {
       expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
     });
 
-    it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
+     it("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
       expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
     });
   });
