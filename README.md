be6177f672a2f04a656de202a1eba0dcd36d9dd1
Albina Zakirova <zak1804@gmail.com>
2018-10-25 05:51:51 +0000

    Done.

--- README.md
+++ README.md
@@ -39,6 +39,7 @@ Use a while loop to loop over the facts array and add `"!!!"` to the end of
 every fact. The function should return an array of strings with exclamation
 points.
 
++ Create a function `
 + Create a function `iLoveTheBeatles` which accepts a number as a parameter. The
 body of the function should create a variable that stores an empty array. Then,
 implement a do-while loop inside the function that adds `"I love the Beatles!"`
--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 // add solution here
-var musicians = ['John Lennon', 'paul McCartney']
-var instruments = ['guitar', 'piano']
-var musicianLenght = musician.lenght
-var createdArray = []
 
-for (var i, j = 0; i < musicianLenght; i, j++) {
-    createdArray[i] = musicians[i], + 'plays', + instruments[j], + '!'
-    //Do something
+ function theBeatlesPlay(musicians, instruments) 
+ {
+   var MyArray=[]
+    for (var i = 0; i < musicians.length; i++)
+    {
+    	MyArray.push(musicians[i] + ' plays ' + instruments[i]);
+    }
+  return MyArray;
+ }
+ 
+ 
+ function johnLennonFacts(facts)
+ { 
+   var array=[]
+   var i=0
+   while (i < facts.length)
+   {
+     array.push(facts[i] + '!!!')
+     i++
+   }
+return array
+ }
+ 
+ function iLoveTheBeatles(number)
+ {
+   var array = []
+     do 
+         {
+           array.push('I love the Beatles!')
+           number++
+         }
+    while(number<15)
+    return array
 }
