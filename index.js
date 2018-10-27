35261768e3dda63931a2515f79c3c97b8b78e117
mirthful-scope-8549 <aneisha.thomas@gmail.com>
2018-10-27 04:48:30 +0000

    Done.

--- index.js
+++ index.js
@@ -22,13 +22,15 @@ function theBeatlesPlay (musicians, instruments) {
 }
 
 //-----
-function johnLennonFacts() {
-  var facts = [
+
+ var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
   ];
+
+function johnLennonFacts(facts) {
   var otherFacts = [];
   var i = 0;
   while (otherFacts.length < facts.length) {
