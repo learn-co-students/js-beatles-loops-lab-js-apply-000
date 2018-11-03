aa2cf5108794a943f6648c0a1fdf2451e851a081
Calie Rushton <calielrushton@gmail.com>
2018-11-03 13:16:52 +0000

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var result = [];
   for (let i = 0; i < musicians.length; i++) {
-    result.push(`${musicians[i]} + "plays" + ${instruments.[i]}`);
+    result.push(`${musicians[i]} plays ${instruments[i]}`);
   }
   return result;
 }
