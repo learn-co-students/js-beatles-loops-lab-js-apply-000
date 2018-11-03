1d94d82021511377db2069eb82ffd50171036ef9
Calie Rushton <calielrushton@gmail.com>
2018-11-03 12:57:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var result = [];
+  for (let i = 0; i < musicians.length; i++) {
+    result.push(`${musicians[i]} + "plays" + ${instruments.[i]}`);
+  }
+  return result;
+}
