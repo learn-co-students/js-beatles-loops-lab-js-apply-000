668d7e42cd19284b008311b34c4865d5c62eea76
Robert Vidal <bobbyvidal@gmail.com>
2018-11-06 15:07:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
+}
+ function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
+}
+ function iLoveTheBeatles(num) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
+}
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for ( var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+ return array;
+}
+
+function johnLennonFacts() {
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  var factsArray = [];
+  var i = 0;
+  while (factsArray < facts) {
+    factsArray.push(facts[i] + '!!!'); 
+    i++;
+}
+  return factsArray;
+}
