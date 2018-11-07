d4faf27087a961b665749f17b0cef49f4676b116
Woody Lucas <wlucas0311@gmail.com>
2018-11-07 03:49:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = [];
+  for (var i = 0; i < musicians.length; i++) {
+    beatles.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return beatles;
+}
+
+function johnLennonFacts(array) {
+  var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice" ];
+  var lennonFacts = [];
+  var i = 0
+  while(i < array.length) {
+    lennonFacts.push(array[i] + '!!!');
+    i++;
+  }
+  return lennonFacts;
+}
+
+function iLoveTheBeatles(num) {
+  var love = [];
+  do {
+    love.push('I love the Beatles!');
+    num++
+    
+  } while(num < 15) {
+    
+  }
+  return love;
+}
