6e2f66bf1a84f95d420e020b8dba808c77d91eee
Patrick Johnston <patrick.hardy.johnston@gmail.com>
2018-11-09 19:41:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+var counter = 0;
+
+function theBeatlesPlay(musicians, instruments) {
+  var facts = [];
+  for (var musician=0; musician < musicians.length; musician++) {
+    var fact = musicians[musician] + " plays " + instruments[musician];
+    facts.push(fact);
+  } 
+  return facts;
+}
+
+function johnLennonFacts(lennonFacts){
+  var coolFacts = [];
+  var fact = 0;
+  while (fact < lennonFacts.length) {
+    var coolFact = lennonFacts[fact] + "!!!";
+    coolFacts.push(coolFact);
+    fact++
+  }
+  return coolFacts;
+}
+
+function iLoveTheBeatles(howMuch) {
+  var screams = [];
+  var i = 0;
+  var limit = 15;
+  var howMany = limit - howMuch
+  do {
+    screams.push("I love the Beatles!");
+    i++;
+  }
+  while (i < howMany);
+  return screams;
+}
