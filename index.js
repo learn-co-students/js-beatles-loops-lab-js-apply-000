6e78812cf19622307696519873b40aa8815463a5
Diane Korongy <dkorongy@gmail.com>
2018-11-09 23:29:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var string[];
+}
+
+for (e=0; e<musicians.length(); e++){
+  string[e]= "";
+}
+for (i=0; i<musicians.length(); i++){
+  string[i] += musicians[i] + "plays" + instruments[i];
+  
+  return string;
+}
+
+
+function johnLennonFacts(facts) {
+  var fact[];
+  for (e=0; e<facts.length; e++){
+    fact[e]="";
+  }
+  var i=0;
+  while (i<facts.length) {
+    fact[i]+= facts[i] + "!!!";
+  i++;
+}
+return fact;
+}
+
+function iLoveTheBeatles(number){
+  var empty[];
+  
+  for (i=0; i<number; i++){
+    fact[i] = "";
+  }
+  do {
+    fact += "I love the Beatles!";
+  }
+  
+  while (number<15);
+
+}
+
+iLoveTheBeatles(15);
+
+
+
