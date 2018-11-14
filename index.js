745f2ae5576ba27adc5f5d7b16d765ce70bbe7b1
inherited-firmware-6870 <johnhaeng@gmail.com>
2018-11-14 23:46:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // add solution here
+
+function theBeatlesPlay (arrMusic, arrInstr) {
+  var arr = [];
+  
+  for (var i = 0; i < arrMusic.length; i += 1) {
+    var musicians = arrMusic[i];
+    
+    arr.push(musicians + ' plays ' + arrInstr[i]);
+  }
+  
+  return arr;
+} 
+
+
+function johnLennonFacts (arr) {
+  var finalArr = [];
+  
+  var i = 0;
+  while (i < arr.length) {
+    var fact = arr[i];
+    i += 1;
+    finalArr.push(fact + "!!!");
+  }
+  
+  return finalArr;
+}
+
+
+
+function iLoveTheBeatles (num) {
+  var arr = [];
+  
+  var i = num
+  do {
+    arr.push('I love the Beatles!');
+    i += 1;
+    
+  } while (i < 15); 
+  
+  return arr;
+}
+
