0dd26b73dc023e822846c22cc0f31b1b270c8404
Jiamin Lim <jiaminlim93@gmail.com>
2018-11-14 17:02:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var allPlayers=[];
+  for (var i = 0; i < musicians.length; i++) { 
+    allPlayers.push(musicians[i]+" plays "+instruments[i])
+  }
+  return allPlayers;
+}
+
+function johnLennonFacts(facts){
+  var newFacts=[];
+  for (var i = 0; i < facts.length; i++) { 
+    newFacts.push(facts[i]+"!!!")
+  }
+  return newFacts;
+}
+
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+    
+    return love;
+}
