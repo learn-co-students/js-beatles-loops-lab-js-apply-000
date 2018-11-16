8e2930e4d176eef6b5453b7a5c21a752241428fc
Shemar Dacosta <dshemar6@gmail.com>
2018-11-16 00:50:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var empty = []
+  for(var i = 0; i < musicians.length; i++){
+    var answer = console.log( musicians[i] +" plays " + instruments[i]);
+    return answer
+  }
+}
