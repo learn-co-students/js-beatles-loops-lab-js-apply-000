fcdf75a0e491ff7a50226a0894c49d80571b7046
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2018-11-17 05:03:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-function theBeatlesPlay(){ 
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instrument = ["Guitar","Bass Guitar","Lead Guitar","Drums"];}// d so
- varArray = forloop (theBeatlesPlay(musicians, instruments)).toEqual;
- (["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar","George Harrison plays Lead Guitar","Ringo Starr plays Drums"]);  
- function JohnLennoFacts()[]
- const facts =["He was the last Beatle to learn to drive",
-                "He was never a vegetarian",
-                "He was a choir boy and boy scout",
-                "He hated the sound of his own voice"] .toEqual("He was the last Beatle to learn to drive!!!", " He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", 
-         "He hated the soundof his own voice!!!");       
-  
-  function iLoveTheBeatles()whileloop
- ("returns an array of'I Love the Beatles!' 15 times when passed parameters 14", function() {
-   expected(iLoveTheBeatles(15)).toEqual(["i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles","i love the Beatles"]);
- 
+function theBeatlesPlay (musicians, playinstruments) {
+  var x = []
+  for(let i = 0; i<musicians.length; i++){
+    x.push(`${musicians[i]} plays ${playinstruments[i]}`)
+  }
+  return x 
+}
+function johnLennonFacts(facts) {
+  var i = 0;
+  while(i < facts.length) {
+    facts[i] += '!!!'
+  i++ 
+  }
+  return facts
     
- })
-  t("returns an array of 'I love the Beatles!' once when passed the parameter 17", function() {
-      expect(iLoveTheBeatles(17)).toEqual(["I love the Beatles!"]);
-    });
+   
+    
+    
   
-                
+}
+
+
+function iLoveTheBeatles(number) {
+  var x = []
+  
+  do{
+    x.push("I love the Beatles!")
+    number++;
+  } while(number <15)
+  return x
+    
+}
+  
+
+
+  
+  
  
