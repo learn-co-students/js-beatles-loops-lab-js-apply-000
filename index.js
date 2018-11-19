1f22b87140bd1d0327696ad7dc7250c301b41e95
Maximo Bautista <max.bautista97@gmail.com>
2018-11-19 01:49:15 +0000

    Done.

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ function johnLennonFacts(facts){
   }
   return facts;
 }
+
+function iLoveTheBeatles(number){
+  let array = [];
+  do{
+    array.push("I love the Beatles!");
+    number++;
+  }while(number < 15);
+  return array;
+}
+
+
+
+
+
