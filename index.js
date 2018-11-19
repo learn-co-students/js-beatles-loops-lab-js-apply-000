3dfc3923417cb29256b57138b47e62ade408cc33
Maximo Bautista <max.bautista97@gmail.com>
2018-11-19 01:40:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  let music = [];
+  for(var index = 0; index < musicians.length; index++ ){
+    let plays = `${musicians[index]} plays ${instruments[index]}`
+    music.push(plays);
+  }
+  return music;
+}
+
+function johnLennonFacts(facts){
+  let index = 0;
+  while(index < facts.length){
+    facts[index] =  facts[index] + "!!!";
+    index++;
+  }
+  return facts;
+}
