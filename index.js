a3cab791c9ecf20508853b455832cd091716045c
Berkay Akcakil <burkeakcakil@gmail.com>
2018-11-19 22:18:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-function theBeatlesPlay(musicians, instruments){
-  var array = []
-  for(let i = 0; i < musicians.length; i++){
+//"Musicians play instruments" till run out of musicians//
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for ( let i = 0; i < musicians.length; i++) {
     array.push(musicians[i] + " plays " + instruments[i])
   }
   return array
 }
- function johnLennonFacts(facts){
+//Return facts w/ !!! at the end of every fact till run out of facts//
+function johnLennonFacts(facts) {
   var array = []
   var i = 0
-  while(i < facts.length){
-    array.push(facts[i] + "!!!")
+  while (i < facts.length) {
+    array.push(facts[i] + "!!!");
     i++
   }
   return array
 }
- function iLoveTheBeatles(number){
+//Return "I love the Beatles!" 15 times//
+function iLoveTheBeatles(n) {
   var array = []
-  do{
-    array.push("I love the Beatles!")
-    number++
-  } while(number<15)
+  do {
+    array.push("I love the Beatles!");
+    n++
+  } while (n < 15)
   return array
 }
