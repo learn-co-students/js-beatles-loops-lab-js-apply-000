f62c28eb8348bf6a421a3dbac1aab86770378d2a
andy reyes <11710reyes@gmail.com>
2018-11-20 18:43:44 +0000

    Done.

--- index.js
+++ index.js
@@ -21,5 +21,9 @@ function johnLennonFacts(facts) {
 
 function iLoveTheBeatles(number) {
   var array = [];
-  
+  do {
+    (array.push("I love the Beatles!"))
+  number++;
+  } while (number < 15);
+    return array
 }
