c59f02f2f59632808614e554978850266f4065bc
andy reyes <11710reyes@gmail.com>
2018-11-20 18:48:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-var instruments = [];
-var musicians = [];
-
 function theBeatlesPlay(musicians, instruments) {
-  var array= [];
-  for (var i = 0; i < musicians.length; i++) {
+  var array = [];
+  var i = 0;
+  for (i = 0; i < musicians.length; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return array
-}
-
-function johnLennonFacts(facts) {
-  var shout = [];
-  var i = 0;
-  while ( i <facts.length) {
-    shout.push(`${facts[i]}!!!`);
-    i++
-  }
-  return shout;
-}
-
-function iLoveTheBeatles(number) {
-  var array = [];
-  do {
-    (array.push("I love the Beatles!"))
-  number++;
-  } while (number < 15);
-    return array
 }
