41c75b5c429fef857cb3caba12204a9a3178dd5e
essential-transformation-0497 <samiraesquina@gmail.com>
2018-11-20 18:54:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// add solution here
+/* add solution here*/
+const muscians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(muscians, instruments) {
+  var band = [];
+  for (var i=0; i< muscians.length; i++){
+    band.push(muscians[i]  +" plays " +  instruments[i] );
+  }
+  return band;
+}
+var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+
+function johnLennonFacts(factsArray) {
+  var i = 0;
+  var arrayCounter = factsArray.length;
+  var newFactsArray = [];
+ 
+  while (arrayCounter > 0){
+    newFactsArray.push(factsArray[i] + "!!!");
+    i++;
+    arrayCounter--;
+  }
+  return (newFactsArray);
+}
+
+johnLennonFacts(facts) 
+
+
+function iLoveTheBeatles(num){
+  var amor = [];
+  do {
+    amor.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+   return amor;
+}
+
