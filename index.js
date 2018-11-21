105557d02905132fa8325db9e776c9ab925682f8
captivating-computer-3358 <novinary@hotmail.com>
2018-11-21 13:11:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // add solution here
+
+/* --------------------------- theBeatlesPlay --------------------------------  */
+
+function theBeatlesPlay(musicians, instruments)
+{
+  const array = []
+  
+  for (let i = 0, l = musicians.length; i < l; i++)
+  {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+    
+   return array
+}
+
+
+/* ---------------------------- johnLennonFacts -------------------------------  */
+function johnLennonFacts(facts)
+{
+  const exclamationFacts = []
+  
+  let i = 0
+  
+  while (i < facts.length)
+  {
+    
+    exclamationFacts.push(`${facts[i]}!!!`)
+    
+    i++
+  }
+  
+  return exclamationFacts
+  }
+  
+ 
+/* ---------------------------  iLoveTheBeatles --------------------------------  */
+  function iLoveTheBeatles(num)
+  {
+    const array = []
+    
+    do 
+    {
+      array.push('I love the Beatles!')
+      num++
+    }
+    while(num < 15)
+    
+    return array
+    
+  }
+  
+  
+  
+  
+  
+  
