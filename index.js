4591f27dcd15d39c9c99f720ab66acd9c00f86fa
sapphira abayomi <saphieabayomi@gmail.com>
2018-11-26 11:05:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newSentence = [];
+  for(let i = 0; i < musicians.length; i++){
+    newSentence.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newSentence
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  var newString = []
+  while ( i < facts.length){
+    newString.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return newString
+}
+
+function iLoveTheBeatles(number){
+  var love = []
+
+  do{
+    love.push("I love the Beatles!")
+    number ++
+  }
+  while(number < 15);
+  return love
+}
