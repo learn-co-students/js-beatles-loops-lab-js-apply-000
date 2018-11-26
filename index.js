d03dfa43c794aaa50db2bb2d386097480afac21d
Mike Osenni <mosenni@fandm.edu>
2018-11-26 02:29:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// add solution here
+
+
+function theBeatlesPlay(musicians, instruments){
+ var theBand = [];
+ for (let i = 0; i < musicians.length; i++) {
+   theBand.push(musicians[i] + " plays " + instruments[i]);
+
+ }
+ return theBand
+}
+
+function johnLennonFacts(facts){
+var truths = [];
+let i = 0
+while (i < facts.length) {
+  truths.push(facts[i] + "!!!");
+  i++;
+}
+  return truths
+}
+
+function iLoveTheBeatles(n) {
+  var love = [];
+  do {
+    love.push("I love the Beatles!");
+    n++;
+
+  } while (n < 15);
+  return love
+}
