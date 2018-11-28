345bb8fb33b2d9421df0ca78d77bd8aaffd596e0
Jennifer Clark <jen.boyd.clark@gmail.com>
2018-11-28 09:05:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+let theBeatlesPlay = (musicians, instruments) => {
+  let bPI = [];
+  for (let i = 0; i < 4; i++){
+    bPI[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return bPI;
+}
+let johnLennonFacts = (jLFacts) => {
+  let factsArray = [];
+ let counter = 0;
+  while(counter < jLFacts.length){
+    factsArray[counter] =  jLFacts[counter] + "!!!";
+    counter ++;
+  }
+return factsArray;
+}
+
+let iLoveTheBeatles = (num) => {
+  let beatlesLove = [];
+  let i = 0;
+  do{
+    beatlesLove[i] = "I love the Beatles!";
+    i++
+  } while (num + i  < 15);
+  return beatlesLove;
+}
