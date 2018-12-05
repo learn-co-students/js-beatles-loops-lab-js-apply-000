184da19e30275b39b70c73e647ef96b038fe402e
Obi Nkwonta <obi136@gmail.com>
2018-12-05 14:19:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay (players, instruments){
+  var beatPlay = [];
+  for (var i = 0; i < players.length; i++){
+    beatPlay.push(players[i] + " plays " + instruments[i]);
+  }
+  return beatPlay;
+}
+
+function johnLennonFacts(facts){
+  var factsLoop = [];
+  var i = 0;
+  while (i < facts.length){
+    factsLoop.push(facts[i] + "!!!");
+    i++
+  }
+
+  return factsLoop;
+
+}
+
+function iLoveTheBeatles(num){
+  var who = [];
+
+  do {
+    who.push("I love the Beatles!");
+    num++
+  } while (num < 15);
+
+  return who;
+}
