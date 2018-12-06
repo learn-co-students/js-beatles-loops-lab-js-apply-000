a1cbace476a74cc66b233d5cc505d00c97d24a82
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-06 03:16:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments){
+function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
-  for(var i = 0; i < musicians.length && i < instruments.length; i++){
+  for(var i = 0; i < musicians.length && i < instruments.length; i++) {
     var newString = `${musicians[i]} plays ${instruments[i]}`;
     newArray.push(newString);
   }
   return newArray;
 }
-function johnLennonFacts(facts){
+// alternative solution: instead of storing newString variable, could push the interpolated string directly into newArray.
+
+function johnLennonFacts(facts) {
   var i = 0;
-  while (i < facts.length){
+  while (i < facts.length) {
     var excited = '!!!';
     facts[i] = facts[i] + excited;
     i++;
@@ -17,17 +19,41 @@ function johnLennonFacts(facts){
    return facts; 
 }
 
+/* alternative solution: 
+function johnLennonFacts(facts) {
+  var excitedArray = [];
+  var i = 0;
+  while (i < facts.length){
+    excitedArray.push(facts[i] + "!!!");
+    i++;
+  }
+  return excitedArray;
+}
+*/
+
 function iLoveTheBeatles(num){
-var loveArray = [];
-var i = 0;
-if ( num < 15){
-  var newNum =  15 - num;
-}
-do {
-  var saying = "I love the Beatles!";
-  loveArray.push(saying);
-  i++;
-}
+  var loveArray = [];
+  var i = 0;
+  if ( num < 15) {
+    var newNum =  15 - num;
+  }
+  do {
+    var saying = "I love the Beatles!";
+    loveArray.push(saying);
+    i++;
+  }
   while (i < newNum);
   return loveArray;
+} 
+
+/* alternative solution:
+function iLoveTheBeatles(num) {
+  var loveArray = [];
+  do {
+    loveArray.push("I love the Beatles!");
+    num++;
+  }
+  while (num < 15);
+  return loveArray;
 }
+*/
