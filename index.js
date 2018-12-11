bddf7c92c41e2524f36fee2bbd444b94b49dbbf7
global-robot-7991 <bradleyln293@gmail.com>
2018-12-11 17:05:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+function theBeatlesPlay (players, instruments) {
+  var allplayers = [];
+  for (var i = 0; i < players.length; i++) {
+    allplayers.push ( players [i] + " plays " + instruments [i] );
+  }
+  return allplayers;
+}
+
+function johnLennonFacts() {
+  const facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian',
+'He was a choir boy and boy scout','He hated the sound of his own voice'];
+  var newfacts = [];
+  var i = 0;
+  while (i < facts.length) {
+    newfacts.push(facts[i] +"!!!");
+    i++;
+  }
+  return newfacts;
+}
+
+function iLoveTheBeatles(num) {
+  var love = [];
+  do {
+    love.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+
+    return love;
+  }
