00ffccd5bd3758670866811fba1015098b6e04f8
Isabella de Medici <bellamedici242@gmail.com>
2018-12-12 14:40:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  for(var i = 0; i < musicians.length; i++) {
+    var s = musicians[i] + " plays " + instruments[i];
+     arr.push(s);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  var newArray = [];
+  var i = 0;
+  while(i < facts.length) {
+   var newFacts = facts[i] + "!!!";
+   newArray.push(newFacts);
+   i++;
+  }
+  return newArray;
+}
+
+function iLoveTheBeatles(number) {
+  var arr = [];
+    do  { 
+      arr.push("I love the Beatles!"); 
+      number++; 
+  } while(number < 15);
+  return arr;
+}
