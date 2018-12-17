491cbe07969288d3c6037d2614213c29c7b5c71a
peaceful-trainer-9354 <vladigolnikov@gmail.com>
2018-12-17 03:03:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var roles = [];
+  for (var i = 0; i < musicians.length; i++) {
+    roles.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return roles;
+}
+
+function johnLennonFacts(facts) {
+  var exclamations = [];
+  var j = 0;
+  while (j < facts.length) {
+    exclamations.push(facts[j] + '!!!');
+    j++;
+  }
+  return exclamations;
+}
+
+function iLoveTheBeatles(n) {
+  var loveArray = [];
+  do {
+    loveArray.push('I love the Beatles!');
+    n++;
+  } while (n < 15);
+  return loveArray;
+}
