3258e9b29deb38efd72081ec32fa47492b84726f
celestial-utility-5402 <datathriven@gmail.com>
2018-12-17 17:18:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var results = [];
+  var i;
+  for (i = 0; i < musicians.length; i++) {
+    results.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return results;
+}
+
+function johnLennonFacts(facts) {
+  formatted_facts = [];
+  var i;
+  while (i < facts.length) {
+    formatted_facts.push(facts[i] + "!!!");
+  }
+  return formatted_facts;
+}
