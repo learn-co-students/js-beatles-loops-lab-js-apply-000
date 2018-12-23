a821a070b4479a4474f03d3fe4a7a31ae27a9615
Chhaian Pin <chhaianpin@hotmail.com>
2018-12-23 01:45:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatles = []
+  var count
+  for(count = 0; count < musicians.length; count++) {
+    var musePlusInt = musicians[count] + " plays " + instruments[count];
+    beatles.push(musePlusInt)
+  }
+  return beatles
+}
+
+function johnLennonFacts(facts) {
+  var excFacts = []
+  var count = 0;
+  while (count < facts.length) {
+    excFacts[count] = facts[count] + "!!!"
+    count++
+  }
+  return excFacts
+}
+
+function iLoveTheBeatles(number) {
+  var numDeclarations = []
+  var count = 0
+  do {
+    numDeclarations[count] = "I love the Beatles!"
+    count++
+    if (number === 17) {
+      break;
+    }
+  } while (count <= number)
+  return numDeclarations
+}
