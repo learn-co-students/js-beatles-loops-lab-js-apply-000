fc4fa5d6d9f6f2a8d440dbd214fef707da988765
Stephanie Tsuei <stsuei@buffalo.edu>
2018-12-26 03:56:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var emptyArray = [];
+
+  for (var i = 0; i < musicians.length; i++){
+      emptyArray.push(musicians[i] +  "plays" +instruments[i]);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(facts){
+  var emptyArray = [];
+  var i = 0;
+  while (i < facts.length){
+    emptyArray.push('${facts[i] + "!!!"}');
+    i = i+1;
+
+  }
+  return emptyArray;
+}
+
+function iLoveTheBeatles(num){
+  var emptyArray = [];
+  var i = num;
+  do {
+    array.push("I love the Beatles!");
+    num = num + 1;
+  }
+  while (num < 15);
+  return emptyArray;
+}
