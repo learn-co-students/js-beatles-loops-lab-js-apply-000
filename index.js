311321650a026be10fcbff703a0698191a8f0584
Shlomo porges <porges.s@gmail.com>
2018-12-30 23:33:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(p1,p2){
+var result=[];
+  for (var i=0;i<p1.length;i++){
+    result.push(p1[i] + ' plays ' + p2[i])
+  }
+  return result;
+}
+
+function johnLennonFacts(pfacts){
+var counter = 0, result=[];
+while (counter< pfacts.length){
+  result.push(pfacts[counter]+'!!!');
+  counter++;
+}
+return result;
+}
+
+function iLoveTheBeatles (num){
+  var i=0,result = [];
+  do{
+    result.push("I love the Beatles!");
+    num++;
+  }
+  while(num < 15) ;
+  return result;
+}
