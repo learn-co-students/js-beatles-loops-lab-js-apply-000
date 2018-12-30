6326e7d24255db53789b58e219df4f4afb2c456c
pragmatic-form-3566 <chelsiasulin@gmail.com>
2018-12-30 22:20:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay (musiciansArr, instrumentsArr) {
+  var emptyArr = []
+  for (let i=0; i<musiciansArr.length; i++) {
+    emptyArr.push(`${musiciansArr[i]} plays ${instrumentsArr[i]}`)
+  }
+  return emptyArr
+}
+
+
+function johnLennonFacts (factsArr) {
+  var i = 0
+    while (i < factsArr.length) {
+      factsArr[i] += '!!!'
+      i++
+  }
+  return factsArr
+}
+
+function iLoveTheBeatles (num) {
+  var newArr = []
+  do {
+    newArr.push('I love the Beatles!')
+    num++
+  } while (num < 15)
+  return newArr
+}
