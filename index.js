6e22e0548cd6d2257a6861b3ec9d76a554ff51ff
sam ghoreishi <sam20gh@gmail.com>
2018-12-30 16:45:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
- function theBeatlesPlay(musicians, instruments) {
-  var play = [];
-   for(var i = 0; i < musicians.length; i++) {
-    play.push(`${musicians[i]} plays ${instruments[i]}`);
-  }
-  return play;
- }
-function johnLennonFacts(facts) {
-  var array = [];
-  let i = 0;
-  while (i < facts.length) {
-    array.push(`${facts[i]}!!!`);
-    i++;
-  }
-  return array
+// function theBeatlesPlay(musicians, instruments) {
+//   var play = [];
+//   for(var i = 0; i < musicians.length; i++) {
+//     play.push(`${musicians[i]} plays ${instruments[i]}`);
+//   }
+//   return play;
+// }
+// function johnLennonFacts(facts) {
+//   var array = [];
+//   let i = 0;
+//   while (i < facts.length) {
+//     array.push(`${facts[i]}!!!`);
+//     i++;
+//   }
+//   return array
+// }
+
+
+// function iLoveTheBeatles(number){
+//   var love=[];
+//   var i=0;
+//   do {
+//     love.push("I love the Beatles!");
+//     i++;
+//   }
+//   while ( i <= number && number < 15);
+//   return love;
+// }
+  
+function theBeatlesPlay(musicians, instruments){
+  var play =[];
+  for(var  i= 0 ; i < musicians.length; i++){
+    play.push(`${musicians[i]} plays ${instruments[i]}`)
+    }
+    return play
 }
 
+
+function johnLennonFacts(facts){
+  var johnLennonFactsNew = [];
+  let i = 0;
+  while (i < facts.length){
+    johnLennonFactsNew.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return johnLennonFactsNew;
+}
 
 function iLoveTheBeatles(number){
-  var love=[];
-  var i=0;
+  var love = [];
+  var i = 0;
   do {
-    love.push("I love the Beatles!");
+    love.push(`I love the Beatles!`);
     i++;
   }
-   while ( i <= number && number < 15);
+  while ( i <= number && number < 15);
   return love;
 }
-  
-
-
