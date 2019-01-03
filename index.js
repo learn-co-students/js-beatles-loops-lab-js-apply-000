9859e94cc3b698072918b06c7ce6a22354938a0c
Ana Pichardo <kayablissfilms@gmail.com>
2019-01-03 00:49:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ['guitar', 'Bass guitar', 'Drums', 'Lead guitar'];
+
+const facts =[
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice",
+];
+
+function theBeatlesPlay (musicians, instruments){
+  for( var i = 0; i < musicians.length; i++){
+    for(var j =0; j < instruments.length; j++){
+    var newArray = [];
+    newArray[i].unShift().push([]);
+      
+     }
+  }
+}
+return newArray[i] + "plays" + newArray[j];
+
+
