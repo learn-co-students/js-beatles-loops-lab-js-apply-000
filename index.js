281a8f83e48d47d038722a1571c9e9ce73ed5985
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-04 22:06:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// add solution here
+function theBeatlesPlay(arrM, arrI) {
+  var finalArr = [];
+  
+  for (var i = 0; i < arrM.length; i++) {
+    var currentM = arrM[i];
+    var currentI = arrI[i];
+    
+    finalArr.push(currentM + ' plays ' + currentI);
+  }
+  
+  return finalArr;
+}
+
+function johnLennonFacts(arrF) {
+  var factsArr = [];
+  
+  var j = 0;
+  
+  while (j < arrF.length) {
+    factsArr.push(arrF[j] + '!!!');
+    
+    j++;
+  }
+  
+  return factsArr;
+}
+
+function iLoveTheBeatles(num) {
+  var muchLove = [];
+  
+  do {
+    muchLove.push("I love the Beatles!");
+    num++;
+  }
+  while (num < 15);
+  
+  return muchLove;
+}
