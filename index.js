ca1d47ddbfa79fc34654b4207efaef3c91507388
Ana Pichardo <kayablissfilms@gmail.com>
2019-01-04 00:53:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ['Guitar', 'Bass Guitar', 'Lead guitar', 'Drums'];
 
-const facts =[
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice",
-];
 
 function theBeatlesPlay (musicians, instruments){
   for( var j = 0; j < musicians.length; j++){
@@ -25,14 +19,27 @@ function theBeatlesPlay (musicians, instruments){
 
 
 function johnLennonFacts(facts){
-  var i =0;
-  while (i <=facts.length){
-   i++; 
+  var exClam = [];
+  var i = 0;
+  while (i < facts.length) {
+  exClam.push(`${facts[i]}!!!`);
+  i++;
+  
   }
-  var exClam =[];
-  exClam.push(`${facts[i]} !!! `);
- return exClam;
-  }
+  return exClam;
+  
+}
   
   
+function iLoveTheBeatles(num){
+  var arr = [];
+ 
+  do {
+   arr.push( "I love the Beatles!");
+    
+    num++;
+  } while (num < 15);
+    return arr;  
+}  
+
 
