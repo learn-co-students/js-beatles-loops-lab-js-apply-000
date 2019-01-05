2e02121f043d184cb859f9e36cb7e9a044aa6fe6
Isaac Avilez <isaac1180@aol.com>
2019-01-05 22:05:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var Beatles = [];
+  var i;
+  var str;
+  for (i = 0; i <musicians.length; i++) {
+    str = musicians[i] + " plays " + instruments[i];
+    Beatles.push(str);
+  } 
+  return Beatles;
+}
+
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = facts[i] + "!!!" ;
+    i++;
+  }
+  return facts;
+}
+
+
+function iLoveTheBeatles(number) {
+  var LoveBeatles = [];
+  do { 
+    number++;
+    LoveBeatles.push("I love the Beatles!");
+    
+  } while (number < 15)
+  return LoveBeatles;
+}
