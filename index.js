c02cce6aae57219308d92c03a2b1919a1c5602e0
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-01-14 19:46:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+
 // add solution here
+
+function theBeatlesPlay(musicians,instruments)
+{
+  var heplays = []
+  for (let i=0; i<musicians.length; i++)
+  {
+    heplays.push(musicians[i]+" plays "+instruments[i])
+  }
+  return heplays
+}
+
+function johnLennonFacts(facts)
+{
+  var johnfacts = []
+  var i=0
+  while (i<facts.length)
+  {
+    johnfacts.push(facts[i]+"!!!")
+  i++
+  }
+  return johnfacts
+}
+
+function iLoveTheBeatles(num)
+{
+ var love = []
+ let i=0
+ do
+ {
+   love.push("I love the Beatles!")
+   i++
+ }
+ while (i<=num && num<17)
+ return love
+}
