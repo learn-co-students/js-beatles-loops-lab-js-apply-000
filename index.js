dfeba72d96b43f55cf6cad2750925b1e3d081bc6
organized-quotation-1910 <nicholas.moore5@icloud.com>
2019-01-14 22:47:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+let beatlesMembers = [];
+
+function theBeatlesPlay(musicians, instruments){
+  for (let i=0; i<musicians.length; i++){
+    beatlesMembers[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return beatlesMembers;
+}
+
+function johnLennonFacts(facts){
+  let i = 0;
+  while (i < facts.length) {
+    facts[i]+= "!!!";
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number){
+  let iLoveArray = [];
+  do {
+    iLoveArray.push("I love the Beatles!") ;
+  } while (15 - number > iLoveArray.length)
+  return iLoveArray;
+}
+
+
+
+
+
+
+
+
