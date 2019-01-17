eefef2329fd7345d30ce4427cf11066f90e3bf8c
convivial-system-4430 <cdneal091@gmail.com>
2019-01-17 19:47:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// add solution here
+// add solution here;
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+
+function theBeatlesPlay(musicians , instruments)
+{
+  var empty = [];
+  for (var i = 0; i < 4; i++){
+    empty [i] = musicians[i] + " plays " + instruments[i];
+  }
+  return empty;
+}
+
+function johnLennonFacts(facts)
+{
+  var i = facts.length - 1;
+  while(i > -1)
+  {
+    facts[i] = facts[i] + "!!!";
+    i--;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(number)
+{
+  var empty = [];
+  var i = 0;
+  do
+  {
+   
+   empty [i] = "I love the Beatles!";
+   i++;
+   number++;
+  } while (number < 15);
+  return empty;
+}
