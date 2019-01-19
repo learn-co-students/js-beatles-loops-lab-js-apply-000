18c10e7ff9b97d2d979c14360e3c60a0e500ed49
Karyna Dominguez <karinaminty@gmail.com>
2019-01-19 20:30:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var Allmusicians=[ ];
+  for(var i=0; i<musicians.length; i++){
+    Allmusicians.push(musicians[i] + " plays " + instruments[i]);
+    
+  }
+return Allmusicians;
+}
