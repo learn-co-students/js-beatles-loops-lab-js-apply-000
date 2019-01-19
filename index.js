189c8d073816199161fb2e13d733c5c6f2b87a96
Karyna Dominguez <karinaminty@gmail.com>
2019-01-19 20:52:27 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,25 @@ function theBeatlesPlay(musicians,instruments){
   }
 return Allmusicians;
 }
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+    var newFacts = [ ];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
+}
+function iLoveTheBeatles(num){
+  var ar =[ ];
+  do {
+    ar.push("I love the Beatles!");
+    num++;
+  }
+  while (num<15);
+  return ar;
+}
