1948902eee3226380b4741fa7dbaad9e9688c60f
eager-loop-4095 <jingchenjc2019@gmail.com>
2019-01-20 21:00:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var array = [];
+  for (var i= 0; i<musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var newFacts = [];
+  while (i < facts.length) {
+    newFacts.push(facts[i] + '!!!');
+    i++;
+  }
+  return newFacts;
+}
+//My note:no need to define facts. facts could be any array.
+// johnLennonFacts(facts); 
+
+function iLoveTheBeatles (num) {
+  var result=[];
+  do {
+    result.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  return result;
+}
