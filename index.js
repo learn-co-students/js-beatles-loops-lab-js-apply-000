c840d210c530852ae86c6dcac0ee4dde7d5fad7e
HARRY BHATT <bob@brahmingate.co.uk>
2019-01-30 18:22:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// add solution here
+
+function theBeatlesPlay (musicians, instruments) {
+var artists = [];
+
+for (var counter = 0; counter < musicians.length; counter++) {
+  var artistSkill = musicians[counter] + ' plays ' + instruments[counter];
+  artists[counter] = artistSkill;
+}
+ return artists;
+}
+
+
+function johnLennonFacts(facts) {
+  var factCounter = 0;
+  
+  while(factCounter < facts.length) {
+    facts[factCounter] += '!!!';
+    factCounter++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles (inputNumber) {
+  var ilb = [];
+  
+  if (inputNumber < 15) {
+    var counter = 0;
+    
+    do {
+      ilb[counter] = 'I love the Beatles!';
+      counter++;
+    } while (counter < inputNumber + 1);
+    
+    return ilb;
+  }
+  else
+  {
+    return 'I love the Beatles!';
+  }
+
+}
+
