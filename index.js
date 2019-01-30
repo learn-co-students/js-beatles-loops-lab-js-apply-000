41a95aed74f9a8acd60dc8373e7490df768c2baf
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-01-30 17:25:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var solution = []
+  var i = 0 
+  for(i=0;i<musicians.length;i++){
+    var str = `${musicians[i]} plays ${instruments[i]}`
+    solution.push(str)
+  }
+  return solution
+}
+
+function johnLennonFacts(facts){
+  var solution = []
+  var i = 0
+  while(i<facts.length){
+    var str = `${facts[i]}!!!`
+    solution.push(str)
+    i++
+  }
+  return solution
+}
+
+function iLoveTheBeatles(number){
+  var solution = []
+  var i = 0
+  do{
+    var str = "I love the Beatles!"
+    solution.push(str)
+    i++
+  }
+  while(i<(15-number)&&(number<15))
+  return solution
+}
