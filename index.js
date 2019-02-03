dac3a02bca3cf8948f28bb7a048507e94bfa2bfa
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-02-03 22:14:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-function theBeatlesPlay(musicians, instruments) {
-  const arr = []
+function theBeatlesPlay(musicians, instruments) { //thefunction thebeattlesplay accepts two parameter musicians and instrumnets//
+  const arr = [] // we have created a variable arr with an empty array//var fruits = ["apple", "babana"]//fruits.length = 2//
 
-  for (let i = 0, l = musicians.length; i < l; i++) {
+  for (let i = 0; i< musicians.length; i++) { 
+    //when i=0;  0<1;i=1//
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  //`${musicians[0]=john lennon plays $(instrument)[0]=guitar//
+    //when i=1; 1<2;i=2//
+    //`${musician[1] =   plays //
   }
+  
 
-  return arr
+  return arr;
 }
 
-function johnLennonFacts(facts) {
-  const shoutedFacts = []
+function johnLennonFacts(facts) { //function johnLennonfacts accepts an array of arguments ie john lennon facts//
+const shoutedFacts = [] //in shoutedfacts array is stored//
 
-  let i = 0
-  while (i < facts.length) {
-    shoutedFacts.push(`${facts[i]}!!!`)
-    i++
+  let i = 0  //when 0<3// 
+  while (i < facts.length) { 
+    shoutedFacts.push(`${facts[i]}!!!`) //`${facts[0]=no 0 facts is stored in the variable shoutedfacts//
+    i++  //while loop has only one condition so do i++ after first is completed//
   }
 
-  return shoutedFacts
+  return shoutedFacts; //stop  the function executon and returns the avlue to the funcion call//
 }
 
 function iLoveTheBeatles(n) {
---
+++ pp
@@ -0,0 +1,27 @@
+
+function theBeatlesPlay(musicians, instruments){
+const var = []
+for (i=0; i<musicians.length, i++) {
+var.push(`${musicians[i]} + "plays" + ${instruments[i]`)
+}
+return var;
+}
+
+function johnLennonFacts(facts) {
+const johnfacts = []
+while(i<facts)
+
+facts.push(`${facts[i]} + "!!!"`)
+}
+function iLoveTheBeatles(n){
+const var = []
+do {
+var.push("I love the Beatles !")
+n++
+}
+while(n<15)
+return var;
+}
+
+
+  
