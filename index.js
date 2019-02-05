69c3aa9f0a54ee67364efa4a9bf71692ac98d738
manon jacquin <manon.jacquin@gmail.com>
2019-02-05 20:42:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  const play = []
+  
+  for (var i = 0; i < musicians.length; i++) {
+    play.push(musicians[i] + " plays " + instruments[i])
+  }
+  
+  return play
+
+}
+
+function johnLennonFacts(array) {
+  var exclamation = []
+  for (var i = 0; i < array.length; i++) {
+    exclamation.push(array[i] + "!!!")
+  }
+  
+  return exclamation
+ 
+}
+
+function iLoveTheBeatles(num) {
+  var newSentence = []
+  do {
+    newSentence.push("I love the Beatles!")
+    num++
+    
+  } while (num < 15) return newSentence
+
+}
+
+
