9adfbd0d52f2578058cf19a3315fb7baf8f3dbd3
Brandon J Sotiroff <bsotiroff1@gmail.com>
2019-02-16 16:01:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // add solution here
+const theBeatlesPlay = (musicianArr, instrumentArr) => {
+  let outputArr = [];
+  
+  for(let i = 0; i < musicianArr.length; i++){
+    let currentElem = musicianArr[i]
+    let instrumentElem = instrumentArr[i];
+    
+    outputArr.push(`${currentElem} plays ${instrumentElem}`);
+  }
+  
+  return outputArr;
+}
+
+const johnLennonFacts = (anArr) => {
+  let counter = 0;
+  let outputArr = [];
+  while(counter < anArr.length){
+    let currentElem = anArr[counter];
+    
+    let newElem = currentElem + '!!!';
+    
+    outputArr.push(newElem);
+    counter++;
+  }
+  
+  return outputArr;
+}
+
+const iLoveTheBeatles = (num) => {
+  let howMuchILoveTheBeatlesArr = [];
+  
+  if(num >= 15){
+    howMuchILoveTheBeatlesArr.push('I love the Beatles!');
+  } else {
+      do {
+    howMuchILoveTheBeatlesArr.push('I love the Beatles!')
+    num++;
+  } while (num < 15)
+  
+  }
+
+  
+  return howMuchILoveTheBeatlesArr;
+}
+
