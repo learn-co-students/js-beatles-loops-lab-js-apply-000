db565575820f10979f22ea686bbe4842947893f6
miao shan <minnieshan@gmail.com>
2019-02-17 22:46:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
 
-functiontheBeatlesPlay(musicians, instruments){
-  var arr=[];
-  for(var i=0; i<musicians.length; i++){
-    arr.push(musicians[i]+"play"+instruments[i]);
+  for (let i = 0, l= musicians.length; i<l;  i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
- return arr;
 
+  return arr;
 }
 
-function johnLennonFacts(){
+function johnLennonFacts(facts) {
+  var arr = [];
 
+  var i = 0;
+  while (i < facts.length) {
+    arr.push(`${facts[i]}!!!`)
+    i++;
+  }
 
+  return arr;
 }
 
 function iLoveTheBeatles(num){
-
-
+  var arr=[];
+do{
+  arr.push("I love the Beatles!");
+  num++;
+  }
+while(num<15);
+return arr;
 }
