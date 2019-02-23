63bb7392197cfab006294ad5becb30dd6a5c7c3c
Gabe Vazquez <gabe.vazquez@blackrock.com>
2019-02-23 18:33:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var ary = [];
+  for (var i = 0; i < musicians.length; i++)  {
+    ary.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return ary
+}
+
+function johnLennonFacts(facts) {
+  var ary = []
+  var i = 0
+  while(i < facts.length){
+    ary.push(`${facts[i]}!!!`)
+    i++
+  }
+  return ary
+}
+
+function iLoveTheBeatles(num){
+  var ary = []
+  do {
+    ary.push("I love the Beatles!")
+    num++
+  } while(num < 15)
+  return ary
+}
