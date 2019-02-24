b6124003531572fc7fd8508fb3da78be7cae699d
Matthew Eric <mf9646a@alumni.american.edu>
2019-02-24 04:34:57 +0000

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ function theBeatlesPlay(musicians, instruments) {
   for (let i = 0, l = musicians.length; i < l; i++) {
     arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  
+
   return arr
 }
-
 
 function johnLennonFacts(facts) {
   const shoutedFacts = []
@@ -31,11 +30,3 @@ function iLoveTheBeatles(n) {
 
   return arr
 }
-
-theBeatlesPlay(musicians, instruments){
-}
-
-
-
-
-// add solution here
