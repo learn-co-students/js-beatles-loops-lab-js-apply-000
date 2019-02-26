da53368010641a316aa6ef5c26d701a7cdd41e4c
Stuart John Gordon Dow <stuart.dow@blackrock.com>
2019-02-26 22:46:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+
+function theBeatlesPlay (musicians, instruments){
+  var MusInstr =[];
+    for (var i = 0; i < musicians.length; i++) {
+      MusInstr.push(musicians[i] + " plays " + instruments[i]);
+}
+return MusInstr;
+}
+
+function johnLennonFacts(arrayJLFacts) {
+  var i = 0;
+    while (i < arrayJLFacts.length) {
+      arrayJLFacts[i] += '!!!';
+      i++;
+  }
+  return arrayJLFacts;
+}
+
+function iLoveTheBeatles (pNumber){
+  var arraryBeatlesLove =[];
+    do {arraryBeatlesLove.push ("I love the Beatles!");
+        pNumber++;
+    } while (pNumber < 15);
+return arraryBeatlesLove
+}
