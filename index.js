f9140b4dd9b559108864c1efc7668f310c67c5d5
restful-transform-8641 <xose.manolo.rodriguez@gmail.com>
2019-02-26 03:46:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var musiciansWithInstruments = [];
+  for(var i = 0; i < musicians.length; i++){
+    musiciansWithInstruments.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return musiciansWithInstruments;
+}
+
+//function johnLennonFacts
+function johnLennonFacts(facts){
+  for(var i = 0; i < facts.length; i++){
+    facts[i] = facts[i] + '!!!';
+  }
+  return facts;
+}
+//function iLoveTheBeatles
+function iLoveTheBeatles(num){
+  var loveArray = [];
+  do{
+    loveArray.push("I love the Beatles!");
+    num++;
+  }while(num < 15);
+  
+  return loveArray;
+}
