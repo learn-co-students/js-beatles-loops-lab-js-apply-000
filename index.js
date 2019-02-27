ddb90ebf34ded4df843233ca4f62ec4cacd83098
jackie.lin128 <jackie.lin128@gmail.com>
2019-02-27 02:16:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var newArray = [];
+  for (let i = 0; i < musicians.length; i++) {
+    var curMus = musicians[i];
+    var curIns = instruments[i];
+    newArray.push(curMus + " plays " + curIns)
+  }
+  return newArray;
+}
+
+
+function johnLennonFacts(facts) {
+  var factCounter = 0;
+  while (factCounter < facts.length) {
+    facts[factCounter] = facts[factCounter] + '!!!';
+    factCounter++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(aNum) {
+  var emtArray = [];
+  do {
+    emtArray.push('I love the Beatles!')
+    aNum++;
+  } while (aNum < 15);
+  
+  return emtArray;
+}
