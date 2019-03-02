a17427e189b84378e6f68d5e2cc724d457cf3a11
Frankie Tse <ftse105@gmail.com>
2019-03-02 19:00:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
+  var emptyArr = [];
+
+  for (var i = 0; i < arrayOfMusicians.length; i++) {
+    var str = arrayOfMusicians[i] + "plays" arrayOfInstruments[i];
+    emptyArr.push(str)
+  }
+  return emptyArr;
+}
