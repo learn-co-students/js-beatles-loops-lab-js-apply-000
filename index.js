6cee9ff4656d14f8d4a9a26a3dd99b1a7e59a669
Frankie Tse <ftse105@gmail.com>
2019-03-02 19:13:56 +0000

    Done.

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
   var emptyArr = [];
 
   for (var i = 0; i < arrayOfMusicians.length; i++) {
-    var str = arrayOfMusicians[i] + "plays" arrayOfInstruments[i];
+    var str = arrayOfMusicians[i] + " plays " + arrayOfInstruments[i];
     emptyArr.push(str)
   }
   return emptyArr;
+}
+
+function johnLennonFacts(facts) {
+  var arr = [];
+
+  while (facts.length > 1) {
+    var str = facts[i] + " !!!";
+    arr.push(str);
+  }
+  return arr;
 }
