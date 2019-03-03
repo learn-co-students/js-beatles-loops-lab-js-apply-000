813121a9f0fb05b5ee86763c9a03feb3118ab242
Allie <46724208+alolson@users.noreply.github.com>
2019-03-03 16:47:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,39 @@
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
-	var beatles = [];
-	for (var i = 0; i<musicians.length; i++) {
-	beatles.push(musicians[i] + " plays " + instruments[i]);
-	}
-	return(beatles);
+  const empty = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    empty.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return empty
 }
 
-/////
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
-function johnLennonFacts(facts){
-  
-var i = 0;
-	var lennon = facts.length
-	var morelennon = [];
+///////
 
-	while (lennon > 0) {
-		morelennon.push(facts[i] + "!!!");
-		i++;
-		lennon--;
-	}
-	return(morelennon);
+
+function johnLennonFacts(facts) {
+  const excitedFacts = []
+
+  let i = 0
+  while (i < facts.length) {
+    excitedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+
+  return excitedFacts
 }
-////////
 
+///////
 
-function iLoveTheBeatles(number){
-  var empty = [];
-  number++;
-  
-	var i = 0;
-	do {
-		empty.push("I love the Beatles!");
-		i++;
+function iLoveTheBeatles(number) {
+  const empty = []
 
-	} while (i < number);
-	
-if (number === 18) {
-		return("I love the Beatles!");
-	}
-	
-	return(empty);
-  
+  do {
+    empty.push('I love the Beatles!')
+    number++
+  } while (number < 15)
+
+  return empty
 }
