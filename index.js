738b31ae65b052e553da7130a07beaad7afee99b
Blake A Long <blake.a.long@gmail.com>
2019-03-05 04:05:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    var string = musicians[i] + " plays " + instruments[i];
+    emptyArray.push(string);
+  }
+  return emptyArray;
+}
+
+var facts = ["He was the last Beatle to learn to drive", 
+             "He was never a vegetarian",
+             "He was a choir boy and boy scout",
+             "He hated the sound of his own voice" ];
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while ( i < facts.length) {
+    facts[i] += "!!!";
+    i++;
+  }
+return facts;
+}
+
+function iLoveTheBeatles(num) {
+  var eArray = [];
+  do { 
+    eArray.push("I love the Beatles!");
+    num++;
+} while ( num < 15 );
+return eArray;
+}
