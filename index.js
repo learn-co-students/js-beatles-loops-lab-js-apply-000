161865c6f9cab3a9291b522c28751cf8eb2a928e
algebraic-propagation-5468 <luishong50@gmail.com>
2019-03-06 00:18:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments){
+  var empty =[];
+  var musicians =['John','Paul','George','Ringo'];
+  var instruments = ['guitar','bass guitar','bass guitar','drums'];
+  for (var i=0;i<4;i++)
+  return                  //how to add elements of arrays? concatinate?
+}
+
+function johnLennonFacts (){
+  const facts =[
+    "He was the last Beatle to learn to drive",
+    "He was never a vegetarian",
+    "He was a choir boy and boy scout",
+    "He hated to sound of his own voice"
+    ];
+}
+ while (johnLennonFacts()){   //how to return array?
+   console.log(facts[1]+"!!!")
+   console.log(facts[2]+"!!!")
+   console.log(facts[3]+"!!!")
+   console.log(facts[4]+"!!!")
+   
+ }
+ var i = 0;
+ function iLoveTheBeatles (){
+   i=i+1;
+   return i;
+   
+   var empty=[];
+   do{
+     console.log(empty.push("I Love the Beatles!"))
+     
+   } while (iLoveTheBeatles()<15);
+ }
