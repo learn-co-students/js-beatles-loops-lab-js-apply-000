575945859343f85dce7456ec9b506de63826020c
Karsten Holbein <karsten.holbein@blackrock.com>
2019-03-08 12:53:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var i=[];
+  var x;
+  for (x = 0; x < musicians.length; x++) { 
+  return i[x];
+}
+  
+}
+
+function johnLennonFacts(){
+  var i = 0;
+  var text = "";
+
+for (;text[i];) {
+  text[i] + "!!!";
+  i++;
+  
+while (text[i]) {
+  text += text[i] + "<br>";
+  i++;
+}
+
+
+}
+
+function iLoveTheBeatles (){
+
+var output= []
+var i = 0
+ 
+do {i += output[i] + "!!!";
+  i++;
+}
+while (i < 10);
+}
+return output
+
+}
