c5b1fe3a0ef0f2b987f543eec2da7fc276621eb7
Steve Pyne <stpyne@blackrock.com>
2019-03-11 02:41:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+
+
+
+function theBeatlesPlay(musicians, instruments) {
+  var sentences =[];
+  var len = musicians.length;
+  for (var i = 0; i < len; i++) { 
+    sentences.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return sentences;
+}
+
+function johnLennonFacts(facts) {
+  var sentences =[];
+  var len = facts.length;
+  var i = 0; 
+  while (i < len) {
+    sentences.push(facts[i] + "!!!");
+  i++;
+  }
+  return sentences;
+}
+
+function iLoveTheBeatles(number) {
+  var sentences =[];
+  do {
+    sentences.push(`I love the Beatles!`);
+    number++;
+  }
+  while (number<15);
+  return sentences;
+}
