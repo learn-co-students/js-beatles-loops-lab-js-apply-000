5950ec6c8ebc79e8db446138a72624e6ab530daf
Ariq Holland <001.aqh@gmail.com>
2019-03-15 22:10:04 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function theBeatlesPlay(musicians, instruments){
   var array=[];{
     for (var i=0; i<musicians.length; i++){
     array.push(musicians[i]+ " plays "+ instruments[i]);}
+    //OR array.push(`${musicians[i]} plays ${instruments[i]}`);} *Slower*
   }
   return array;
 }
