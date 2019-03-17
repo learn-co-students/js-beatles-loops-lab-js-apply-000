92c1f9ffef36e4b6b7edd0f1bec0ebea82b5b2b5
Brian Velez <brian.velez@blackrock.com>
2019-03-17 21:46:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 // add solution here
-const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-]
-const num = 7;
 
 function theBeatlesPlay(musicians, instruments) {
   var output = [];
