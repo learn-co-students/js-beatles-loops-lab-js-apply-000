17457921896e57657216e4a3dcc83ce59b611782
yana.loginova <yana.loginova@cix.csi.cuny.edu>
2019-03-18 21:08:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(arrayMusic,arrayInstrum){
+  let emptyArray=[]
+  for(let i=0;i<=arrayMusic.length-1;i++){
+    let another=arrayMusic[i]+" plays "+arrayInstrum[i];
+    arrayInstrum[i]++;
+    emptyArray.push(another);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(arrayFacts){
+  let newArray=[];
+  let i=0;
+  while(i<arrayFacts.length){
+    newArray.push(`${arrayFacts[i]}!!!`)
+      i++;
+  }
+return newArray ;
+}
+
+function iLoveTheBeatles(num){
+  let empty=[];
+  let loveExpression="I love the Beatles!"
+  do{
+    num++
+    empty.push(loveExpression)
+  }
+  while(num<15);
+  return empty;
+}
+
