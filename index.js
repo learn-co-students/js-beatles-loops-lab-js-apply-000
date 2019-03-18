343a1771f0e2f50124ce79fdf12df65e2b25a222
Sandeep Sharma <sandeep.sharma1@blackrock.com>
2019-03-18 11:22:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // add solution here
+
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var theBeatlesPlay = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    theBeatlesPlay.push(musicians[i] + " " + "plays" + " " + instruments[i]);
+  }
+  return theBeatlesPlay
+}
+
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts (facts) {
+  let i  = 0;
+  var johnLennonFacts = [];
+  while (facts[i]){
+    johnLennonFacts.push(facts[i] + "!!!");
+    i++;
+  }
+  return johnLennonFacts;
+  
+}
+
+
+let x = 7;
+function iLoveTheBeatles(x){
+  var iLoveTheBeatles = [];
+  do {
+    iLoveTheBeatles.push("I love the Beatles!");
+    x++;
+  }
+  while (x < 15) {
+    return iLoveTheBeatles;
+  }
+  
+}
