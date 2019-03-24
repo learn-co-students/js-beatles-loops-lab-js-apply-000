b6d6d34cc3a9f89cc766bc06731c754ae46a51de
Trevahr Robertson <trevahr@yahoo.com>
2019-03-24 19:11:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments)
+{
+  var a = []
+  for (var i = 0; i < musicians.length; i++)
+  {
+    a.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return a;
+}
+
+function johnLennonFacts(facts)
+{
+ var b = []
+ var i = 0
+  while (i < facts.length)
+  {
+    b.push(`${facts[i]}!!!`)
+    i++
+  }
+  return b;
+}
+
+function iLoveTheBeatles(num)
+{
+  var c = [];
+  do {
+    
+  }
+}
