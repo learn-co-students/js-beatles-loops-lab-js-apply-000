9ef2daf43bd92d82d4022892f286d567b50794e8
determined-list-7554 <vira.hryhoruk@gmail.com>
2019-03-25 15:42:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+
+
+function theBeatlesPlay(musicians, instruments){
+  var musinstrum = [];
+  var text;
+  for (let i = 0; i < musicians.length; i++) {
+  text = musicians[i] + " plays " + instruments[i];
+  musinstrum[i] = text;
+  }
+  return musinstrum;
+}
+
+ function johnLennonFacts(facts){
+  var newFacts = [];
+  var i = 0
+  while (i < facts.length) {
+    newFacts.push(facts[i] + "!!!")
+    i++;
+  }
+  return newFacts;
+}
+
+ function iLoveTheBeatles(num) {
+  var phrases = [];
+  do {
+    phrases.push("I love the Beatles!");
+    num++
+  } while (num < 15);
+  return phrases;
+}
