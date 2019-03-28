9d742b1ddc5f380afa5e0c3a7d7f8005954d4e42
Gavin O'Connor <gedmund@gmail.com>
2019-03-28 17:22:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// theBeatlesPlay accepts two parameters, loops over an array of musicans.
 var theBeatlesPlay = function(musicians, instruments) {
     const theBeatles = []
 
@@ -6,24 +7,24 @@ var theBeatlesPlay = function(musicians, instruments) {
     }
     return theBeatles
 }
-
+// johnLennonFacts accepts an array of facts, and adds '!!!' to the end of each fact.
 var johnLennonFacts = function(factsArray) {
   const facts = []
   var i = 0
 
-  while (factsArray.length > i) {
+  while (i < factsArray.length) {
     facts.push(`${factsArray[i]}!!!`)
     i++
   }
   return facts
 }
-
+// iLoveTheBeatles accepts a number and adds a string to an array while the number is less than 15.
 var iLoveTheBeatles = function(n) {
-  var loveBeatles = []
+  var beatlesArray = []
 
   do {
-    loveBeatles.push(`I love the Beatles!`)
+    beatlesArray.push(`I love the Beatles!`)
     n++
   } while (n < 15)
-  return loveBeatles
+  return beatlesArray
 }
