f459d07d2667646ee8ecfcadd8c140bd4d3b66a6
Neal JIn Oh <no4259@gmail.com>
2019-03-29 18:26:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // add solution here
+
+
+function theBeatlesPlay(musicians, instruments) {
+
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + "plays" + instruments[i]);
+  }
+  return musicians;
+}
+
+
+function johnLennonFacts() {
+  var johnLennonFacts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"]
+  var i = 0;
+  while (array.length < johnLennonFacts.length) {
+    array.push(johnLennonFacts[i] + "!!!");
+    i++
+  }
+  return array;
+}
+
+
+
+function iLoveTheBeatles(number) {
+var array = [];
+do {
+  array.push("I love the Beatles!");
+  number += 1
+}
+while (number < 15);
+return array;
+}
+
+
+
