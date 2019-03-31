cbd4e565102fdd663bcd6ecb945789bcc32174db
exuberant-minifier-2741 <htsegaw@gmail.com>
2019-03-31 04:21:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // add solution here
+ffunction theBeatlesPlay(instruments, musicians){
+  var array = [];
+
+  for(var i = 0; i < musicians.length; i++){
+    var string = musicians[i] + ' plays ' + instruments[i];
+    array.push(string);
+  }
+}
+
+function johnLennonFacts(facts){
+  var array = [];
+
+  for(var i = 0; i < facts.length; i++){
+     var string = facts[i] + '!!!';
+     array.push(string);
+  }
+  return array;
+}
