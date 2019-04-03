893e5d112a66ad9157097f1a1f62a6c2fc1187e0
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-03 00:49:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-function theBeatlesPlay(musicians, instruments){
-  var array = [];
-  for (let i = 0; i<musicians.length; i++){
+function theBeatlesPlay(musicians, instruments) {
+  const array = [];
+  for (let i = 0, l = musicians.length; i < l; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`);
-  }  
+  }
   return array;
 }
 
-function johnLennonFacts(facts){
-  var bigFacts = [];
+function johnLennonFacts(facts) {
+  const bigFacts = [];
   let i = 0;
-  while(i < facts.length){
+  while (i < facts.length) {
     bigFacts.push(`${facts[i]}!!!`);
+    i++;
   }
-  return bigFacts; 
+  return bigFacts;
 }
 
-function iLoveTheBeatles(n){
-  var array = [];
+function iLoveTheBeatles(n) {
+  const array = [];
   do {
-    array.push("I love the Beatles!");
+    array.push('I love the Beatles!');
     n++;
   } while (n < 15);
   return array;
