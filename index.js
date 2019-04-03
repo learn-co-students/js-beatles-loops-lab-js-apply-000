d230b31f9f7655d6e5a19a701438dff6b4e623f8
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-03 00:24:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for (let i = 0; i<musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }  
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var bigFacts = [];
+  let i = 0;
+  while(i < facts.length){
+    bigFacts.push(`${facts[i]}!!!`);
+  }
+  return bigFacts; 
+}
+
+function iLoveTheBeatles(n){
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return array;
+}
