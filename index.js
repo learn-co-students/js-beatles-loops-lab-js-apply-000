5a09ebf8361c88ca267cfbcf519b929736088a38
Ellyn Anderson <eka249@nyu.edu>
2019-04-04 07:01:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+
+function theBeatlesPlay(){
+  var members = ['John' 'Paul' 'Ringo' 'George']
+  var instruments= ['guitar' 'bass' 'drums' 'guitar']
+  var beatles = []
+  var i;
+  for (i in members, i in instruments) { beatles += members +'plays' + instruments}
+
+  }
+}
+return beatles
