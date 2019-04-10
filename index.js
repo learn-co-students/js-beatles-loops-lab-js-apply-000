def9ba9bed1a56a132dbcff53fdb925a00835b41
moniet <moniet.sawhney@gmail.com>
2019-04-10 12:37:11 +0100

    made some changes to solution

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+
+  var myArr = [];
+  var strs;
+
+  for (i=0; i < musicians.length; i++) {
+    strs = `${musicians[i]} plays ${instruments[i]}`;
+    myArr.push(strs);
+  }
+
+  return myArr;
+
+}
+
+function johnLennonFacts(facts) {
+
+  var str;
+  var counter = 0;
+  var factArr = [];
+
+  while (counter < facts.length) {
+
+    str = facts[counter] + '!!!';
+    factArr.push(str);
+    counter++;
+
+  }
+
+  return factArr;
+
+}
+
+function iLoveTheBeatles(num) {
+
+  var myArr = [];
+  var theStr = 'I love the Beatles!';
+
+  do {
+
+    myArr.push(theStr);
+    num++;
+
+  } while (num < 15);
+
+  return myArr;
+
+}
