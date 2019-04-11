5ba31fa0fdbb49e197125096fd91ddcc0c2e32af
Jinzi Yan <stephanie.jinzi.yan@gmail.com>
2019-04-11 07:49:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// add solution here
+function theBeatlesPlay(array1,array2) {
+  var newarray = [];
+  for(var i = 0; i < array1.length; i++) {
+    newarray.push(`${array1[i]} plays ${array2[i]}`)
+  }
+  return newarray;
+}
+function johnLennonFacts(facts) {
+  var i = 0;
+  var newarray= [];
+  while(i < facts.length) {
+    newarray.push(`${facts[i]}!!!`)
+    i ++;
+  }
+  return newarray
+}
+function iLoveTheBeatles(number) {
+  var newarray = [];
+  do {
+    newarray.push("I love the Beatles!")
+    number ++
+  }while(number < 15) 
+  return newarray;
+}
