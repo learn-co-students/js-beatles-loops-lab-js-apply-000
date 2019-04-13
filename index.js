ffb8c7a3c50912810cc2b0cb7a99149272edbd70
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-04-13 04:22:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+    var x = []
+    for (var i = 0; i < musicians; i++) {
+    array += "John Lennon Plays guitar" + i + "<musicians(instruments)" ;
+    i++;
+  };
+
+function johnLennonFacts(array) {
+    var i = 0
+    var array;
+    while (i < array) {
+    array [i] += '!!!' ;
+    i++ ;
+  };
+
+function iLoveTheBeatles(number) {
+  var array = ""
+  var i = 0;
+
+  do {
+    array += ('I Love the Beatles!');
+    i++;
+  }
+  while (i < 15) ;
+  return array;
+}
+  };
