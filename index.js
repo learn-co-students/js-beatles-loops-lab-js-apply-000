6c34114395ad7d47f77455436653f049a0c3fe74
punctual-skill-3845 <bichsonnguyen@gmail.com>
2019-04-16 00:18:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// add solution here
+
+function theBeatlesPlay(arr1, arr2) {
+  let returnArr = [];
+  for (let i=0;i<arr1.length;i++) {
+    returnArr.push(arr1[i] + ' plays ' + arr2[i]);
+  }
+  return returnArr;
+}
+
+function johnLennonFacts(arr) {
+  let i = 0;
+  while (i<arr.length) {
+    arr[i] = arr[i] + '!!!';
+    i++;
+  }
+  return arr;
+}
+
+function iLoveTheBeatles(num) {
+  let arr = []
+  let i = num;
+  do {
+    arr.push('I love the Beatles!');
+    i++;
+  }
+  while (i < 15);
+  return arr;
+}
