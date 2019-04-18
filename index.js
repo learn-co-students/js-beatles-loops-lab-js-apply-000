409e6a713bbb5d445caffb475f6e10e241d67c74
Fanqiang Meng <fanmeng0417@gmail.com>
2019-04-18 05:03:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // add solution here
+function theBeatlesPlay(musicans, instruments){
+  var array = []
+  var string = ""
+  for (var i = 0; i <  musicans.length; i++){
+    string = musicans[i] + " plays " + instruments[i]
+    array.push(string)
+  }
+  return array
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  var string = ""
+  var array = []
+  while (i < facts.length){
+    string = facts[i] + "!!!"
+    array.push(string)
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(number){
+  var string = "I love the Beatles!"
+  var array = []
+  var condition = 0
+  var counter = 0
+  if (number > 15){
+    do{
+      array.push(string)
+      counter++
+    }while (counter < 1)
+    return array
+  }
+  else {
+    condition = 15 - number
+    do{
+      array.push(string)
+      counter++
+    }while (counter < condition)
+    return array
+  }
+}
