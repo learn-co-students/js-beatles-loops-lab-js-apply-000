2802ca14a8cddc5727d81a5b61144781c49a39e6
beta-scanner-6136 <mahdi.saeikhalili@gmail.com>
2019-04-20 03:00:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var result = []
+  for(var i=0;i<4;i++){
+    result.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return result
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  while(i<facts.length){
+    facts[i] += "!!!"
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  var result = []
+  do{
+    result.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+  return result
+}
+
