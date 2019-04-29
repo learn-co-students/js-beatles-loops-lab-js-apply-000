5c8bf74b21e41f70c3edd95f71364c649b8e50b2
lively-storage-3976 <sam.dbarrie@gmail.com>
2019-04-29 15:51:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  const array = []
+  for (let i=0; i<musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  const loudFacts = []
+  let i = 0;
+  while (i < facts.length) {
+    loudFacts.push(`${facts[i]}!!!`), i++
+  }
+  return loudFacts
+}
+
+function iLoveTheBeatles(n) {
+  const array = []
+  do {
+    array.push('I love the Beatles!')
+    n++
+  } while (n < 15)
+  return array
+}
