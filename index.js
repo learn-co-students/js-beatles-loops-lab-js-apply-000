0d6df67c159782eb6415d04bf39054475dc734dc
Aaron B <abriscoe2008@gmail.com>
2019-05-01 17:57:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 function theBeatlesPlay(musicians, instruments) {
-  var array = []
+  var array = [];
 
   for (let i = 0, l = musicians.length; i < l; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`)
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
 
-  return array
+  return array;
 }
 
 function johnLennonFacts(facts) {
-  var exclaimFacts = []
+  var exclaimFacts = [];
 
-  let i = 0
+  let i = 0;
   while (i < facts.length) {
-    exclaimFacts.push(`${facts[i]}!!!`)
-    i++
+    exclaimFacts.push(`${facts[i]}!!!`);
+    i++;
   }
 
-  return exclaimFacts
+  return exclaimFacts;
 }
 
 function iLoveTheBeatles(n) {
-  var array = []
+  var array = [];
 
   do {
-    array.push('I love the Beatles!')
-    n++
-  } while (n < 15)
+    array.push('I love the Beatles!');
+    n++;
+  } while (n < 15);
 
-  return array
+  return array;
 }
