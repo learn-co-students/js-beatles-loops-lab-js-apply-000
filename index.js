80618bda7469067004f658d53825a02b78b9681a
algebraic-animation-3644 <nathanchan1007@gmail.com>
2019-05-01 03:06:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+    var beatles = [];
+    for (var i = 0; i < musicians.length; i++) {
+        beatles[i] = musicians[i] + " "+ "plays" + " " +instruments[i]
+    }
+    return beatles
+}
+function johnLennonFacts(facts) {
+    var i = 0;
+    while (i < facts.length) {
+            facts[i] = facts[i] + "!!!"
+            i++
+    }
+    return facts
+}
+function iLoveTheBeatles(number) {
+    var array = [];
+    var i = 0;
+    do {
+        array[i] = "I love the Beatles!"
+        i++;
+    } while (i < Math.abs(number-15));
+    return array
+}
