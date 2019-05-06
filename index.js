61d51474bf81191e2c4e454516207d1078d5e1df
David Espinoza <davidespinoza@att.net>
2019-05-06 17:34:44 +0000

    Done.

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function johnLennonFacts(facts){
 function iLoveTheBeatles(number){
   var myArr = [];
   do{
-    myArr = ["I love the Beatles!"];
-    number++;
+	number++;
+    myArr.push("I love the Beatles!");
+    //myArr[15 - number] = ["I love the Beatles!"];
   }while(number < 15)
-
-	return myArr[15 - number];
+	return myArr;
 }
