779b106cbc32bf31978237c08501eb716b12e94d
decent-hacker-5309 <jiahaoln@gmail.com>
2019-05-10 20:44:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let finalArray = [];
+
+  for(let i=0; i<musicians.length; ++i){
+    let currMusician = musicians[i];
+    let currInstrument = instruments[i];
+
+      finalArray.push(`${currMusician} plays ${currInstrument}`)
+
+
+  }
+
+  return finalArray;
+}
+
+
+function johnLennonFacts(facts){
+  let finalArray = [];
+
+  for(let i=0; i<facts.length; ++i){
+    finalArray.push(facts[i]+"!!!");
+  }
+
+  return finalArray;
+}
+
+
+function iLoveTheBeatles(number){
+  let finalArray = [];
+
+  do{
+    finalArray.push("I love the Beatles!");
+    number++;
+  } while(number < 15)
+  return finalArray;
+}
