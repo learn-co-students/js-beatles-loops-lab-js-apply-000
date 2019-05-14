b5974cb0a2e191f56b82afbbd7a22e6ddd9ae5c5
Kenneth Portillo <portillokenneth@gmail.com>
2019-05-14 04:14:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  
+  var theBeatles = []
+  
+  for (let i = 0; i < musicians.length; i++){
+    theBeatles.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return theBeatles
+}
+
+function johnLennonFacts(facts){
+  var john = []
+  let i = 0
+  while (i < facts.length){
+   john.push(`${facts[i]}!!!`)
+   i++
+  }
+  return john
+}
+
+function iLoveTheBeatles (n){
+  
+  var love = []
+  do{
+    love.push("I love the Beatles!");
+    n++;
+  }
+  while (n < 15)
+  return love
+}
