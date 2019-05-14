ca6bb0850849ec830cec4871b4414cb5ac97ec1a
Kimberly Bone <k_bone@uncg.edu>
2019-05-14 01:25:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var newMusicians = [];
+  for (let i = 0; i < musicians.length; i++) {
+    newMusicians.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return newMusicians;
+}
+
+function johnLennonFacts(facts){
+  var newFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    newFacts.push(facts[i] + '!!!');
+    i++;
+  }
+    return newFacts;
+}
+
+function iLoveTheBeatles(n) {
+  var array =[];
+  do {
+    array.push('I love the Beatles!'); 
+    n++;
+  } while (n < 15);
+  return array;
+}
