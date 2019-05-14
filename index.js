c8b8c2e404960ced54f983b1499b246b68928838
Gia Edgington <gia.edgington@gmail.com>
2019-05-14 00:35:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var BeatlesPlay = []
+  var i
+    for(i = 0; i < musicians.length; i++){
+      BeatlesPlay.push(musicians[i] + " plays " + instruments[i]);
+    } i++
+  return BeatlesPlay
+}
+
+function johnLennonFacts(array){
+  var newArray = []
+  let i = 0
+  while(i < array.length){
+    array[i]+= "!!!"
+    newArray.push(array[i])
+    i++
+  }
+  return newArray
+}
+
+function iLoveTheBeatles(num){
+  var newArray= []
+  do {
+    newArray.push('I love the Beatles!')
+    num++
+  } 
+  while(num < 15)
+  return newArray
+}
+
+
