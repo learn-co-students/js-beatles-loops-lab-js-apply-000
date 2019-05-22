472626e0c5bd36b44689c1eb4ceb28698420250d
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-05-22 02:37:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let newArr=[];
+  for(let i=0; i<musicians.length;i++){
+    let musician=musicians[i];
+    let instrument=instruments[i];
+    newArr.push(`${musician} plays ${instrument}`)
+  }
+  return newArr;
+}
+function johnLennonFacts(facts){
+  let i=0;
+  let newArr=[];
+  while(i<facts.length){
+    let fact=facts[i];
+    newArr.push(`${fact}!!!`);
+    i++;
+  }
+  return newArr;
+}
+function iLoveTheBeatles(number){
+  let arr=[];
+  let i=number;
+  do{
+    arr.push("I love the Beatles!");
+    i++;
+  }
+  while(i<15);
+  return arr;
+}
