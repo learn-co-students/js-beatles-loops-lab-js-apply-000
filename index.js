3183dc29f9c45516b022b4711c4608b2d5b33a2e
cuddly-assembler-2780 <vinetree@gmail.com>
2019-05-22 23:01:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay (arrMusician, arrInstrument) {
+  var emptyArray = [];
+  for (let i=0; i<arrMusician.length; i++) {
+    emptyArray.push(`${arrMusician[i]} plays ${arrInstrument[i]}`);
+  }
+ return emptyArray;
+}
+
+function johnLennonFacts (arrFacts) {
+  var i=0;
+  while (i<arrFacts.length) {
+    arrFacts[i] = arrFacts[i] + "!!!";
+    i++;
+  }
+  return arrFacts;
+}
+
+function iLoveTheBeatles (num) {
+  var outputArray = [];
+  do {
+    outputArray.push("I love the Beatles!");
+    num++
+  } 
+  while (num<15);
+  
+  return outputArray;
+}
+
