3ed41f023ba05e6ad5968fac51463178ebc43ee6
global-value-0259 <josephtrettevik@gmail.com>
2019-05-28 16:08:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var length = musicians.length;
+  var localArray = [];
+  for (let counter = 0; counter < length; counter++) {
+    localArray.push(`${musicians[counter]}` + ' plays ' + `${instruments[counter]}`)
+  }
+  return localArray
+}
+
+function johnLennonFacts (facts) {
+  var localArray = [];
+  while (localArray.length < facts.length) {
+    localArray.push(`${facts[localArray.length]}` + '!!!')
+  }
+  return localArray;
+}
+
+function iLoveTheBeatles(num) {
+  var localArray = [];
+  do {
+    localArray.push('I love the Beatles!');
+    num++;
+  } while (num < 15); 
+  return localArray;
+}
