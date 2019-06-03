081d61bfb8e8f5c3aee617ddb8af9b14bdee7830
Xingliang Tong <cindybox.33@gmail.com>
2019-06-03 22:32:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+function theBeatlesPlay(array1,array2){
+ const combinedArray = [];
+for (let i =0;i<array1.length;i++){
+  combinedArray.push(array1[i] + " plays " + array2[i]);
+
+}return combinedArray;
+}
+
+function johnLennonFacts(array){
+  const upArray = [];
+  let i=0;
+  while(i<array.length){
+    upArray.push(array[i]+"!!!");
+    i++;
+  }return upArray;
+}
+
+function iLoveTheBeatles(n){
+  const newArr = [];
+  let i=0;
+  do{n++;newArr.push("I love the Beatles!");}
+  while(n<15);
+  return newArr;
+}
