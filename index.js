24b4be9ac07c154d840b3406a56f01f7066ee2d3
amusing-history-4188 <mackenzieerinpech@gmail.com>
2019-06-05 13:46:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+function theBeatlesPlay(musician, instrument) {
+  var x = [];
+  for(var i=0; i<musician.length; i++) {
+  x.push(musician[i] + ' plays ' + instrument[i])
+  }
+  return x;
+}
+
+function johnLennonFacts(array) {
+  var i = 0
+  while (i < array.length) {
+    array[i] += '!!!';
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  var arr = [];
+  var i = 0;
+  do {
+    arr.push('I love the Beatles!');
+    i++;
+  } 
+  while (i <= number && number < 15);
+  return arr;
+}
