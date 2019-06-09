29b1dc900923a532acde05c9a052979764a66084
curious-paradigm-3416 <tyler.daelemans@gmail.com>
2019-06-09 02:38:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var output = [];
+  
+  for (var i = 0; i < musicians.length; i++) { 
+    output.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return output;
+}
+
+function johnLennonFacts(facts) {
+  for (var i = 0; i < facts.length; i++) {
+    facts[i] = facts[i] + "!!!";
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(number) {
+  var output = [];
+  
+  do {
+    output.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  
+  return output;
+}
