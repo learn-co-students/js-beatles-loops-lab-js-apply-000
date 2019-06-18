98bab8efe73075233a8d333abcb3d858b1825056
Lisa Li <lisa.li72159@gmail.com>
2019-06-18 23:22:25 +0000

    Done.

--- README.md
+++ README.md
@@ -48,3 +48,6 @@ number is less than `15`. The function should return the array with the strings
 `"I love the Beatles!"`.
 
 <p data-visibility='hidden'>View <a href='https://learn.co/lessons/js-beatles-loops-lab' title='Beatles Loops'>Beatles Loops</a> on Learn.co and start learning to code for free.</p>
+
+
+
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments) {
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++) {
+    var newStr = '';
+    var musician = musicians[i];
+    newStr += musician + " plays " + instruments[i];
+    arr.push(newStr);
+  }
+  return arr;
+}
+
+function johnLennonFacts(arr) {
+  var newArr = [];
+  var i = 0;
+  while (i < arr.length) {
+    newArr.push(arr[i] + "!!!");
+    i++;
+  }
+  return newArr;
+}
+
+function iLoveTheBeatles(num) {
+  var arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    num++;
+  }
+  while (num < 15);
+  return arr;
+}
