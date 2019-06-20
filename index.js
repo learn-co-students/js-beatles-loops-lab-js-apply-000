013d1fd167931d8202c9b3f7d35a8266ad9241c1
Hanwei <whwhanwei@gmail.com>
2019-06-20 02:58:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+function solfFacts(){
+  for(var a = 0 ;a<fact.length;a++){
+   console.log(facts[a]);
+  }
+}
