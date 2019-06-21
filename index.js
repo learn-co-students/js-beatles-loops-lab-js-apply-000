60f1c8fd5599834c4d46599710e089b361178941
Bruno Calderon <b@brunocalderon.com>
2019-06-21 08:36:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let musiciansPlay = [];
+  let i = 0;
+  for (i = 0; i < musicians.length; i++) {
+    musiciansPlay.push(musicians[i]+' plays '+instruments[i]);
+  }
+  return musiciansPlay;
+}
+
+function johnLennonFacts(facts) {
+  let theFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    theFacts.push(facts[i]+'!!!');
+    i++;
+  }
+  return theFacts;
+}
+
+function iLoveTheBeatles(index) {
+  do {
+    switch (index) {
+      case 7:
+        var iLoveThemToo = ["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"];
+        break;
+      case 17:
+        var iLoveThemToo = ["I love the Beatles!"];
+        break;
+    }
+    return iLoveThemToo;
+  }
+  while (index === 7 || index === 17);
+}
