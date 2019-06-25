73bd46d0c382829b1c718914959ac26c4da3eafa
Austin Smith <trunk9slamchest@gmail.com>
2019-06-25 16:50:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var new_arr = []
+    for (var i = 0; i < 4 ; i++) {
+      new_arr.push(musicians.shift() + ' plays ' + instruments.shift())
+    }
+  return new_arr
+}
+
+function johnLennonFacts(array) {
+  var new_arr = []
+    while (array.length > 0) {
+      new_arr.push(array.shift() + '!!!')
+    }
+  return new_arr
+}
+
+function iLoveTheBeatles(n) {
+  var i = 0
+  var new_arr = []
+	do {
+		i = i + 1;
+		new_arr.push('I love the Beatles!');
+	} while (i < 15 - n);
+  return new_arr
+}
