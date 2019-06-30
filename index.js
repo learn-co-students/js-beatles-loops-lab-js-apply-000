f088cccc7674553c3a9b8946f112035bb630c92c
inherited-window-0818 <robt15@hotmail.com>
2019-06-30 21:38:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  var i;
+  for(i = 0; i < musicians.length; i++){
+    array[i] = musicians [i] + ' plays ' + instruments [i];
+  }
+  
+  return array;
+  
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  var add = '!!!';
+  var newEntry;
+  while(i < facts.length){
+    newEntry = facts[i].concat(add);
+    facts[i] = newEntry;
+    i++;
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(number){
+  var array = [];
+  var i = 0;
+  
+  do{
+    array[i] = 'I love the Beatles!';
+    i++;
+    
+  } while (i <= number && number < 15)
+  
+  return array;
+}
