7bf8d6e958ae609a0a80f24690e0f25540d7e1c3
Asif Khan <akhan@codeinreach.com>
2019-07-02 20:44:03 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Beatles Loops
+,m# Beatles Loops
 
 ## Objectives
 
--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+
 // add solution here
+
+var arr= [];
+    
+
+function theBeatlesPlay(musicians,instruments){
+  
+for (let i = 0; i < 4; i++) {
+    arr.push(musicians[i] +" plays "+ instruments[i])
+
+}
+return arr;
+}
+
+function johnLennonFacts(string) 
+{
+    var i= 0;
+    var jLFarr= [];
+    
+    while (i < string.length) 
+    {
+        jLFarr.push(string[i] + "!!!");
+        i++;
+    }
+    return jLFarr;
+}
+
+function iLoveTheBeatles(number){
+    var arr1 = [];
+    var i = -1;
+    do{
+        i++;
+        arr1.push("I love the Beatles!");
+        
+    }while (i <number && number != 17);
+    return arr1;
+
+}
