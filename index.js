d58cd7b6d0a238a64555a77cf459b09f295287dd
alluring-kernel-9567 <jjss886@gmail.com>
2019-07-08 04:22:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let base = [];
+  for(let i = 0; i < musicians.length; i++) {
+    base.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return base;
+}
+
+function johnLennonFacts(facts) {
+  let length = facts.length
+  let count = 0;
+  let base = []
+  while (count < length) {
+    base.push(facts[count] + '!!!');
+    count ++;
+  }
+  return base;
+}
+
+function iLoveTheBeatles(num) {
+  let phrase = "I love the Beatles!"
+  let i = 0;
+  let base = [];
+  num = num % 17;
+  do {
+    base.push(phrase);
+    i++;
+  } while (i <= num)
+  return base
+}
