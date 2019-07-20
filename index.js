01e52658934268fbf4586b01cb78e1bfb4a18451
capable-daemon-6664 <hello@isabelklee.co>
2019-07-20 16:16:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+// This is a for loop.
+function theBeatlesPlay (musicians, instruments) {
+  let statement = [];
+
+for (let i = 0; i < musicians.length; i++) {
+      statement.push(musicians[i] + ' plays ' + instruments[i]);
+   }
+   return statement;
+}
+
+// This is a while loop.
+function johnLennonFacts (facts) {
+  let johnLennonFacts = [];
+
+let i = 0;
+while (i < facts.length) {
+  johnLennonFacts.push(facts[i] += "!!!");
+  i++;
+   }
+  return johnLennonFacts;
+}
+
+//This is a do-while loop.
+function iLoveTheBeatles(number) {
+  let beatleLover= [];
+
+do {
+    beatleLover.push('I love the Beatles!');
+    number++;
+  } while (number < 15);
+  
+ return beatleLover;
+}
