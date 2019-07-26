3678863160672d3a3b826eab9195202640493913
Anisha Miah <anishamiah@gmail.com>
2019-07-26 06:05:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// add solution here
+function theBeatlesPlay(arrMus, arrIns){
+  let emptArray = []
+   for (let i = 0; i < arrMus.length ; i++){
+         let str = arrMus[i] + " plays " + arrIns[i]
+         emptArray.push(str)
+   }
+   return emptArray
+ 
+} 
+
+function johnLennonFacts(arrFacts){
+  let factsArr = []
+  for ( let i = 0; i< arrFacts.length; i++){
+    let str = arrFacts[i] + "!!!"
+    factsArr.push(str)
+  }
+  return factsArr
+  
+}
+function iLoveTheBeatles(num){
+  let emptyArray = []
+  let str = "I love the Beatles!"
+  do {
+    emptyArray.push(str)
+    num = num + 1
+  }
+  while(num<15) 
+  
+  return emptyArray
+  
+}
+  
+  
+  
