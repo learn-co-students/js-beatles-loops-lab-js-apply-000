949675894edbb20816fd8ed61862cbd086526608
Zachary Flowers <zflowers0@gmail.com>
2019-07-27 19:27:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let list = [];
+  
+  for (let i = 0; i < musicians.length; i++) {
+    list[i] = musicians[i] + ' plays ' + instruments[i];
+  }
+  return list;
+}
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  while (i < facts.length) {
+    facts[i] += '!!!';
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  let list = [];
+  let i = num;
+  do {
+    i++;
+    list.push("I love the Beatles!");
+  } while (i < 15);
+  
+  return list;
+}
