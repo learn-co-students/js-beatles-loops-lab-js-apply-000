facad98e3035340447db81e2db267673a27ae9b1
artificial-semicolon-2761 <jimmy.h.cheung@gmail.com>
2019-07-30 05:43:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = []
+  for (var i = 0; i < musicians.length; i++) {
+    var sentence = musicians[i] + " plays " + instruments[i]
+    emptyArray.push(sentence)
+  }
+  return emptyArray
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+    facts[i] += "!!!"
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(num) {
+  var emptyArray = []
+  do {
+    emptyArray.push("I love the Beatles!")
+    num++
+  }
+  while(num < 15)
+  return emptyArray
+}
