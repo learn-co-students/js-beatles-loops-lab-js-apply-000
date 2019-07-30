e1504ac4dc5f3c7ecd1fb402edf70dedb1622a82
Mark DeNardo <mark.denardo@gmail.com>
2019-07-30 22:28:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+ function theBeatlesPlay(musicians, instruments){
+  var theBeatlesPlay = [];
+  
+   for (var i = 0; i < musicians.length; i++){
+    musicians[i]+" plays "+ instruments[i];
+     theBeatlesPlay.push(musicians[i]+" plays "+ instruments[i]);
+   }
+  return theBeatlesPlay;
+ }
+ 
+ function johnLennonFacts(factsArray){
+    var i=0;
+    var facts = [];
+      while (i<factsArray.length){
+      facts.push(factsArray[i]+"!!!");
+      i++;
+    }
+   return facts;
+ }
+ 
+ function iLoveTheBeatles(number){
+   var emptyArray = [];
+  
+   do {
+     emptyArray.push("I love the Beatles!")
+     number++;
+   }
+   while (number<15);
+   return emptyArray;
+   
+ }
+ 
