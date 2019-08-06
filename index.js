d6dfc3ef4a7c7d210e48077561a1a74d158b25d5
Donald Lobree <edl1155@gmail.com>
2019-08-06 03:48:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+
+function theBeatlesPlay(arrayMus, arrayInst) {
+  var newArr = [];
+  for (let i = 0; i < arrayMus.length; i++) {
+    newArr.push(`${arrayMus[i]} plays ${arrayInst[i]}`);
+  }
+  return newArr;
+}
+
+function johnLennonFacts(facts) {
+  const newArr = [];
+  for ( let fact of facts) {
+    newArr.push(`${fact}!!!`);
+  }
+  return newArr;
+}
+
+function iLoveTheBeatles(num) {
+  const myArr = [];
+  if (num < 15) {
+    let i = 1;
+    do {
+      myArr.push(`I love the Beatles!`);
+      i++;
+    }
+    while (i <= (15 - num));
+  } else if ( num === 17) {
+    return ['I love the Beatles!'];
+  }
+  return myArr;
+}
