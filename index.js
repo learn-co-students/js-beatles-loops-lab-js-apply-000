15779016aee618361abf4041c2ca516f9594842c
Andre Barreto <andrelbarreto@yahoo.com.br>
2019-08-13 05:03:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(players, instruments) {
+   var allPlayers = [];
+   for (var i=0; i < players.length; i++) {
+     allPlayers.push(players[i] + "plays" + instruments[i]);
+   }
+   return allPlayers
+}
+
+function iLoveTheBeatles(num) {
+  var love = [];
+  do {
+      love.push("I love the Beatles!");
+      num++;
+  } while (num < 15);
+  
+  return love;
+  }
