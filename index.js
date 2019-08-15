be4fdd20ac9cec344aefc17f592692b38bd845bd
sunny-utility-4827 <mzakzook@me.com>
2019-08-15 18:56:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var pair = [];
+  var index;
+  for (index = 0; index < musicians.length; index++) { 
+    pair.push(`${musicians[index]} plays ${instruments[index]}`); 
+  } 
+  return pair;
+}
+
+function johnLennonFacts(facts) {
+  var bigFacts = [];
+  var index = 0;
+  while (index < facts.length) {
+    bigFacts.push(`${facts[index]}!!!`);
+    index++;
+  }
+  return bigFacts;
+}
+
+function iLoveTheBeatles(number) {
+  var loveBeatles = [];
+  do {
+  loveBeatles.push('I love the Beatles!');
+  number++;
+  }
+  while (number < 15);
+  return loveBeatles;
+}
