867028df17577bf17f16ccdaaa30e331062e9fe6
Nathan Bohn <nathantbohn@gmail.com>
2019-08-25 01:37:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var startsEmpty = [] 
+  for(var i = 0; i<musicians.length;i++){
+    var string = musicians[i] + ' plays ' + instruments[i]
+    startsEmpty[i] = string 
+  }
+  return startsEmpty
+}
