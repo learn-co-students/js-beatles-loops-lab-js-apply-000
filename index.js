a3b4baea1a24f8e88fa61213eb64e7c7748a88d6
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-28 06:36:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+const theBeatlesPlay = (arr1,arr2) =>{
+  let newArr = []
+  for(let i=0;i<arr1.length; i++){
+    newArr.push(`${arr1[i]} plays ${arr2[i]}`)
+  }
+  return newArr;
+}
+
+const johnLennonFacts = arr =>{
+  let newArr = []
+  let i = 0
+  while(i<arr.length){
+    newArr.push(arr[i]+"!!!")
+    i+=1
+  }
+  return newArr
+}
+
+const iLoveTheBeatles = num =>{
+  let arr = []
+  do{
+    arr.push('I love the Beatles!')
+    num+=1
+    
+  }while(num<15)
+  return arr
+}
