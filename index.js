56ca116a9543d5b4c5cc7507079f4b141c5ba72f
considerate-glossary-7180 <clarkandkathy@gmail.com>
2019-08-28 21:24:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var description = []; 
+  
+  for (i = 0; i < musicians.length; i++) {
+    var string = musicians[i] + " plays " + instruments[i];
+    description.push(string);
+  }
+  return description;
+}
+
+function johnLennonFacts() {
+  
+}
+
+function iLoveTheBeatles() {
+  
+}
