94608343dd91a767585336517ce038b56b4c6f27
Juan A Guzman <juniorguzman02@hotmail.com>
2019-09-03 20:38:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // add solution here
+function theBeatlesPlay(players, instruments) {
+    var theBand= [];
+    for (var i=0; i < players.length; i++) {
+       theBand.push(players[i] + " plays " + instruments[i]);
+    }
+    return theBand;
+    
+}
+
+
+
+function johnLennonFacts(fact) {
+  var i = 0
+  while (i < fact.length) {
+    fact[i] += '!!!';
+    i++;
+  }
+  return fact;
+}
+ 
+
+
+function iLoveTheBeatles(num) {
+    var emptyArray = [];
+    do {
+        emptyArray.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return emptyArray;
+}
+
+  
+
+
+
+
