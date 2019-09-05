127feb2a137a58753092036e03aa525fcd4d976d
Jacob Knopf <jmknopf1007@gmail.com>
2019-09-05 15:51:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// add solution here
+function theBeatlesPlay(players, instruments) {
+    var allPlayers = [];
+    for (var i=0; i < players.length; i++) {
+        allPlayers.push(players[i] + " plays " + instruments[i]);
+    }
+    return allPlayers;
+}
+function johnLennonFacts(array) {
+   var i = 0;
+   while (i < array.length) {
+    array[i] = array[i] + "!!!";
+     i++;
+   }
+   return array;
+}
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
