d26163a96de4b37bcaadfaa63884eb622304758e
Connie Lai <connielion011@gmail.com>
2019-09-10 02:59:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // add solution here
+function theBeatlesPlay(musArr, instrArr){
+  const res = [];
+  for(let i = 0; i < musArr.length; i++){
+    let instrument = instrArr[i];
+    let name = musArr[i];
+    res.push(`${name} plays ${instrument}`);
+  }
+  return res;
+}
+
+const johnLennonFacts = factsArr => {
+  let i = 0;
+  const res = [];
+  while(i<factsArr.length){
+    let fact = factsArr[i]+'!!!';
+    res.push(fact);
+    i+=1;
+  }
+  return res;
+}
+
+const iLoveTheBeatles = num => {
+  const arr = [];
+  
+  let i = num;
+  
+  do {
+   arr.push("I love the Beatles!");
+   i++;
+  }
+  while (i < 15);
+  
+  return arr;
+}
+
