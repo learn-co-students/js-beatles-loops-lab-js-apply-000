ee1ed227bc553e33e159af0d41ab70760024289f
candescent-media-9936 <alexbonwit@mac.com>
2019-09-15 01:06:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var beatlesArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    beatlesArray.push(musicians[i] +' plays ' + instruments[i]);
+  }
+  return beatlesArray;
+}
+
+function johnLennonFacts(factsArray) {
+  var factsIndex = 0;
+  while (factsIndex < factsArray.length) {
+    factsArray[factsIndex] += '!!!';
+    factsIndex++;
+  }
+  return factsArray;
+}
+
+function iLoveTheBeatles(num) {
+  var arr = [];
+  do {
+    arr.push('I love the Beatles!');
+    num++;
+  } while (num < 15);
+  return arr;
+}
