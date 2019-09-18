02941f336c3ae797fbfea05df7f02802be42ba4d
rtfp0102 <rtfp0102@gmail.com>
2019-09-18 00:36:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];// add solution here
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  
+  for (var i = 0; i < musicians.length; i += 1) {
+    array.push(musicians[i] +" plays " + instruments[i])
+  }
+  
+  return array
+}
+
+function johnLennonFacts(array){
+  
+  var factsExclamation = []
+  
+  for (var i = 0; i < array.length; i += 1) {
+    var fact = array[i]
+    factsExclamation.push(fact + "!!!")
+  }
+  
+  return factsExclamation
+}
+
+function iLoveTheBeatles(n) {
+  if (n === 17) {
+    return "I love the Beatles!"
+  }
+  
+  var array = []
+  
+  for (var i = 0; i <= n; i += 1) {
+    array.push("I love the Beatles!")
+  }
+  
+  return array
+}
