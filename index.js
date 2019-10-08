e01aa77847909b2ffdbf5a835ef87d37431913f7
Aaron Briscoe <abriscoe2008gmail.com>
2019-10-08 16:15:03 -0500

    Done.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ theBeatlesPlay = (musicArr, instrArr) => {
     var arr = []
     var i
     for (i = 0; i < musicArr.length && instrArr.length; i++) {
-        arr.push(`${musicArr[i]}` + 'plays'+ `${instrArr[i]}`)
+        arr.push(`${musicArr[i]}` +' plays '+ `${instrArr[i]}`)
 
         return  arr
     }
