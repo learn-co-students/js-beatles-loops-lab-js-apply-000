f3e13123579122d6c8eebadee4961013e6218fda
Sam Hoidal <sjhoidal@gmail.com>
2019-10-26 14:49:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // add solution here
 
-/* 1)
-Create a function theBeatlesPlay, which accepts two parameters--an array of musicians and an array of instruments.
-Create a variable with an empty array.
-Use a for loop, which iterates over the array. of musicians. Be careful about what value you set your counter variable to store. (Hint: Think about what the first index of an array is).
-The first time through the loop, it should create a string using the first index of the musicians array and the first index of the instruments array: "John Lennon plays guitar".
-Add this string to the empty array you created. The loop should make the same sentence for every member of the musicians array.
-The function should return the array of new strings containing what instruments each musician plays.
+/*
+Defined "musicians" and "instruments" as arrays to serve as my arguments in theBeatlesPlay function.
+Created theBeatlesPlay function using musicians and instruments as parameters.
+Declared band and defined it as an empty array as collector for returned values.
+Created for loop by adding an initializer to start at the beginning of my arrays, a condition to loop until the it has reached the end of the arrays using length method, and increment by one each time.
+Used .push method to create concatenated string within band with the correspodning arguments of each loop iteration. 
+Return array that has been collected in band.
 */
-
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
@@ -20,19 +19,9 @@ function theBeatlesPlay(musicians, instruments) {
   return band;
 }
 
-/* 2) 
-Create a function johnLennonFacts.
-This function will accept one argument, an array of facts about John Lennon (note that it might not be exactly the following facts):
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-Use a while loop to loop over the facts array and add "!!!" to the end of every fact.
-Return an array of strings with exclamation points.
+/*
+Same logic as above for loop. While loop would continue to run until condition is false, where for loop runs for a defined number of iterations. In these scenarios, a for loop could be used for both (or vise versa).
 */
-
  const facts = [ 
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
@@ -49,21 +38,17 @@ function johnLennonFacts(facts) {
   return nextFact;
 }
 
-/* 3)
-Create a function iLoveTheBeatles which accepts a number as a parameter.
-The function should create a variable that stores an empty array.
-Implement a do-while loop inside the function that adds "I love the Beatles!" to the empty array.
-The loop should then increment the number passed in as a parameter. The condition of the loop should check to see that the parameter number is less than 15. It should contain a number of strings that represents the difference between the input number and 15.
-Return the array with the strings "I love the Beatles!".
+/*
+Just like while loop, except the do-while loop executes at least once before the while condition is tested. 
 */
 
-var iLoveTheBeatles = function(number) {
+var iLoveTheBeatles = function(num) {
   var heyJude = [];
   do {
     heyJude.push("I love the Beatles!");
-    number++;
+    num++;
   }
-  while (number < 15);
+  while (num < 15);
   return heyJude
 }
   
