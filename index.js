5b89e3342f4e4ab24934e436670226354309eeb6
Sam Hoidal <sjhoidal@gmail.com>
2019-10-27 16:33:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
 // add solution here
 
-/*
-Defined "musicians" and "instruments" as arrays to serve as my arguments in theBeatlesPlay function.
-Created theBeatlesPlay function using musicians and instruments as parameters.
-Declared band and defined it as an empty array as collector for returned values.
-Created for loop by adding an initializer to start at the beginning of my arrays, a condition to loop until the it has reached the end of the arrays using length method, and increment by one each time.
-Used .push method to create concatenated string within band with the correspodning arguments of each loop iteration. 
-Return array that has been collected in band.
-*/
 var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
-  var band = []
-  for (var i=0; i<musicians.length; i++) {
+  let band = []
+  for (let i=0; i<musicians.length; i++) {
     band.push(musicians[i] + ' plays ' + instruments[i]);
   }
   return band;
 }
 
-/*
-Same logic as above for loop. While loop would continue to run until condition is false, where for loop runs for a defined number of iterations. In these scenarios, a for loop could be used for both (or vise versa).
-*/
+
  const facts = [ 
     "He was the last Beatle to learn to drive",
     "He was never a vegetarian",
@@ -29,7 +19,7 @@ Same logic as above for loop. While loop would continue to run until condition i
     "He hated the sound of his own voice"];
 
 function johnLennonFacts(facts) {
-  var nextFact = [];
+  let nextFact = [];
   let i = 0;
   while (i<facts.length) {
     nextFact.push(facts[i] + "!!!");
@@ -38,12 +28,9 @@ function johnLennonFacts(facts) {
   return nextFact;
 }
 
-/*
-Just like while loop, except the do-while loop executes at least once before the while condition is tested. 
-*/
 
 var iLoveTheBeatles = function(num) {
-  var heyJude = [];
+  let heyJude = [];
   do {
     heyJude.push("I love the Beatles!");
     num++;
