06f0ed7505247d893982229a5cd5fe25d64104da
Kent Chan <kntchn@gmail.com>
2019-11-14 23:25:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arrayVariable = [];
+  for (int counter = 0; counter < musicians.length - 1; counter++) {
+    createdString = "musicians(counter) " + "plays" + "instruments(counter)" ;
+    arrayVariable.push(createdString);
+  }
+  return arrayVariable
+}
+
+//second function
+//function johnLennonFacts {
+
+//}
+
 // add solution here
