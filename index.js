cd1015ce1ef3fb52aa3c4935560496945e9a1b59
anam soomro <anamsoomroed@gmail.com>
2019-11-19 01:00:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // add solution here
 function theBeatlesPlay (musicians, instruments){
   var beatlesPlay = []
-  var i
-  for (i=0 ; i<musicians.length ; i++){
+  for (var i=0 ; i<musicians.length ; i++){
     beatlesPlay.push(`${musicians[i]} plays ${instruments[i]}`)
   }
   return beatlesPlay
