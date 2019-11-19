05bdf8ceed20298aadc1a80a3a6638534c7e136f
anam soomro <anamsoomroed@gmail.com>
2019-11-19 01:32:55 +0000

    Done.

--- index.js
+++ index.js
@@ -8,17 +8,32 @@ function theBeatlesPlay (musicians, instruments){
 }
 
 
-function johnLennonFacts(facts){
-  var excitingFacts=[]
-  var i = 0
-  while (i < facts.length){
-    excitingFacts.push(`${facts[i]}!!!`)
-    i++
-  }
-  return excitingFacts
-}
+// function johnLennonFacts(facts){
+//   var excitingFacts=[]
+//   var i = 0
+//   while (i < facts.length){
+//     excitingFacts.push(`${facts[i]}!!!`)
+//     i++
+//   }
+//   return excitingFacts
+// }
+
+//this function doesnt change global facts array
 
 //come back to try to do the above without making a new array 
+
+function johnLennonFacts (facts){
+ var i = 0
+  while (i < facts.length){
+    facts[i] = `${facts[i]}!!!`
+    i++
+  }
+  return facts
+}
+//this function changes the global facts array
+
+
+
 
 function iLoveTheBeatles(number){
   var howMuchLove=[]
