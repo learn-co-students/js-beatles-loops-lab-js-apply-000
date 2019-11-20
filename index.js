3b23cdb5a2bdb9a6fb2e955507c09a810c9e199b
Fika <afzalov98@gmail.com>
2019-11-20 15:21:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments) {
+let emptyArray = [];
+for(let i = 0; i < musicians.length; i++) {
+  emptyArray.push(musicians[i] + ' plays ' + instruments[i]);
+}
+return emptyArray
+}
+
+function johnLennonFacts(facts){
+  let array = [];
+  let count = 0;
+  while (count < facts.length) {
+  array.push(facts[count] + '!!!');
+  count++;
+}
+return array
+}
+
+
+
+
+
+
