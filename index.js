5811bbf9292059accc2999fcc25e72095e793b9b
affable-invocation-5984 <esokoletsky@gmail.com>
2019-11-21 05:15:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+
+
+
+let count = 0
+
+
+function theBeatlesPlay(musician, instrument) {
+  let array = []
+  for(let i=0; i<=musician.length; i++) {
+    return array.push(`${musician[i]} plays ${instrument}`)
+  }
+}
