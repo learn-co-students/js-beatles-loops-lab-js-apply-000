054d83a03fe03acfe69af31c0e2fd8626623eb1f
affable-invocation-5984 <esokoletsky@gmail.com>
2019-11-22 16:33:50 +0000

    Done.

--- index.js
+++ index.js
@@ -6,8 +6,30 @@ let count = 0
 
 
 function theBeatlesPlay(musician, instrument) {
-  let array = []
-  for(let i=0; i<=musician.length; i++) {
-    return array.push(`${musician[i]} plays ${instrument}`)
+  let array = [];
+  for(let i=0; i < musician.length; i++) {
+     array.push(`${musician[i]} plays ${instrument[i]}`)
   }
+  return array;
+}
+
+
+function johnLennonFacts(array) {
+  let n = 0;
+  while(n < array.length) {
+    array[n] += "!!!"
+    n++
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number) {
+  let array = [];
+
+  do {
+    number++;
+    array.push("I love the Beatles!")
+    
+  } while(number < 15)
+  return array;
 }
