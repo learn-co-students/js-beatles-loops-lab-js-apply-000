314acaa477124145d1febce64e4d35dfa2bcd857
Kent Chan <kntchn@gmail.com>
2019-12-05 02:26:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 function theBeatlesPlay(musicians, instruments) {
   var arrayVariable = [];
-  for (int counter = 0; counter < musicians.length - 1; counter++) {
-    createdString = "musicians(counter) " + "plays" + "instruments(counter)" ;
+  for (var counter = 0; counter < musicians.length; counter++) {
+    let createdString = musicians[counter] + " plays " + instruments[counter] ;
     arrayVariable.push(createdString);
   }
   return arrayVariable
 }
 
 //second function
-//function johnLennonFacts {
-
-//}
+function johnLennonFacts(facts) {
+  var newFacts = [];
+  for (let i=0; i < facts.length; i++) {
+    let newString = facts[i] + "!!!"
+    newFacts.push(newString)
+  }
+  return newFacts
+}
 
 // add solution here
+function iLoveTheBeatles(num) {
+  let i = 15-num;
+  let emptArray = [];
+
+  while(i <= 15 && num < 15) {
+    emptArray.push("I love the Beatles!");
+    i++;
+  }
+
+  if(num > 15) {
+    emptArray.push("I love the Beatles!")
+  }
+
+  return emptArray;
+}
