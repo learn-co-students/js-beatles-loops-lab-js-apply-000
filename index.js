65c7dd3f11ba2568f912bed6d7618fe4b2e31cac
patrick leal <pjleal3846@icloud.com>
2019-12-13 21:13:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay(musicians,instruments){
+var myarray =[];
+let count = 0;
+while (count < musicians.length){
+  myarray.push(musicians[count] + " plays " + instruments[count])
+  count++;
+  } 
+  return myarray
+}
+
+function johnLennonFacts(facts){
+  var myarray = [];
+  let count = 0;
+  while (count < facts.length){
+    myarray.push(facts[count] + "!!!");
+    count++;
+  }
+  return myarray
+}
+function iLoveTheBeatles(number){
+  var text = []
+    do {
+   text << "I Love the Beatles!"
+   number++;
+  }  while (number < 15)
+  return text
+} 
+
