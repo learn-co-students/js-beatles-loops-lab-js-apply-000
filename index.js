6287602990f926f1eb7b3d884dd2fa551c766bce
awesome-partial-8458 <ningelvis@gmail.com>
2019-12-18 17:34:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var empty = [];
+  for(var i = 0; i < musicians.length; i++){
+    var value1 = musicians[i];
+    var value2 = instruments[i];
+    empty.push(""+value1+" plays "+value2+"");
+  }
+  return empty;
+} 
+
+function johnLennonFacts(facts){
+  var empty = [];
+  var index = 0;
+  while(index < facts.length){
+    empty.push(facts[index] + "!!!");
+    index++;
+  }
+  return empty;
+}
+
+function iLoveTheBeatles(number){
+  var empty = [];
+  var text = "I love the Beatles!";
+  var i = number;
+  do{
+    i = i + 1;
+    empty.push(text);
+  }
+  while(i<15);
+  return empty;
+}
