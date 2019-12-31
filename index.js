d9729a8535bd6d180420c5735db207675fbcab9e
upbeat-queue-5483 <bryan.lesht@colorado.edu>
2019-12-31 18:02:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+
+
+
+function theBeatlesPlay(musicians, instruments) {
+let musicianPlusInstrument = [];
+for (let i =0; i<(musicians.length);i++) {
+ 
+  musicianPlusInstrument.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return musicianPlusInstrument
+}
+
+function johnLennonFacts(facts) {
+  let arraycounter = 0
+  let factsEx = []
+  do {factsEx.push(`${facts[arraycounter]}!!!`)
+	arraycounter+=1}
+  while (facts.length > arraycounter) 
+  
+  return factsEx
+  
+}
+
+function iLoveTheBeatles(num) {
+  let beatlesLove = []
+  do {beatlesLove.push('I love the Beatles!')
+    num ++
+  }
+  
+  while (num<15)
+  return beatlesLove
+}
