655e09bd299846976558d67d852f7771c405a17e
Samuel Lesser <ineedmoreplates@gmail.com>
2020-01-02 22:05:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+var legends = [];
+var i = [];
+
+function theBeatlesPlay(musicians,instruments) {
+    for (i = 0; musicians.length > i; i++){
+      legends.push(musicians[i]+' plays '+instruments[i]);
+}
+         return legends;
+}
+  
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];  
+  
+function johnLennonFacts(facts) {
+  var i = 0;
+  var yell = [];
+    while(i < facts.length){
+      yell.push(facts[i] +'!!!');
+        i++;
+    }
+        return yell;
+}
+ 
+function iLoveTheBeatles(number) {
+  var i = [];
+  var love = [];
+ do{facts[i]+'I love the Beatles!'}
+  while(i < 15);
+  love.push(facts[i]+'I love the Beatles!');
+   i++;
+      return love;
+}
