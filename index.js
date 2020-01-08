46fee99576c1e94e1705ec2b27a98b530857cef8
Matthew Peak <matthewpeak@gmail.com>
2020-01-08 17:07:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(dude,instrument){
+ let x =[];
+ for(let i=0;i<dude.length;i++){
+   x.push(dude[i]+" plays "+instrument[i]);
+ }
+ return x ;
+}
+
+function johnLennonFacts(facts){
+let x =[];
+ for(let i=0;i<facts.length;i++){
+   x.push(facts[i]+"!!!");
+ }
+ return x ;
+}
+
+function iLoveTheBeatles(n) {
+  var x = [];
+  var i = 0;
+  do {
+    x.push('I love the Beatles!');
+    i++;
+  } while (i <= n && n < 15);
+  return x;
+}
+
+
+
