0c5a88ce70b782794edae66959a8194009156cbe
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-01-11 04:01:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let arr = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  
+  return arr;
+}
+
+function johnLennonFacts(arr) {
+  let exclaim = arr.map(el => el + '!!!');
+  return exclaim
+}
+
+function iLoveTheBeatles(num) {
+  let arr = [];
+  let start = num
+  do {
+    arr.push("I love the Beatles!");
+    start++;
+  } 
+  while (start < 15);
+  return arr;
+}
