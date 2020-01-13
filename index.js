04275557b7a1ae2edc15bd8a0163f1f72cc9821d
Mohana Bansal <mohanabansal10@gmail.com>
2020-01-13 21:57:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let result = [];
+  for(let i in musicians){
+    result.push(musicians[i]+ " plays " + instruments[i]);
+  }
+  return result;
+}
+
+function johnLennonFacts(facts){
+  let result = [];
+  let i = 0;
+  while(i < facts.length) {
+    result.push(facts[i]+"!!!");
+    i++;
+  }
+  return result;
+}
+
+function iLoveTheBeatles(num){
+  let result = [];
+  if(num === 17) return ['I love the Beatles!']
+  for(let i = 0; i< 15-num ; i++){
+    result.push('I love the Beatles!');
+  }
+  return result;
+}
+
