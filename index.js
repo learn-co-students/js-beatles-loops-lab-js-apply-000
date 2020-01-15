395cb2a8c1a5d7b51ac26e45a8537a000e2456d8
astounding-spooling-2354 <nlashner@gmail.com>
2020-01-15 03:14:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// add solution here
+
+
+function theBeatlesPlay(arr1, arr2) {
+  let newArr = []
+  for (let i = 0; i < arr1.length; i++){
+    let member = arr1[i]
+    let instrument = arr2[i]
+    newArr.push(member + ' plays ' + instrument)
+  }
+  return newArr
+}
+
+function johnLennonFacts(array){
+  let newArr = []
+  let i =0
+  while (i < array.length){
+    let newStr = array[i] + '!!!'
+    newArr.push(newStr)
+    i++
+  }
+  return newArr
+}
+
+
+function iLoveTheBeatles(num) {
+  let arr = []
+  do {
+    arr.push('I love the Beatles!')
+    num ++
+    }
+  while (num < 15)
+  return arr
+}
