bc8b6910a817247eabb5659d60429b0eeb651947
Madeleine Knox <madeleinelknox@gmail.com>
2020-01-20 16:22:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let sentences = []
+  for(let i = 0; i<musicians.length;i++){
+    let sentence = musicians[i]+" plays " + instruments[i]
+    sentences.push(sentence)
+  }
+  return sentences
+}
+
+function johnLennonFacts(facts){
+  let i = 0
+  while(i<facts.length){
+    facts[i]+= "!!!"
+    i++
+  }
+  return facts
+}
+
+function iLoveTheBeatles(number){
+  let result = []
+  do{
+    number++
+    let love = "I love the Beatles!"
+    result.push(love)
+  }while(number<15);
+  return result
+}
