cdae73b615d0a35e19390074e7c501c5316a6fcf
Zhiyuan Liu <cloudnotes.64@gmail.com>
2020-01-21 06:02:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments)
+{
+  var whoplayswhat = []
+  for(var i = 0; i < musicians.length; i++)
+  {
+    var sentence = musicians[i] + " plays " + instruments[i]
+    whoplayswhat.push(sentence)
+  }
+  return whoplayswhat
+}
+
+function johnLennonFacts(facts)
+{
+  var i = 0
+  while(i < facts.length)
+  {
+    facts[i] += "!!!"
+    i+= 1
+  }
+  return facts
+}
+
+function iLoveTheBeatles(num)
+{
+  var blah = []
+  do{blah.push("I love the Beatles!")
+     num += 1
+    }
+  while(num < 15)
+  return blah
+}
