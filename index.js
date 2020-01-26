99d001182c20c0a46d6d7969d2e607c712040bc4
Michelle Frattaroli <mishyjari@outlook.com>
2020-01-26 02:34:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+const theBeatlesPlay = (musicians,instruments) => {
+  const output = [];
+  for (let i = 0; i < musicians.length; i++ ) {
+    output.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return output;
+};
+
+const johnLennonFacts = facts => {
+  const output = [];
+  let i = 0;
+  while (i < facts.length) {
+    output.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return output;
+};
+
+const iLoveTheBeatles = n => {
+  const output = [];
+  do {
+    output.push('I love the Beatles!');
+    if ( n < 15 ) { n ++ }
+    else if ( n > 15 ) { n-- }
+    else if ( n === 17 ) { n = 15 }
+  } while ( n !== 15 );
+  return output;
+};
