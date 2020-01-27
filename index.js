5b7962a56f4d9711379ea171818f0e527c89db16
Sam schulenberg <schu3673@umn.edu>
2020-01-27 20:47:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // add solution here
+let index=[];
+function theBeatlesPlay(musicians,instruments){
+  for(let i=0;i<musicians.length;i++){
+    index.push(musicians[i]+" plays "+instruments[i]);
+  }
+  return index;
+}
+function johnLennonFacts(facts){
+    let x=0;
+    while(x<facts.length){
+      facts[x]=facts[x]+'!!!';
+      x++;
+    }
+    return facts;
+  }
+function iLoveTheBeatles(number){
+  var lov=[];
+  do{
+    lov.push("I love the Beatles!");
+    number++;
+  }
+  while(number<15);
+  return lov;
+}
+  
