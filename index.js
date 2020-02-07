dd72573ef8beb7551087d5704c32827b2c2df300
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-07 05:59:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  const imagine = []
+ 
+ for (let i = 0, l = musicians.length; i < l; i++) {
+    imagine.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+ return imagine;
+}
+ 
+
+function johnLennonFacts(facts){
+  const shoutedFacts =[]
+  
+  let i = 0;
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+}
+return shoutedFacts;
+}
+
+function iLoveTheBeatles(i) {
+  const empty = []
+  
+  do {
+    empty.push('I love the Beatles!')
+    i++
+  } while (i < 15);
+  return empty;
+  }
+  
+
