bb7065f3033004637a232920549b3a8309684dff
lively-bundle-0725 <tbandachit@gmail.com>
2020-02-11 08:33:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function theBeatlesPlay(musicians,  instruments) {
+  var sentence = [];
+  for(var i = 0; i < musicians.length; i++){
+    sentence[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return sentence;
+}
+
+var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(facts){
+  var sentence = [];
+  var i = 0;
+  while(i < facts.length){
+    sentence[i] = facts[i] + "!!!";
+    i++
+  }
+  return sentence
+  
+}
+
+function iLoveTheBeatles(x){
+  var sentence = [];
+  var i = 0
+  do{
+    sentence[i] = "I love the Beatles!";
+    i++
+  } while (x < 15 && i <= x);
+  return sentence;
+}
+
