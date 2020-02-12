50b89df751808c98ea64fc636479be88cc3f6d60
Scott Delbango <scott.delbango@gmail.com>
2020-02-12 05:31:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+var theBeatlesPlay = function() {
+  var fabFour = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+  var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+  var comeTogether = [];
+  for (var i = 0; i < fabFour.length; i++) {
+    comeTogether.push(fabFour[i] + " plays " + instruments[i]);
+  }
+  return comeTogether;
+};
+
+var facts = [];
+var johnLennonFacts = function(facts) {
+  var i = 0;
+    while (i < facts.length) {
+       facts[i] += "!!!";
+     i++;
+    }
+    return (facts);
+  };
+
+var i = 0;
+var n = 0;
+var iLoveTheBeatles = function(n) {
+  var superFan = [];
+  do {
+    superFan.push ("I love the Beatles!");
+    i++;
+  }
+  while ((n < 15) && (i < (15 - n)));
+  return superFan;
+};
