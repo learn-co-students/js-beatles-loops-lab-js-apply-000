c785bfea7c8556d20205b414475ce6ce4535567a
Ahmed Nasr <for_mido98@yahoo.com>
2020-02-15 23:50:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // add solution here
+var theBeatlesPlay = function(musicians,instruments){
+  var x=[]  
+  for (var i=0 ; i < instruments.length; i++ ) {
+    x[i] = musicians[i]+" plays "+ instruments[i]
+  }
+  return x
+  
+
+}
+var johnLennonFacts = function (facts){
+  var x = []
+var i = 0
+while (i<facts.length){
+  x[i]= facts[i]+"!!!";
+  i++;
+  
+}
+  return x
+  
+}
+var iLoveTheBeatles = function(n){
+ var x =[] 
+ var i =0
+ do{ 
+   
+   x[i]="I love the Beatles!";i++;
+   
+ }
+ while (i<15-n);
+ return x ;
+}
