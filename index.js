11b39e982b249445e837cb7fd618ab83fec822fc
Kurosh Hashemi <kurosh.hashemi@gmail.com>
2020-02-15 17:24:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+
+function theBeatlesPlay(musicians,instruments){
+  var x =[];
+  for(var m=0; m < musicians.length; m++){
+    x.push(musicians[m] + ' plays ' + instruments[m])
+  }
+  return x
+}
+
+function johnLennonFacts(facts){
+  var x = [];
+  var i = 0;
+  while(i < facts.length){
+    x.push(facts[i] + '!!!');
+    i++
+  }
+  return x
+}
+
+function iLoveTheBeatles(num){
+ var myArr = [];
+ var i =0;
+ do {
+   myArr.push('I love the Beatles!');
+   i++;
+ } while(i <= num && num <= 15);
+ return myArr
+}
