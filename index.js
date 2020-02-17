432134843f94a5a1aa4aa47aaac6130abbef9397
Ian McKenzie <imckenzie777@gmail.com>
2020-02-17 21:18:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let bandArr = [];
+  for (let i = 0; i < musicians.length && instruments.length; i++) {
+    bandArr.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+return bandArr;
+}
+function johnLennonFacts(arr) {
+  let i = 0;
+  let updArr = [];
+  const excl = '!!!';
+
+  while(i < arr.length) {
+    if(i % 2 === 0) {
+      updArr.push(arr[i] + excl);
+    } else {
+      updArr.push(arr[i] + excl);
+    }
+    i++
+  }
+  return updArr;
+}
+function iLoveTheBeatles(num) {
+  let fans = [];
+  let diffStmt = () => fans.push('I love the Beatles!');
+
+  do {
+    diffStmt();
+    num++;
+  } while (num > 0 && num < 15);
+
+  return fans;
+}
