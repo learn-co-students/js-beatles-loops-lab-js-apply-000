72c738d6d0bafb4d2a57f5d22cfb343385c0cb3d
Jansen Fink <jansenfink@charter.net>
2020-02-17 23:23:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// add solution here
+const combo = [];
+
+function theBeatlesPlay (beatlesMusicians, instruments) {
+
+for (var i =0; i<instruments.length; i++)
+  {
+    combo[i] = beatlesMusicians[i] + " plays " + instruments[i];
+  }
+  return combo;
+
+}
+ var facts = [ 'He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice', 'While on tour, he enjoyed playing Monopoly', 'Harmonica was the first instrument Lennon learned to play'];
+
+function johnLennonFacts (facts) {
+    var i = 0; 
+    while (i<facts.length){
+    facts[i] += '!!!';
+      i++;
+    }
+    return facts;
+}
+
+
+function iLoveTheBeatles(i){
+  var emptyArray = [];
+  do {
+    emptyArray.push("I love the Beatles!");
+    i++;
+  } while (i<15);
+  return emptyArray;
+}
