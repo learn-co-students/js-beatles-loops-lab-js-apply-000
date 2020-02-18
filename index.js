b07dd792bfaf969583cd7493b8cf287920e7e0b5
Adrian Head <soahead@gmail.com>
2020-02-18 19:40:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"], 
+    instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(musicians, instruments){
+  var empty =[];
+  for (var i = 0; i < musicians.length; i++){
+    var str = musicians[i] + " plays " + instruments[i];
+    empty = str;
+    console.log(empty);
+}
+}
+theBeatlesPlay(musicians, instruments)
+
+var facts = []
+function johnLennonFacts(facts){
+  
+}
+
+function iLoveTheBeatles(number){
+  
+}
