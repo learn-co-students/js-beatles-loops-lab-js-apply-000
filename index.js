bea88026103f8640d38aab083fdd4784022e561b
Adrian Head <soahead@gmail.com>
2020-02-19 23:51:32 +0000

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,22 @@ return empty
 }
 
 function johnLennonFacts(facts){
-  
+  var empty = [];
+  for (var i = 0; i < facts.length; i++){
+    var str = facts[i] + "!!!";
+    empty.push(str)
+  }
+  return empty
+}
+
+function iLoveTheBeatles(number){
+  var empty =[]
+  do
+{
+  var str = "I love the Beatles!";
+  empty.push(str);
+  number++;
+}
+while (number < 15); 
+return empty
 }
