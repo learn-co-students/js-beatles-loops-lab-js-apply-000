cb700a71b3e012ecef3131d57b443c109dc1deaa
Stephen Peters <7speter@gmail.com>
2020-02-21 04:56:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var BeatlesInstruments = [];
+  for (i = 0 ; i < musicians.length; i++){
+    BeatlesInstruments.push(musicians[i] + " plays " + instruments[i])
+  }
+}
