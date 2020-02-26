bab33997fb2f3f4513879f4b57ca8d11b2960846
Kelsey Dickson <kelseymedved@gmail.com>
2020-02-26 18:46:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  let newString = [];
+  for(let i = 0; i < musicians.length; i++){
+    newString.push(musicians[i] + ' plays ' + instruments[i]);
+    }
+return newString;
+}
+
+function johnLennonFacts(arrOfFacts){
+    let result = [];
+    let count = 0;
+    while(count < arrOfFacts.length){
+      result.push(arrOfFacts[count] + '!!!');
+      count++
+    }
+    return result;
+  }
