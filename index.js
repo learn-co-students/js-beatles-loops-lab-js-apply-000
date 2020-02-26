cb20f6b98737baba5634f36f8f303d83d2237f37
Akil Griffiths <akilgg@gmail.com>
2020-02-26 00:35:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+var sentences=[]
+function theBeatlesPlay(arr1, arr2){
+var i = 0
+
+  for (i=0 ; i < arr1.length ; i ++ ) {
+    sentences.push(arr1[i] + " plays " + arr2[i]);
+
+  }
+  return (sentences);
+}
+
+
+
+function johnLennonFacts(facts){
+var i = 0
+  while (i < facts.length) {
+
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return (facts)
+}
+
+function iLoveTheBeatles(num){
+var i = num
+var howMuch=[]
+  do {
+howMuch.push('I love the Beatles!');
+i++;
+  } while (i < 15);
+return (howMuch)
+}
