8659287dde0b26c9ccfec326826c8b92fa6bfa59
Jordan Melidor-Fuxis <jordan.melidor@hotmail.fr>
2020-02-26 21:42:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+
+function theBeatlesPlay(array1, array2){
+  var array = []
+  for (let i= 0; i <array1.length; i++){
+    array.push(array1[i]+ " plays " + array2[i])
+  }
+  return array
+}
+
+function johnLennonFacts(array){
+  var i = 0
+  while (i <array.length){
+    array[i] += "!!!"
+    i++
+  }
+  return array
+}
+
+function iLoveTheBeatles(int){
+  var array = []
+  do {
+   array.push("I love the Beatles!")
+   int++
+  }while (int < 15);
+  return array
+}
