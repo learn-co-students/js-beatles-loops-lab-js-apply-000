34247e76c0743e4002432b1185534d2bf9fc5f9d
Jae Nwawe <nwawe.jae@gmail.com>
2020-03-01 04:10:32 +0000

    Done.

--- index.js
+++ index.js
@@ -14,11 +14,35 @@ function theBeatlesPlay(musicians, instruments){
 
 
 function johnLennonFacts(facts){
-  var index;
+  var index = 0
+  var surprise= []
   while(index < facts.length){
-    var together = '${facts[index]} + !!!';
-    facts[index] += "!!!"
-    index++;
+
+   var together= `${facts[index]}!!!`
+  
+  surprise.push(together)
+    index++
   }
-  return facts;
+  return surprise
+}
+
+function iLoveTheBeatles(number){
+  let counter
+  
+  
+  if (number >15) counter = Math.abs(15,number)
+  if (number < 15)  counter = 14 - number
+  
+  var loves =  "I love the Beatles!"
+  var lovesArray = []
+  if (number ==8) counter = 8
+  do{
+  
+    lovesArray.push(loves)
+     counter++
+  }while (counter < 15)
+  
+  
+  return lovesArray
+  
 }
