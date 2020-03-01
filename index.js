9033a30953147c9617b0647d1feea805116074a0
Jae Nwawe <nwawe.jae@gmail.com>
2020-03-01 02:10:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+ var mPlays = [] 
+  var counter
+  for(counter = 0; counter < musicians.length; counter++){
+  
+    var together= `${musicians[counter]} plays ${instruments[counter]}`
+    mPlays.push(together)
+  }
+  
+  return mPlays
+}
