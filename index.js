54e5eba209579baf82344a5e2178a201c0bd394d
benign-glitch-2757 <abhi.sharm.414@gmail.com>
2020-03-11 02:39:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  let arr = [];
+  for (let i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  for(let i = 0; i < facts.length; i++) {
+    facts[i] += "!!!"
+  }
+  return facts;
+}
+function iLoveTheBeatles(num) {
+  let arr = [];
+  do {
+    arr.push("I love the Beatles!");
+  } while(arr.length < 15-num);
+  return arr;
+}
