0609182e980675ed033cf9b736bd0e2b5a277cde
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-02 09:01:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]; //"John Lennon plays guitar"
+
+function theBeatlesPlay(musicians, instruments) {
+  var musiciansPlays = [];
+  
+  for (var i = 0; i < musicians.length; i += 1) {
+    var musicians1 = musicians[i];
+    var instruments1 = instruments[i];
+    var sentence = musicians1 + " plays " + instruments1
+    musiciansPlays.push(sentence);
+  }
+  return musiciansPlays;
+}
+
+
+const facts = [
+  "He Was Legally Blind",
+  "We Don't Know Where His Ashes Are",
+  "He Was Raised By His Aunt",
+  "He Wanted To Write A Children's Book"
+];
+
+function johnLennonFacts (facts) {
+  var newFacts = [];
+  var i = 0;
+  
+  while (i < facts.length) {
+    var factsSent = facts[i];
+    newFacts.push(factsSent + "!!!");
+    
+    i += 1;
+  }
+  
+  return newFacts;
+}
+
+function iLoveTheBeatles(number) {
+  const array = []
+
+  do {
+    array.push('I love the Beatles!')
+    number++
+  } while (number < 15)
+
+  return array;
+}
