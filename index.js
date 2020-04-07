175d9470bbb47dc9bf7f60b3db151dd450f8a025
Jason Kang <hetae.kang@gmail.com>
2020-04-07 18:33:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // add solution here
+let output1 = [];
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let i = 0; i < instruments.length; i++) {
+    output1.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return output1;
+}
+
+let output2 = [];
+
+function johnLennonFacts(facts) {
+  let i = 0;
+  while (i < facts.length) {
+    facts[i] += '!!!';
+    i++
+  }
+  return facts;
+}
+
+let output3 = [];
+
+function iLoveTheBeatles(num) {
+  
+  if (num < 15) {
+    do {
+      num++;
+      output3.push('I love the Beatles!')
+    } while (num < 15)
+  } else {
+    output3 = ['I love the Beatles!'];
+  }
+  return output3;
+}
