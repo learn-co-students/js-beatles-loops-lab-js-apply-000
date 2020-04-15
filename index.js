fd35e095535cbb59ecf606716c82eef8695584ef
Nicole Wiehe <nicoleoliviawiehe@gmail.com>
2020-04-15 22:43:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var theirInstrument = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    var musician = musicians[i]; 
+    var instrument = instruments[i]; 
+    var sent = musician + ' plays ' + instrument;
+    theirInstrument.push(sent);
+  }
+  
+  return theirInstrument;
+}
+
+
+function johnLennonFacts(facts) {
+  var lennonFacts = [];
+  
+  var i = 0;
+  while (i < facts.length) {
+    var fact = facts[i];
+    lennonFacts.push(fact + '!!!');
+    i++;
+  }
+  
+  return lennonFacts;
+}
+
+
+function iLoveTheBeatles(number) {
+  var love = []; 
+  
+  do {
+    love.push('I love the Beatles!')
+    number++
+    
+  } while (number < 15)
+ 
+ return love;   
+}
