707be0de4a713522bc975792dd424aba1b1a76d4
stupendous-keyboard-4608 <nicholas.mcdonnell@gmail.com>
2020-04-25 22:20:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 // add solution here
-const playtime = []
+
+import { abs } from 'math.js'
 
 function theBeatlesPlay(musicians, instruments) {
-  for (i = 0; i < musicians.length; i++){
-    playtime.push(musicians[i] + "plays" + instruments[i])
+  const playtime = []
+  for (let i = 0; i < musicians.length; i++){
+    playtime.push(musicians[i] + " plays " + instruments[i])
   }
   return playtime
 }
+
+
+function johnLennonFacts(facts){
+  let i = 0;
+  while (i < facts.length){
+    facts[i] = facts[i] + "!!!";
+    i++;
+  }
+  return facts
+}
+
+function iLoveTheBeatles(num){
+  const love = [];
+  do {
+    love.push("I love the Beatles!")
+    num++;
+  } while (num < 15)
+  return love
+}
+
