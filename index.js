de32273166484024698b6125c9ef2651830d3bfd
danny <ohdangitsdaniel@gmail.com>
2020-05-04 19:43:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // add solution here
+
+function theBeatlesPlay(arr1, arr2){
+  let output = [];
+  
+  for(let i=0; i < arr1.length; i++){
+    let temp = arr1[i] + " plays " + arr2[i];
+    output.push(temp);
+  }
+  return output;
+}
+
+function johnLennonFacts(arr){
+  let output = [], counter = 0;
+  
+  while(counter < arr.length){
+    output.push(arr[counter] + "!!!");
+    counter++;
+  }
+  return output;
+}
+
+function iLoveTheBeatles(num){
+  let output = [], counter = 0;
+  
+  do {
+    output.push("I love the Beatles!");
+    counter++;
+  } 
+  while(counter <= num  && num < 15);
+  
+  return output;
+}
