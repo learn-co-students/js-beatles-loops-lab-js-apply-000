982278ea5c676a1621b78f7ca8e84c8ddece5b2c
Angela Ratto <angelaratto@gmail.com>
2020-05-05 20:27:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let whoPlaysWhat = [];
+  for (let i=0; i<musicians.length; i++) {
+    whoPlaysWhat[i] = musicians[i] + " plays " + instruments[i];
+  }
+  return whoPlaysWhat;
+}
+
+function johnLennonFacts(factArr) {
+  let counter=0;
+  while (counter < factArr.length) {
+    factArr[counter] = factArr[counter] + "!!!";
+    counter++;
+  }
+  return factArr;
+}
+
+function iLoveTheBeatles(num) {
+  let arr = [];
+  do {
+    arr.push("I love the Beatles!");
+    num++;
+  }
+  while (num<15);
+  return arr;
+}
