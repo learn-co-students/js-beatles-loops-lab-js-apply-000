ba88ddf15109fdc29ac14609003485070cdbf765
Amelia Haba <ameliahaba347@gmail.com>
2020-05-16 22:53:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// add solution here
-function theBeatlesPlay (musicians, instruments) {
+'use strict';
+function theBeatlesPlay(musicians, instruments) {
   var array = [];
-  for (i = 0; i < musicians.length; i++)
-  array.push(muscians[i] + ' plays ' + instruments[i]);
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
 }
-return array;
-}
-function johnLennonFacts(facts); {
+
+
+function johnLennonFacts(facts) {
   var results = [];
   var count = 0;
   while (count < facts.length) {
@@ -17,9 +19,11 @@ function johnLennonFacts(facts); {
 }
 
 function iLoveTheBeatles(num) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!")
-    num++;
-  } while (num < 15);
-  return array;
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+
+    return love;
+}
