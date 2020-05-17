00bb924747fa1613ff927a381812c03c3f99d5d9
vivacious-queue-3174 <caryaharper@gmail.com>
2020-05-17 06:20:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+const instrumentsPlayed = []; 
+
+for ( let i = 0;  i < musicians.length; i++) {
+  instrumentsPlayed.push(`${musicians[i]} plays ${instruments[i]}`)
+}
+
+return instrumentsPlayed;
+}
+
+function johnLennonFacts(johnFacts) {
+  const facts = [];
+  let i = 0;
+  while (i < johnFacts.length) {
+    facts.push(`${johnFacts[i]}!!!`)
+    
+    i++;
+  }
+
+return facts;
+  }
+
+function iLoveTheBeatles(copies) {
+  const love = [];
+  let n = copies;
+  do {
+   n = n + 1;
+  love.push('I love the Beatles!')
+  
+  } while (n < 15);
+  return love;
+
+  
+}
