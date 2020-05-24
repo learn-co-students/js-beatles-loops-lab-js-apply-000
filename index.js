01c9595764420bac638bbb1f66b4101339b955f5
Clement Mensah <clemensah10@gmail.com>
2020-05-24 07:50:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arr = []
+  for(var i = 0; i < musicians.length; i++){
+    arr.push(musicians[i] + ' plays '+ instruments[i])
+  }
+  return arr;
+}
+
+function johnLennonFacts(array){
+  var i = 0
+  while(i<array.length){
+    array[i]+='!!!';
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles(number){
+  var array = [];
+  var i = 0;
+  do{
+    array.push('I love the Beatles!');
+    i++;
+  }while(i<=number && number < 15);
+  return array
+}
