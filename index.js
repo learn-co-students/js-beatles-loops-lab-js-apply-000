72ac475c0c6464ba75a2554048722a8f6ae7f4e6
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-06-16 21:58:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var musiciansPlay = [];
+  for (var i = 0; i < musicians.length; i++) {
+    musiciansPlay.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return musiciansPlay;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  while (i < facts.length) {
+    facts[i] = `${facts[i]}!!!`;
+    i++;
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(n) {
+  var loveStrings = [];
+  do {
+    loveStrings.push("I love the Beatles!");
+    n++;
+  } while (n < 15);
+  return loveStrings;
+}
