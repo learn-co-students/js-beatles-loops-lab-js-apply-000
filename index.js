837215e3631200cd3191e4ee3cf316591407ba14
Melody Chen <mcmelodychen@gmail.com>
2020-06-19 21:40:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // add solution here
+
+const theBeatlesPlay = (musicians, instruments) => {
+  let matched = [];
+  for (let i = 0; i < musicians.length; i++) {
+    const currentMusician = musicians[i];
+    const currentInstruments = instruments[i]; 
+    
+    matched.push(`${currentMusician} plays ${currentInstruments}`);
+  }
+  return matched;
+}
+
+
+const johnLennonFacts = (facts) => {
+  let arrFacts = [];
+  for (let i = 0; i < facts.length; i++) {
+    arrFacts.push(facts[i] + '!!!');
+  }
+  return arrFacts; 
+}
+ 
+ 
+const iLoveTheBeatles = (num) => {
+  let senArr = [];
+  const sentence = 'I love the Beatles!';
+  
+  if ( num > 15) {
+    senArr.push(sentence);
+  } else {
+      let i = 0;
+      while (i <= num) {
+        senArr.push(sentence);
+        i++;
+      }
+    }
+  return senArr;
+  
+}
