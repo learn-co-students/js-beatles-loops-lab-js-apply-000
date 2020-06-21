af93c2bbdb3f8f38329f2c9294dc9c6e6aadfb48
Johnson Liu <jyliu17@gmail.com>
2020-06-21 02:06:48 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -15,7 +15,6 @@ describe('theBeatlesPlay', function() {
         "He hated the sound of his own voice"
       ])).toEqual(["He was the last Beatle to learn to drive!!!", "He was never a vegetarian!!!", "He was a choir boy and boy scout!!!", "He hated the sound of his own voice!!!"]);
     });
-    
     it("takes in any array of strings and returns a new array with exclamation points added to each element", function() {
       expect(johnLennonFacts([
         "While on tour, he enjoyed playing Monopoly",
@@ -23,7 +22,6 @@ describe('theBeatlesPlay', function() {
       ])).toEqual(["While on tour, he enjoyed playing Monopoly!!!", "Harmonica was the first instrument Lennon learned to play!!!"])
     })
   });
-
   describe('iLoveTheBeatles', function() {
     it("returns an array of 'I love the Beatles!' 8 times when passed the parameter 7 ", function() {
       expect(iLoveTheBeatles(7)).toEqual(["I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!", "I love the Beatles!"]);
