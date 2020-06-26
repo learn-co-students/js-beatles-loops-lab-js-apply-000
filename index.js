c36f8b1a8c9d66ef6444ef32de11a929bca280cf
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-06-26 17:31:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  let whoPlaysWhat = [];
+  for (var i = 0; i < musicians.length; i++) {
+    whoPlaysWhat.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return whoPlaysWhat;
+}
+
+function johnLennonFacts(arrayOfFacts) {
+  let count = 0;
+  let excitingFacts = [];
+  while (count < arrayOfFacts.length) {
+    excitingFacts.push(arrayOfFacts[count] + '!!!');
+    count++;
+  }
+  return excitingFacts;
+}
+
+function iLoveTheBeatles(num) {
+  let whoDoYouLove = [];
+  do {
+    whoDoYouLove.push('I love the Beatles!');
+    num++;
+  } while (num < 15);
+  return whoDoYouLove;
+}
