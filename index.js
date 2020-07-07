4b8ed1cf6ec309bfbf990889e1e6d09cdde6ff40
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-07-07 19:45:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var arrayOne = [];
+  for(let i=0; i < musicians.length; i++){
+      arrayOne.push(musicians[i]+' plays ' + instruments[i])
+    
+  }
+  return arrayOne;
+}
+function johnLennonFacts(facts){
+  let i=0;
+  let arrayTwo=[];
+  while(facts[i]){
+    arrayTwo.push(facts[i]+"!!!");
+    i++;
+  }
+  return arrayTwo;
+}
+function iLoveTheBeatles(n){
+  var arrayThree = [];
+  let i = 0;
+
+  do {
+    arrayThree.push("I love the Beatles!");
+    n++;
+  }
+  while( n < 15);
+  return arrayThree
+}
