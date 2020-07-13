4b66a73a34c26cc885fc67b507ac2110a0d9fd98
Ayuna Tsyrenova <ayuna91@gmail.com>
2020-07-13 04:00:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var playlist=[];
+  for(let i = 0; i<musicians.length; i++){
+     playlist.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return playlist;
+}
+
+function johnLennonFacts(arrayOfFacts){
+  var johnLennonFacts = [];
+  var i = 0;
+  while(i<arrayOfFacts.length){
+    johnLennonFacts.push(`${arrayOfFacts[i]}!!!`);
+    i++;
+  }
+ return johnLennonFacts;
+}
+
+
+function iLoveTheBeatles(number){
+  var array = [];
+  do{
+    array.push('I love the Beatles!');
+    number++;
+  }while(number<15)
+  return array;
+}
+
+
+
+
+
+
+
+
+
