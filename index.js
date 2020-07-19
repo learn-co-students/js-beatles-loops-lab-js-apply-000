8306bd8a61aaca6d77498869b5732bc003be4364
zhengjianliu <49765007+zhengjianliu@users.noreply.github.com>
2020-07-19 15:47:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-function theBeatlesPlay(musicians,instruments) {
-  var p = [];
-  for (var i = 0; i < musicians.length; i++) {
-    p.push(musicians[i] + ' plays ' + instruments[i])
+// add solution here
+function theBeatlesPlay(musicians,instruments){
+  var p = []
+  for(var i = 0; i <musicians.length; i++){
+    p.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return p;
+  return p
 }
 
-function johnLennonFacts(arrays) {
+function johnLennonFacts(facts){
   var i = 0
-  while (i < arrays.length) {
-    arrays[i] += '!!!';
-    i++;
+  while(i<facts.length){
+    facts[i] += '!!!'
+    i++
   }
-  return arrays;
+  return facts;
 }
 
-function iLoveTheBeatles(incr) {
-  var p = [];
-  var i = 0;
-  do {
-    p.push('I love the Beatles!');
-    i++;
-  } while (i <= incr && incr < 15);
-  return p;
+function iLoveTheBeatles(number){
+  var array = []
+  var i = 0
+  do{
+    array.push('I love the Beatles!')
+    i++
+  }while(number < 15 && i < 15 - number)
+  return array
 }
