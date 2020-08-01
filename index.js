937b20103311c173cb44e551c2bdd399fd260dbe
trustworthy-link-3948 <minglianglin50@gmail.com>
2020-08-01 21:17:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var result = []
+  var i
+  
+  for(i=0; i < musicians.length; i++) {
+    result.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  
+  return result
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  var result = []
+  
+  while (i < facts.length) {
+    result.push(`${facts[i]}!!!`)
+    
+    i ++    
+  }
+  
+  return result
+}
+
+function iLoveTheBeatles (num) {
+  var result = []
+  
+  do {
+    result.push("I love the Beatles!")
+    num += 1
+  }
+  while(num < 15)
+  
+  return result
+}
