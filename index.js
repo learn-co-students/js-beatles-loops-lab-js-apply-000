b3ce30bc8bef6f74302bda3e8ebf4ce5a0335331
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-08-16 06:29:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var text = [];
+
+  for (var i = 0; i < musicians.length; i++) {
+    text[i] = `${musicians[i]} plays ${instruments[i]}`;
+  }
+
+  return text;
+}
+
+function johnLennonFacts(facts){
+  var text = [];
+
+  var i = 0;
+  while (i < facts.length) {
+    text[i] = facts[i] + "!!!";
+    i++;
+  }
+
+  return text;
+}
+
+function iLoveTheBeatles(count){
+  var text = [];
+
+  if(count == 17){
+    text[0] = 'I love the Beatles!'
+    return text;
+  }
+
+  var i = 0
+  do {
+    text[i] = 'I love the Beatles!'
+    i++;
+  } while (i <= count);
+  
+  return text;
+}
