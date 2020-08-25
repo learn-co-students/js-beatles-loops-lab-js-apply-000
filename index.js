c48dce9d5073d5a70797079e252f31f883f1a193
Paul Kim <khyunwoo1@gmail.com>
2020-08-25 01:31:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,69 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  
+  var emptyArray = [];
+  var emptyString = "";
+
+  for (var i = 0; i < musicians.length; i++) {
+    emptyString = musicians[i] + " plays " + instruments[i];
+    emptyArray.push(emptyString) 
+  }
+  
+  return emptyArray;
+  
+  
+}
+  
+
+
+function johnLennonFacts(lennonArray){
+  
+  var myString = '';
+  var myArray = [];
+
+
+  var i = 0;
+  while(i < lennonArray.length) {
+    
+    myString = (lennonArray[i] + '!!!');
+
+
+    
+    myArray.push(myString);
+    i++;
+}
+  return myArray;
+
+}
+
+
+
+function iLoveTheBeatles(number){
+  
+  var emptyStringTwo = ""
+  var emptyArrayTwo = [];
+  var i = 0;
+
+  if (number < 15){
+  
+      do{
+          i++;
+          emptyStringTwo = "I love the Beatles!";
+          emptyArrayTwo.push(emptyStringTwo);
+      
+          
+      
+      } while(i < (15 - number))
+  
+    return emptyArrayTwo;
+  }
+  
+  else {
+    emptyStringTwo = "I love the Beatles!";
+    emptyArrayTwo.push(emptyStringTwo);
+    return emptyArrayTwo;
+  }
+
+}
+
+
+
