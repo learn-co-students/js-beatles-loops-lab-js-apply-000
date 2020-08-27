ce07984c9ff2c040a2a85f09e2b04ee52c780643
Kalsang Agongtsang <tznklsng@yahoo.com>
2020-08-27 01:43:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+ } 
+  return array;
+}
--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 describe('theBeatlesPlay', function() {
   it("returns an array of strings containing what instruments each musician plays", function() {
     const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+    r", "Lead Guitar", "Drums"];const instruments = ["Guitar", "Bass Guita
 
     expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
   });
