08f90071ee23976538fefb20e357057ddb57d234
Kalsang Agongtsang <tznklsng@yahoo.com>
2020-08-27 02:01:29 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (var i = 0; i < musicians.length; i++) {
   	array.push(musicians[i] + ' plays ' + instruments[i]);
- } 
+  }
   return array;
 }
