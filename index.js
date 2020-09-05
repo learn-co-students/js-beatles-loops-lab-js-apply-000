4069bd5aafda4642692f3d28bae4045231102695
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-05 22:39:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // add solution here
+function theBeatlesPlay(a,b) {
+  var s = [];
+  
+  for (var i = 0; i < a.length; i++) {
+    // s.push(a[i] + " plays " + b[i]);
+    s.push(`${a[i]} plays ${b[i]}`)
+  }
+  return s;
+}
+
+function johnLennonFacts(f) {
+  var ans = [];
+  
+  for (var i = 0; i < f.length; i++) {
+    ans.push(f[i] + "!!!");
+  }
+  return ans;
+}
+
+function iLoveTheBeatles(n) {
+  var ans = [];
+  
+  do {
+    n ++
+    ans.push("I love the Beatles!")
+    
+  } while(n < 15)
+  return ans;
+}
+
+
+
+
