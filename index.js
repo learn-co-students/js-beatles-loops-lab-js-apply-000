9e1143680dbccf719a19415569f8329db68cc136
Kalsang Agongtsang <tznklsng@yahoo.com>
2020-09-06 19:37:53 +0000

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return array;
 }
+
+function johnLennonFacts (facts){
+  var array=[];
+  var i = 0;
+  while ( i < facts.length){
+    array.push (facts[i] + "!!!");
+    i++;
+  }
+  return array;
+}
+
+function iLoveTheBeatles (num){
+  var array=[];
+  do {
+    array.push("I love the Beatles!");
+    num++;
+  }
+   while (num<15);
+    return array;
+}
