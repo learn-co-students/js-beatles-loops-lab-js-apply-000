60472cd69db7bf4d67f23313b5bdb0556eb673d9
Alice Tang <tang.ayt@gmail.com>
2020-09-11 00:12:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(arrMusicians, arrInstruments) {
+  let sentArr = [];
+  for (let i = 0; i < arrMusicians.length; i++) {
+    sentArr.push(arrMusicians[i] + ' plays ' + arrInstruments[i]);
+  }
+  return sentArr;
+}
+
+function johnLennonFacts(arrFacts) {
+  let i = 0;
+  while (i< arrFacts.length) {
+    arrFacts[i] += '!!!';
+    i++;
+  }
+  return arrFacts;
+}
+
+function iLoveTheBeatles(num) {
+  let arr = [];
+  do {
+    arr.push('I love the Beatles!');
+    num++;
+  }
+  while (num < 15)
+  return  arr;
+}
