b985905687245bc60551f04d722ab63fac718d6f
Audrey Yun <96audreyyun@gmail.com>
2020-09-16 22:13:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // add solution here
+
+function theBeatlesPlay (musicians, instruments) {
+  var arr = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i]);
+  }
+  
+  return arr;
+}
+
+function johnLennonFacts (facts) {
+  var exclArr = [];
+  
+  var i = 0;
+  while (i < facts.length) {
+    var fact = facts[i]; 
+    exclArr.push(fact +'!!!');
+    
+    i++;
+  }
+  
+  return exclArr;
+}
+
+function iLoveTheBeatles(num) {
+  var arr= [];
+  var beatlesStr = "I love the Beatles!";
+  var count = 15 - num;
+  var i = 0;
+  
+  do {
+    i++;
+    arr.push(beatlesStr);
+
+  } while (i < count);
+
+  return arr;
+}
