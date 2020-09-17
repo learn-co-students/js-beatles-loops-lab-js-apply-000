a054948252fd30f0b1301dc4047fd8d31e61dc15
Colin Chauche <colin.chauche@gmail.com>
2020-09-17 00:50:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  let whatTheMembersPlay = [];
+  for (let i = 0; i < musicians.length; i++) {
+    let musicanPlays = musicians[i] + ' plays ' + instruments[i];
+    whatTheMembersPlay.push(musicanPlays);
+  }
+  return whatTheMembersPlay;
+}
+
+function johnLennonFacts(facts) {
+  for (let i = 0; i < facts.length; i++) {
+    facts[i] = facts[i] + '!!!';
+  }
+  return facts;
+}
+
+function iLoveTheBeatles(num) {
+  let fanChant = [];
+  do  {
+    fanChant.push('I love the Beatles!');
+    num ++;
+  } while ( num < 15 );
+  return fanChant;
+}
