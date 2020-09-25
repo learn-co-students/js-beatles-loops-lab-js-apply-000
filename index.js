5d4bd16fdc43457bb7eeefb75e128f0f8fca97e6
Jacob Marthaller <marthaller.jr@gmail.com>
2020-09-25 20:00:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // add solution here
+function theBeatlesPlay(musicianArray, instrumentArray) {
+  let result = [];
+  
+  for (let i = 0; i < musicianArray.length; i++) {
+    result.push(musicianArray[i] + ' plays ' + instrumentArray[i]);
+  }
+  return result;
+}
+
+function johnLennonFacts (facts) {
+  let result = [];
+  let i = 0;
+  
+  while (i < facts.length) {
+    result.push(facts[i] + "!!!");
+    i++;
+  }
+  return result;
+}
+
+function iLoveTheBeatles(num) {
+  let result = [];
+  let i = 0;
+  
+  do {
+    result.push("I love the Beatles!");
+    num++;
+  } while (num < 15);
+  
+  return result;
+}
+
+
+
+
+
+
+
