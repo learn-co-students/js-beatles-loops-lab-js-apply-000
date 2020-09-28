c7ce86262d830fb9dda107b9025263d8f381679a
Tenzin Kalsang <tznklsng28@gmail.com>
2020-09-28 02:38:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var array = [];
+  for(var i=0; i< musicians.length; i++){
+    array.push(musicians[i] + " plays " + instruments[i]);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var i=0;
+  var array = [];
+  while(i<facts.length){
+    array.push(facts[i] + "!!!")
+    i++;
+    }
+    return array;
+}
+
+function iLoveTheBeatles(num){
+  var array = [];
+  do {
+    array.push("I love the Beatles!")
+    num++;
+    }
+    while(num < 15);
+  return array;
+}
