f78ae93ded2cc8245a57e1b22366ec0603ca1813
Louis Joe <newhopemissionliberia@gmail.com>
2020-10-10 00:28:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// add solution here
+/*Beatles Loops function
+returns an array of string containing what instruments each musician plays*/
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  for(let i = 0; i < musicians.length; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }return array
+}
+
+/*johnLennonFacts function
+returns an array of string with exclamation points*/
+function johnLennonFacts(facts){
+  var i = 0
+  while (i < facts.length){
+      facts[i] = facts[i] + '!!!'
+     i++
+  }return facts
+}
+
+/*iLoveTheBeatles function
+returns an array of 'I love the Beatles!' 8 times when passed the parameter 7
+returns an array of 'I love the Beatles!' once when passed the parameter 17*/
+function iLoveTheBeatles(n){
+var array = []
+if (n === 17){
+  array.push("I love the Beatles!")
+}else{
+  do{
+      array.push("I love the Beatles!")
+    }while(array.length <= n)
+  }return array
+}
