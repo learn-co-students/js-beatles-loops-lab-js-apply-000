f5a735f00c92cc73d82233627d563fde69dd2372
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-12 20:29:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // add solution here
-function theBeatlesPlay() {
-
+function theBeatlesPlay(musicians , instruments) {
+  var x = [];
+  for (var i = 0; i < musicians.length; i++) {
+      x.push(musicians[i] + ' plays ' + instruments[i]);
+    }
+  return x;
 }
 
-function johnLennonFacts() {
-
+function johnLennonFacts(a) {
+  var i= 0;
+  while (i < a.length) {
+    a[i] = a[i] + "!!!";
+    i++
+  }
+  return a;
 }
 
-function iLoveTheBeatles() {
-
+function iLoveTheBeatles(n) {
+  var s = [];
+  do {
+    s.push('I love the Beatles!');
+    n++;
+  } while( n < 15)
+  return s;
 }
