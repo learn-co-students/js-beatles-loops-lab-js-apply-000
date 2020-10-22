7ccda553a374e05f1a9c7634e7627ef270370332
Vanessa Porras <vanessapcg@gmail.com>
2020-10-22 23:16:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // add solution here
+
+function theBeatlesPlay (musicians, instruments)
+{
+  
+  var array=[];
+
+    for (var i=0; i<musicians.length; i++)
+    {
+        array[i]=musicians[i]+' plays '+instruments[i];
+    }  
+  
+ return array;
+}
+
+
+
+function johnLennonFacts (facts){
+  
+  var a=0;
+ 
+  while (a<facts.length)
+  {
+    facts[a]=facts[a]+'!!!'
+    a++;
+  }
+  
+  return facts;
+}
+
+function iLoveTheBeatles(n){
+  
+  var empty=[];
+  var i=0;
+  
+  do{
+       empty[i]='I love the Beatles!';
+       i++;
+       n++;
+    
+    } while (n<15);
+  
+  return empty;
+}
